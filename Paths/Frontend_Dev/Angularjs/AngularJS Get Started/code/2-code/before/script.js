1a82ee6818c54d4f6aa35dd0705de9a9c1a56bb2
specter01wj <specter01wj@gmail.com>
2017-05-07 20:39:40 -0400

    05/07/2017 v1

--- Paths/Frontend_Dev/Angularjs/AngularJS Get Started/code/2-code/before/script.js
+++ Paths/Frontend_Dev/Angularjs/AngularJS Get Started/code/2-code/before/script.js
@@ -1,8 +1,32 @@
 
 var app = angular.module('myApp', []);
 
-app.controller('MainController', ['$scope', function ($scope) {
+app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+
+    /*var person = {
+      firstName: 'James',
+        lastName: 'Wang',
+        imageSrc: 'http://odetocode.com/images/scott_allen_2.jpg'
+    };
+
+    $scope.person = person;*/
+
+    var onUserComplete = function (response) {
+      $scope.user = response.data;
+    };
+
+    var onError = function (reason) {
+      $scope.error = "Data error!Jin";
+    };
+
+    $http({
+        method: 'GET',
+        url: 'https://api.github.com/users/robconery'
+    }).then(onUserComplete, onError);
+
     $scope.message = 'Hola! James!!';
+
+
 }]);
 
 
