3d4553e885b5b5ed48c6ef0d3807a87043c372cb
specter01wj <specter01wj@gmail.com>
2017-05-16 17:08:46 -0400

    05/16/2017 v1

--- Paths/Frontend_Dev/Angularjs/AngularJS Get Started/code/5-code/before/github.js
+++ Paths/Frontend_Dev/Angularjs/AngularJS Get Started/code/5-code/before/github.js
@@ -15,10 +15,26 @@
                             return response.data;
                         });
       };
+
+      var getRepoDetails = function(username, reponame){
+            var repo;
+            var repoUrl = "https://api.github.com/repos/" + username + "/" + reponame;
+
+            return $http.get(repoUrl)
+                .then(function(response){
+                    repo = response.data;
+                    return $http.get(repoUrl + "/collaborators");
+                })
+                .then(function(response){
+                    repo.collaborators = response.data;
+                    return repo;
+                });
+      };
       
       return {
           getUser: getUser,
-          getRepos: getRepos
+          getRepos: getRepos,
+          getRepoDetails: getRepoDetails
       };
         
     };
