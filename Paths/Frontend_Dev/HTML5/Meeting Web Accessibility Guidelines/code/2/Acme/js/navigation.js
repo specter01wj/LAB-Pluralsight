53628c4dd39c686a5abca75eb1ba0a8ae562f811
specter01wj <specter01wj@gmail.com>
2017-05-24 18:32:17 -0400

    05/24/2017 v1

---
+++ Paths/Frontend_Dev/HTML5/Meeting Web Accessibility Guidelines/code/2/Acme/js/navigation.js
@@ -0,0 +1,4 @@
+var menu = document.querySelector('#main-nav');
+document.querySelector('#menu-trigger').addEventListener('click', function (e) {
+  menu.classList.toggle('active');
+});
---
+++ Paths/Frontend_Dev/HTML5/Meeting Web Accessibility Guidelines/code/2/Acme/js/validation-alt.js
@@ -0,0 +1,54 @@
+var errors = document.getElementById('errors');
+var form = document.getElementById('contact');
+var required = Array.prototype.slice.call(form.querySelectorAll('[required]'));
+
+form.addEventListener('blur', function (e) {
+
+  if (e.target.hasAttribute('required')) {
+    validate(e.target);
+  }
+
+}, true);
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var invalid = [];
+
+  required.forEach(function (field) {
+
+    if (validate(field)) {
+      invalid.push(field);
+    }
+  });
+
+  if (invalid.length) {
+    var list = '<h2>Errors</h2><ol id="error-list">';
+
+    invalid.forEach(function (field) {
+      list += '<li><a href="#' + field.id + '">' +
+        field.previousElementSibling.querySelector('.error-message')
+        .textContent + '</a></li>';
+    });
+
+    list += '</ol>';
+
+    errors.innerHTML = list;
+    errors.focus();
+  } else {
+    errors.innerHTML = '';
+  }
+});
+
+function validate(field) {
+
+  var label = field.previousElementSibling;
+
+  if (!field.value.length) {
+    label.classList.add('is-invalid');
+    field.setAttribute('aria-invalid', 'true');
+    return true;
+  } else if (field.hasAttribute('aria-invalid')) {
+    label.classList.remove('is-invalid');
+    field.removeAttribute('aria-invalid');
+  }
+}
