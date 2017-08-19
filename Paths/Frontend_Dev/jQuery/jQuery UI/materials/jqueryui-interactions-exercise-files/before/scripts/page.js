511d3f11ae4641dc392cbab129cce8b3ac49ebf7
specter01wj <specter01wj@gmail.com>
2017-08-19 13:22:57 -0400

    08/19/2017 v1

---
+++ Paths/Frontend_Dev/jQuery/jQuery UI/materials/jqueryui-interactions-exercise-files/before/scripts/page.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="jquery-1.6.2.js" />
+/// <reference path="jquery-ui-1.8.16.custom.js" />
+
+
+$( document ).ready(function() {
+
+
+	//$("#draggables").draggable();
+
+    $("#d1").draggable({
+    	//axis: 'y',
+    	stack: '.ui-draggable',
+    	//grid: [30,100],
+    	snap: '#d2, #d3',
+    	//containment: '#draggables'
+        //revert: "invalid"
+    });
+
+    $("#d2").draggable({
+        snap: "#d1, #d3",
+        helper: function () {
+            return $("<div>I am a helper</div>");
+        }
+    });
+
+    $("#d3").draggable({
+        handle: "header",
+        helper: 'clone'
+    });
+
+    $("#d1, #d2, #d3").draggable(
+        "option", "stack", ".ui-draggable");
+
+
+});
