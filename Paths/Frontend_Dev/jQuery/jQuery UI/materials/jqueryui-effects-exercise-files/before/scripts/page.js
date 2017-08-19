91ff04ab342cd26437f886923e8d630e29f6d8ce
specter01wj <specter01wj@gmail.com>
2017-08-19 15:29:07 -0400

    08/19/2017 v3

--- Paths/Frontend_Dev/jQuery/jQuery UI/materials/jqueryui-effects-exercise-files/before/scripts/page.js
+++ Paths/Frontend_Dev/jQuery/jQuery UI/materials/jqueryui-effects-exercise-files/before/scripts/page.js
@@ -1,6 +1,51 @@
 ﻿/// <reference path="jquery-1.6.2.js" />
 /// <reference path="jquery-ui-1.8.16.custom.js" />
 
-$(function () {
+$( document ).ready(function() {
     
+
+	/*$("#target").click(function () {
+        $(this).animate({
+        	'width': '+=20',
+        	'height': '+=20',
+        	'background-color': 'red'
+        });
+        $(this).effect('bounce');
+    });*/
+
+	/*$("#target")
+        .css("position", "relative")
+        .click(function () {
+            $(this).animate({
+            	left: 300
+            }, 1000, 'easeInOutElastic');
+    });*/
+
+    $("#target")
+    	.css("position", "relative")
+    	.click(function () {
+        	$(this).toggleClass("leftTarget", "slow");
+    });
+
+
+	$("#draggables").children().draggable();
+
+    $("#trash").droppable({
+        activeClass: "opaque",
+        drop: function (event, ui) {
+            ui.draggable.hide("explode");
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 });
