d33ae1916e4aba1fa1734686b211356ea0342c1d
specter01wj <specter01wj@gmail.com>
2017-10-09 21:59:38 -0400

    10/09/2017 v2

--- Paths/Frontend_Dev/Angularjs/AngularJS Best Practices/angular-best-practices/02-organizing-your-code/Demos/Original/Modules/app.js
+++ Paths/Frontend_Dev/Angularjs/AngularJS Best Practices/angular-best-practices/02-organizing-your-code/Demos/Original/Modules/app.js
@@ -1,5 +1,11 @@
-angular.module('app', []);
+angular.module('app', ['app3', 'app2']);
+angular.module('app2', []);
+angular.module('app3', []);
 
-angular.module('app').controller('Controller1', function($scope) {
-  $scope.name = 'Controller 1';
-})
+angular.module('app2').controller('Controller1', function($scope) {
+  $scope.name = 'Controller 1 in App 2';
+});
+
+angular.module('app3').controller('Controller1', function($scope) {
+  $scope.name = 'Controller 1 in App 3';
+});
