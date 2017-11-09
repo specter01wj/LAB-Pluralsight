b97f5a41dabbe8c211e2f97a29971f3465df7228
specter01wj <specter01wj@gmail.com>
2017-11-09 18:00:11 -0500

    11/09/2017 v1

---
+++ Paths/Backend_Dev/Java/Java Fundamentals - The Java Language/java-fundamentals-language/4/before/calcengine_1/calcengine_1/src/calcengine_1/Calcengine_1.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcengine_1;
+
+/**
+ *
+ * @author jameswang
+ */
+public class Calcengine_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
+
+        for(int i = 0; i < opCodes.length; i++) {
+            switch(opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    results[i] = 0.0d;
+                    break;
+            }
+        }
+        for(double theResult : results) {
+            System.out.print("result = ");
+            System.out.println(theResult);
+        }
+        
+    }
+    
+}
