2c791547b95139a47a5ff9711c0daaf8784291fc
specter01wj <specter01wj@gmail.com>
2018-03-30 01:11:09 -0400

    03/29/2018 v5

---
+++ Paths/Frontend_Dev/HTML5/Practical HTML5/practical-html5/2-practical-html5-m2-basiccssjs-exercise-files/Demo 2/Before/JoesCoffeeStore.Web/JoesCoffeeStore.Web/scripts/testscript.js
@@ -0,0 +1,8 @@
+﻿
+var testButton = document.getElementsByClassName('addtocartbutton');
+
+for (var i=0; i < testButton.length; i++) {
+	testButton[i].onclick = function () {
+	    alert("Coffee added to your cart: No." + i);
+	}
+}
