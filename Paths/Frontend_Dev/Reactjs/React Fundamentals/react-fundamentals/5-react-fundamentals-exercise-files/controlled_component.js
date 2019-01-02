4283b7894ca79bf192ada5455c63f6eee568071f
specter01wj <specter01wj@gmail.com>
2019-01-02 02:04:53 -0500

    12/16/2018 v1

--- Paths/Frontend_Dev/Reactjs/React Fundamentals/react-fundamentals/5-react-fundamentals-exercise-files/controlled_component.js
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-var ControlledComponent = React.createClass({
-    render: function() {
-        return <input type="text" value="init" />;
-    }
-});
- 
-React.renderComponent(<ControlledComponent />, document.body);
