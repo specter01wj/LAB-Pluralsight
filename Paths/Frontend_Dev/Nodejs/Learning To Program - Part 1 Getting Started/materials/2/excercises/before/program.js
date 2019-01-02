a737e69dfaa588bc4aea6507f96715775f5fe4a6
specter01wj <specter01wj@gmail.com>
2019-01-02 01:55:49 -0500

    12/16/2018 v1

--- Paths/Frontend_Dev/Nodejs/Learning To Program - Part 1 Getting Started/materials/2/excercises/before/program.js
+++
@@ -1,8 +0,0 @@
-var totalCost = 14.99;
-
-var sizeOfParty = process.argv[2];
-
-var averageCost  = totalCost / sizeOfParty;
-
-console.log("$" + averageCost);
-console.log(process.argv);
--- Paths/Frontend_Dev/Nodejs/Learning To Program - Part 1 Getting Started/materials/7/excercises/after/program.js
+++
@@ -1,7 +0,0 @@
-var book = require("./lib/grades").book;
-
-for(var i = 2; i < process.argv.length; i += 1) {
-  book.addGrade(parseInt(process.argv[i]));
-}
-
-console.log(book.getAverage());
