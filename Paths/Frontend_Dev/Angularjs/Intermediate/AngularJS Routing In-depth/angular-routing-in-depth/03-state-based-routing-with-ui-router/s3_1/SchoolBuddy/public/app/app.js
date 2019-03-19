e60231f103b2a0baaef5a1969936ddd6ff720210
specter01wj <specter01wj@gmail.com>
2019-03-19 17:21:00 -0400

    03/03/2019 v1

--- Paths/Frontend_Dev/Angularjs/Intermediate/AngularJS Routing In-depth/angular-routing-in-depth/03-state-based-routing-with-ui-router/s3_1/SchoolBuddy/public/app/app.js
+++ Paths/Frontend_Dev/Angularjs/Intermediate/AngularJS Routing In-depth/angular-routing-in-depth/03-state-based-routing-with-ui-router/s3_1/SchoolBuddy/public/app/app.js
@@ -13,6 +13,24 @@
                 controller: 'HomeController',
                 controllerAs: 'home'
         	})
+        	.state('schools', {
+                url: '/schools',
+                controller: 'AllSchoolsController',
+                controllerAs: 'schools',
+                templateUrl: '/app/templates/allSchools.html'
+            })
+            .state('classrooms', {
+                url: '/classrooms',
+                controller: 'AllClassroomsController',
+                controllerAs: 'classrooms',
+                templateUrl: '/app/templates/allClassrooms.html'
+            })
+            .state('activities', {
+                url: '/activities',
+                controller: 'AllActivitiesController',
+                controllerAs: 'activities',
+                templateUrl: '/app/templates/allActivities.html'
+            })
 
     }]);
 
