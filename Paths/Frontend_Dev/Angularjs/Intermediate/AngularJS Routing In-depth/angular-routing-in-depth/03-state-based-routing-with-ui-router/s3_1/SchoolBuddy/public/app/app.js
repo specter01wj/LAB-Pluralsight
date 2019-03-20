60123a8bd87c435963f3714c39975d79ea5b202c
specter01wj <specter01wj@gmail.com>
2019-03-20 17:33:10 -0400

    03/03/2019 v1

--- Paths/Frontend_Dev/Angularjs/Intermediate/AngularJS Routing In-depth/angular-routing-in-depth/03-state-based-routing-with-ui-router/s3_1/SchoolBuddy/public/app/app.js
+++ Paths/Frontend_Dev/Angularjs/Intermediate/AngularJS Routing In-depth/angular-routing-in-depth/03-state-based-routing-with-ui-router/s3_1/SchoolBuddy/public/app/app.js
@@ -34,4 +34,37 @@
 
     }]);
 
+
+    app.run(['$rootScope', '$log', function($rootScope, $log) {
+
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        
+           $log.debug('successfully changed states');
+        
+           $log.debug('event', event);
+           $log.debug('toState', toState);
+           $log.debug('toParams', toParams);
+           $log.debug('fromState', fromState);
+           $log.debug('fromParams', fromParams);
+        });
+
+        $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
+
+            $log.error('The requested state was not found: ', unfoundState);
+
+        });
+
+        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+
+            $log.error('An error occurred while changing states: ', error);
+
+            $log.debug('event', event);
+            $log.debug('toState', toState);
+            $log.debug('toParams', toParams);
+            $log.debug('fromState', fromState);
+            $log.debug('fromParams', fromParams);
+        });
+
+    }]);
+
 }());
