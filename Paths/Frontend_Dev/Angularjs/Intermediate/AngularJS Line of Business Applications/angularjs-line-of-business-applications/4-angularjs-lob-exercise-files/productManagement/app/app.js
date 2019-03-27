f75b557308f148280eb3913cc0c6760b75e8b256
specter01wj <specter01wj@gmail.com>
2019-03-27 12:24:06 -0400

    03/27/2019 v1

--- Paths/Frontend_Dev/Angularjs/Intermediate/AngularJS Line of Business Applications/angularjs-line-of-business-applications/4-angularjs-lob-exercise-files/productManagement/app/app.js
+++ Paths/Frontend_Dev/Angularjs/Intermediate/AngularJS Line of Business Applications/angularjs-line-of-business-applications/4-angularjs-lob-exercise-files/productManagement/app/app.js
@@ -3,33 +3,31 @@
  */
 (function () {
     "use strict";
-    var app = angular.module("productManagement",
-                            ["common.services",
-                             "ui.router",
-                             "productResourceMock"]);
+    var app = angular.module("productManagement", 
+                ["common.services", "ui.router", "productResourceMock"]).config(coreRoutesConfig);;
 
-    app.config(["$stateProvider",
-                "$urlRouterProvider",
-        function($stateProvider, $urlRouterProvider){
-            $urlRouterProvider.otherwise("/");
+    coreRoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-            $stateProvider
-                .state("home", {
-                    url: "/",
-                    templateUrl: "app/welcomeView.html"
-                })
-                // Products
-                .state("productList", {
-                    url: "/products",
-                    templateUrl: "app/products/productListView.html",
-                    controller: "ProductListCtrl as vm"
-                })
-                .state("productEdit", {
-                    url: "/products/edit/:productId",
-                    templateUrl: "app/products/productEditView.html",
-                    controller: "ProductEditCtrl as vm"
-                })
+    function coreRoutesConfig($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise("/");
 
-        }]
-    );
+        $stateProvider
+            .state("home", {
+                url: "/",
+                templateUrl: "app/welcomeView.html"
+            })
+            // Products
+            .state("productList", {
+                url: "/products",
+                templateUrl: "app/products/productListView.html",
+                controller: "ProductListCtrl as vm"
+            })
+            .state("productEdit", {
+                url: "/products/edit/:productId",
+                templateUrl: "app/products/productEditView.html",
+                controller: "ProductEditCtrl as vm"
+            })
+
+    }
+
 }());
