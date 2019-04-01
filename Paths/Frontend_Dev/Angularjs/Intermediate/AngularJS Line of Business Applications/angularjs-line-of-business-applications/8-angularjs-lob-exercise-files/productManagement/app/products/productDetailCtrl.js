dc147cdab203eda78dfd53047875c2af7c45b785
specter01wj <specter01wj@gmail.com>
2019-04-01 17:32:50 -0400

    03/27/2019 v1

--- Paths/Frontend_Dev/Angularjs/Intermediate/AngularJS Line of Business Applications/angularjs-line-of-business-applications/8-angularjs-lob-exercise-files/productManagement/app/products/productDetailCtrl.js
+++ Paths/Frontend_Dev/Angularjs/Intermediate/AngularJS Line of Business Applications/angularjs-line-of-business-applications/8-angularjs-lob-exercise-files/productManagement/app/products/productDetailCtrl.js
@@ -17,13 +17,11 @@
 
         vm.title = "Product Detail: " + vm.product.productName;
 
-        vm.marginPercent =
-            productService.calculateMarginPercent(vm.product.price,
-                                                  vm.product.cost);
+        vm.marginPercent = productService.calculateMarginPercent(vm.product.price, vm.product.cost);
 
         if (vm.product.tags) {
             vm.product.tagList = vm.product.tags.toString();
         }
     }
-    
+
 }());
