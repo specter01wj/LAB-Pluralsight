4d57e537e3d40c1d4c33c40f7d522541144ba4b6
specter01wj <specter01wj@gmail.com>
2019-04-15 01:33:44 -0400

    04/12/2019 v1

---
+++ Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/01-angular-2-in-action/4. Angular Structural Directives/src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { VehiclesComponent } from './vehicles.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [VehiclesComponent],
+  bootstrap: [VehiclesComponent]
+})
+export class AppModule {}
