70cd9f295eaadd8f988f92fb45ec8948d56b903e
specter01wj <specter01wj@gmail.com>
2019-04-20 23:07:09 -0600

    04/17/2019 v1

--- Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/01-angular-2-in-action/3. AngularJS Structural Directives/vehicles.controller.js
+++ Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/01-angular-2-in-action/3. AngularJS Structural Directives/vehicles.controller.js
@@ -5,8 +5,9 @@
 
   function VehiclesController() {
     var vm = this;
-    
+
     vm.name = 'World';
+    
     vm.vehicles = [
       { id: 1, name: 'X-Wing Fighter' },
       { id: 2, name: 'Tie Fighter' },
