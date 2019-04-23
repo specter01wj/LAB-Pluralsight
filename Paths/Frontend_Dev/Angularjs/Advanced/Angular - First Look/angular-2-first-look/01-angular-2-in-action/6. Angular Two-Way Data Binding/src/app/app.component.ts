a216f61dec103ae5cb2582a0a412b91387ad213d
specter01wj <specter01wj@gmail.com>
2019-04-23 18:12:00 -0400

    04/17/2019 v1

--- Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/01-angular-2-in-action/6. Angular Two-Way Data Binding/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/01-angular-2-in-action/6. Angular Two-Way Data Binding/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent {
   title = 'Angular Two-Way Binding';
   story = {
