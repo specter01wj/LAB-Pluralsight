998e0b654ff06b6ec7a8a4065dc45f8c99669b8f
specter01wj <specter01wj@gmail.com>
2019-04-25 12:32:02 -0400

    04/25/2019 v1

--- Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/01-angular-2-in-action/9. AngularJS Event Binding/vehicles.controller.js
+++ Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/01-angular-2-in-action/9. AngularJS Event Binding/vehicles.controller.js
@@ -5,14 +5,17 @@
 
   function VehiclesController() {
     var vm = this;
+
     vm.title = 'Angular 1 Binding Events';
     vm.imagePath = 'angular.png';
     vm.messages = [];
+
     vm.vehicles = [
       { id: 1, name: 'X-Wing Fighter' },
       { id: 2, name: 'Tie Fighter' },
       { id: 3, name: 'Y-Wing Fighter' }
     ];
+
     vm.log = function (msg, data) {
       vm.messages.splice(0, 0, msg);
       console.log(msg);
@@ -20,6 +23,7 @@
         console.log(data);
       }
     }
+    
   }
 })();
 
