281f2fb6cc1ef8fe2157ade363cd22d1f94dd2ae
specter01wj <specter01wj@gmail.com>
2019-05-01 01:35:15 -0400

    04/10/2019 v1

--- Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/06-data-with-http/20. Angular - Needs Http/src/app/vehicle.service.ts
+++ Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/06-data-with-http/20. Angular - Needs Http/src/app/vehicle.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 export class Vehicle {
   constructor(public id: number, public name: string) {}
@@ -6,11 +7,17 @@ export class Vehicle {
 
 @Injectable()
 export class VehicleService {
-  getVehicles() {
-    return [
+	constructor(private http: Http) {
+
+	}
+
+  	getVehicles() {
+  		return this.http.get('api/vehicles.json')
+  			.map((response: Response) => <Vehicle[]>response.json())
+    /*return [
       new Vehicle(1, 'X-Wing Fighter'),
       new Vehicle(2, 'B-Wing Fighter'),
       new Vehicle(3, 'Y-Wing Fighter')
-    ];
-  }
+    ];*/
+  	}
 }
