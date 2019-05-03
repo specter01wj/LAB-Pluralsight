983f1421a8236dc0746f26795ba5a89bba5cae4a
specter01wj <specter01wj@gmail.com>
2019-05-02 23:41:53 -0400

    05/02/2019 v1

--- Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/07-routing/25. Angular - Router/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angularjs/Advanced/Angular - First Look/angular-2-first-look/07-routing/25. Angular - Router/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule, routableComponents } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CharacterService } from './characters/character.service';
 import { VehicleService } from './vehicles/vehicle.service';
