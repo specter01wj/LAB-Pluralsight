36aa59c9972fa8ca5fe54d0b8250789db4792160
specter01wj <specter01wj@gmail.com>
2019-05-06 00:06:57 -0400

    05/05/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/05/demos/APM/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/05/demos/APM/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `
+  /*template: `
     <div><h1>{{pageTitle}}</h1>
         <pm-products></pm-products>
     </div>
-    `
+    `*/
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent {
