c4f5035addd8ce4513b075d508327ff91a232bdc
specter01wj <specter01wj@gmail.com>
2019-05-05 21:07:40 -0400

    05/05/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/04/demos/APM/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/04/demos/APM/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  template: `
-    <div><h1>{{pageTitle}}</h1>
+	selector: "pm-root",
+	template: `<div><h1>{{pageTitle}}</h1>
         <div>My First Component</div>
-    </div>
-    `
+    </div>`
 })
 
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
+
+	pageTitle: string = "James Wang Product Management!!!";
 }
