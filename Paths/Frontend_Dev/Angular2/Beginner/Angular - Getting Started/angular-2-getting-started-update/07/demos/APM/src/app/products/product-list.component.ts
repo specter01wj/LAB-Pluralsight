238049d2683f44d317e2e107064d8f233e4e1f60
specter01wj <specter01wj@gmail.com>
2019-05-14 23:59:16 -0400

    05/12/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/07/demos/APM/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/07/demos/APM/src/app/products/product-list.component.ts
@@ -56,6 +56,7 @@ export class ProductListComponent implements OnInit {
         return this.products.filter((product: IProduct) =>
               product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
+
     
 
     toggleImage(): void {
