8f51d2112bb81e9eb193481d3cd0f3f01fe94021
specter01wj <specter01wj@gmail.com>
2019-06-10 23:05:07 -0600

    05/31/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/10/demos/APM/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/10/demos/APM/src/app/products/product-list.component.ts
@@ -8,6 +8,7 @@ import { ProductService } from './product.service';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
