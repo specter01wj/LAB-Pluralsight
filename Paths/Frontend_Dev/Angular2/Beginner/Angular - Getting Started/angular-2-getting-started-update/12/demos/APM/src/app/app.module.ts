9cc8f79a731ac6b4b2a3d2f7f43111af67f0ec2e
specter01wj <specter01wj@gmail.com>
2019-06-13 22:35:42 -0600

    06/13/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/12/demos/APM/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/12/demos/APM/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+
 import { ProductGuardService } from './products/product-guard.service';
 
 @NgModule({
