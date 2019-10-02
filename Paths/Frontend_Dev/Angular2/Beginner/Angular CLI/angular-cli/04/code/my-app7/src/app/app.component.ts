328e4aac8b46c27c5728e1cb1087e0c53cb13a63
specter01wj <specter01wj@gmail.com>
2019-10-02 15:35:20 -0400

    09/05/2019 v1

---
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular CLI/angular-cli/04/code/my-app7/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'my-app7';
+}
