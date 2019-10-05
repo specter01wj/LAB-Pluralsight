f2770368b9c4042387f641ad207f5fb1e19a2691
specter01wj <specter01wj@gmail.com>
2019-10-05 03:12:30 -0400

    09/05/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular CLI/angular-cli/06/mysol_1/angular-routing/src/app/admin/admin.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular CLI/angular-cli/06/mysol_1/angular-routing/src/app/admin/admin.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { EmailBlastComponent } from './email-blast/email-blast.component';
+import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
-  declarations: [AdminComponent, EmailBlastComponent],
+  declarations: [AdminComponent, EmailBlastComponent, UsersComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
