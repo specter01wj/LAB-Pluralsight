02033347cb3e0b483cfe8f0c1274fdd830470ae8
specter01wj <specter01wj@gmail.com>
2019-10-05 02:55:17 -0400

    09/05/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular CLI/angular-cli/06/mysol_1/angular-routing/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular CLI/angular-cli/06/mysol_1/angular-routing/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
