66e8b75d1eab31abb3802414834cb3473abff929
specter01wj <specter01wj@gmail.com>
2019-10-08 18:55:36 -0400

    09/05/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular CLI/angular-cli/09/mysol_1/angular-routing/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular CLI/angular-cli/09/mysol_1/angular-routing/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { LoggerService } from 'my-lib';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-routing';
 
