bf6f31e01be7e651ddc84baa3e1efce2fa5106e0
specter01wj <specter01wj@gmail.com>
2019-11-21 00:00:15 -0700

    11/18/2019 v1

--- Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/5/mycode/CalcEngine_ch5/src/com/spm/ch5/CalcEngine/CalcEngine.java
+++ Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/5/mycode/CalcEngine_ch5/src/com/spm/ch5/CalcEngine/CalcEngine.java
@@ -4,10 +4,12 @@ public class CalcEngine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
+//		double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+//        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+//        char[] opCodes = {'d', 'a', 's', 'm'};
+//        double[] results = new double[opCodes.length];
+		
+		MathEquation[] equations = new MathEquation[4];
 
         for(int i = 0; i < opCodes.length; i++) {
             
