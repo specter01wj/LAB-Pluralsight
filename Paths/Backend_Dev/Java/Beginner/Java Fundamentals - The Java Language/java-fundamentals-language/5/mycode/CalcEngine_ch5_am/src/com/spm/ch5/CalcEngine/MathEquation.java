ebabb036026f54a2a5e0255a6ade9cdf3e0c4b98
specter01wj <specter01wj@gmail.com>
2019-11-21 02:52:29 -0700

    11/18/2019 v1

--- Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/5/mycode/CalcEngine_ch5_am/src/com/spm/ch5/CalcEngine/MathEquation.java
+++ Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/5/mycode/CalcEngine_ch5_am/src/com/spm/ch5/CalcEngine/MathEquation.java
@@ -2,11 +2,20 @@ package com.spm.ch5.CalcEngine;
 
 public class MathEquation {
 
-	public double leftVal;
-	public double rightVal;
-	public char opCode;
-	public double result;
+	private double leftVal;
+	private double rightVal;
+	private char opCode;
+	private double result;
     
+	public double getLeftVal() {return leftVal;}
+    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+    public double getRightVal() {return rightVal;}
+    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+    public char getOpCode() {return opCode;}
+    public void setOpCode(char opCode) {this.opCode = opCode;}
+
+    public double getResult() { return result;}
+	
     public void execute() {
 	    	switch(opCode) {
 	        case 'a':
