edc8a0e193d4ff390be4143758c9f35654becab6
specter01wj <specter01wj@gmail.com>
2019-11-22 00:09:23 -0700

    11/18/2019 v1

--- Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/7/mycode/CalcEngine_ch7/src/com/spm/ch7/calcengine/CalcEngine.java
+++ Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/7/mycode/CalcEngine_ch7/src/com/spm/ch7/calcengine/CalcEngine.java
@@ -4,7 +4,17 @@ public class CalcEngine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MathEquation[] equations = new MathEquation[4];
+        equations[0] = new MathEquation(100.0d, 50.0d, 'd');
+        equations[1] = new MathEquation(25.0d, 92.0d, 'a');
+        equations[2] = new MathEquation(225.0d, 17.0d, 's');
+        equations[3] = new MathEquation(11.0d, 3.0d, 'm');
 
+        for(MathEquation equation : equations) {
+            equation.execute();
+            System.out.print("result = ");
+            System.out.println(equation.getResult());
+        }
 	}
 
 }
---
+++ Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/7/mycode/CalcEngine_ch7/src/com/spm/ch7/calcengine/MathEquation.java
@@ -0,0 +1,52 @@
+package com.spm.ch7.calcengine;
+
+public class MathEquation {
+	private double leftVal;
+    private double rightVal;
+    private char opCode = 'a';
+    private double result;
+    
+    public MathEquation() {}
+    
+    public MathEquation(char opCode) {
+    		this.opCode = opCode;
+    }
+    
+    public MathEquation(double leftVal, double rightVal, char opCode) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+
+    public double getLeftVal() {return leftVal;}
+    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+    public double getRightVal() {return rightVal;}
+    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+    public char getOpCode() {return opCode;}
+    public void setOpCode(char opCode) {this.opCode = opCode;}
+
+    public double getResult() { return result;}
+
+    public void execute() {
+    	
+        switch(opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            default:
+                System.out.println("Error - invalid opCode");
+                result = 0.0d;
+                break;
+        }
+
+    }
+}
