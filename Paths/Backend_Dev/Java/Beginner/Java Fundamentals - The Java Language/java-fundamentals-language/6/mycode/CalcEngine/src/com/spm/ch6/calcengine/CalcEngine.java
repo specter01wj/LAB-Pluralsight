c9b768d1bee15e655e83616ede34f79dfb70cb92
specter01wj <specter01wj@gmail.com>
2019-11-21 20:47:41 -0700

    11/18/2019 v1

--- Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/6/mycode/CalcEngine/src/com/spm/ch6/calcengine/CalcEngine.java
+++ Paths/Backend_Dev/Java/Beginner/Java Fundamentals - The Java Language/java-fundamentals-language/6/mycode/CalcEngine/src/com/spm/ch6/calcengine/CalcEngine.java
@@ -4,17 +4,23 @@ public class CalcEngine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+		MathEquation testEquation = new MathEquation();
+	    testEquation.execute();
+	    System.out.print("test=");
+	    System.out.println(testEquation.getResult());
+		
+		
+//		MathEquation[] equations = new MathEquation[4];
+//        equations[0] = create(100.0d, 50.0d, 'd');
+//        equations[1] = create(25.0d, 92.0d, 'a');
+//        equations[2] = create(225.0d, 17.0d, 's');
+//        equations[3] = create(11.0d, 3.0d, 'm');
 
-        for(MathEquation equation : equations) {
-            equation.execute();
-            System.out.print("result = ");
-            System.out.println(equation.getResult());
-        }
+//        for(MathEquation equation : equations) {
+//            equation.execute();
+//            System.out.print("result = ");
+//            System.out.println(equation.getResult());
+//        }
 	}
 	
 	public static MathEquation create(double leftVal, double rightVal, char opCode) {
