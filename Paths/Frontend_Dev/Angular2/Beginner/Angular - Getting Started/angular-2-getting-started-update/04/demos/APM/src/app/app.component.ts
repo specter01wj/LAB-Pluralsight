9613881885948349e9a033f66f680fb7b37fd615
specter01wj <specter01wj@gmail.com>
2019-12-26 10:21:08 -0500

    12/25/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/04/demos/APM/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/04/demos/APM/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
 	selector: "pm-root",
 	template: `<div><h1>{{pageTitle}}</h1>
-        <div>My {{myNum}} Component</div>
-    </div>`
+        		<div>My {{myNum}} Component</div>
+    			</div>`
 })
 
 export class AppComponent {
 
-	
+
 	pageTitle: string = "James Wang Product Management!!!";
 	myNum: number = 23;
 }
