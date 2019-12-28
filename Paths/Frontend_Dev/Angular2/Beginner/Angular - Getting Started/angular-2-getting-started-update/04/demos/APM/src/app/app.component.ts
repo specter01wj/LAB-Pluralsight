69c8459a4db9390530d4f320d5a5b7d15afa1da1
specter01wj <specter01wj@gmail.com>
2019-12-28 13:38:44 -0500

    12/27/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/04/demos/APM/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/04/demos/APM/src/app/app.component.ts
@@ -2,22 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: "pm-root",
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
 	template: `<div><h1>{{pageTitle}}</h1>
         		<div>My {{myNum}} Component</div>
     			</div>`
