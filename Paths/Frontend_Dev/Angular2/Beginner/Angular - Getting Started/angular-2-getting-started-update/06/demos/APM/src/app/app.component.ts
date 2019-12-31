2a0be146d9fb50fa45c1ac021b73eaceda2defc4
specter01wj <specter01wj@gmail.com>
2019-12-31 17:56:11 -0500

    12/27/2019 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/06/demos/APM/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/06/demos/APM/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
     `
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
+  	pageTitle: string = 'Acme Product Management';
 }
