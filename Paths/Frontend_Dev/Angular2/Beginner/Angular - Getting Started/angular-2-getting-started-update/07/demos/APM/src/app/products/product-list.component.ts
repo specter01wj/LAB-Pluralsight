b7b94173bc8e433f6d42f7a39c616a0b80c0f07e
specter01wj <specter01wj@gmail.com>
2020-01-04 04:16:18 -0500

    01/03/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/07/demos/APM/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/07/demos/APM/src/app/products/product-list.component.ts
@@ -51,14 +51,6 @@ export class ProductListComponent implements OnInit {
 
     constructor() {
         this.filteredProducts = this.products;
-
-
-
-
-
-
-
-        
         this.listFilter = 'cart';
     }
 
