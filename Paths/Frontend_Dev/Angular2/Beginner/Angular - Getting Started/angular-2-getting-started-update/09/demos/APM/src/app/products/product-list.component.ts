e73934f7c954045a6eab2a6bd187b766b0827154
specter01wj <specter01wj@gmail.com>
2020-01-07 23:40:12 -0500

    01/06/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/09/demos/APM/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/09/demos/APM/src/app/products/product-list.component.ts
@@ -24,6 +24,7 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
+    
     products: IProduct[] = [];
 
     constructor(private _productService: ProductService) {
