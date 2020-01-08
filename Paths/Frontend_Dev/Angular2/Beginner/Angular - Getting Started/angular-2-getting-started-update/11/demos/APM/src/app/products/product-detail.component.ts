75353d6b4544261b727826923142ec3c3d7ef28c
specter01wj <specter01wj@gmail.com>
2020-01-08 18:49:10 -0500

    01/08/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/11/demos/APM/src/app/products/product-detail.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/11/demos/APM/src/app/products/product-detail.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+	selector: 'app-product-detail',
+  	templateUrl: './product-detail.component.html',
+  	styleUrls: ['./product-detail.component.css']
 })
 
 export class ProductDetailComponent implements OnInit {
