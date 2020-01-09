bb2ee54623ff4b59b69372c83c928791d1bfc887
specter01wj <specter01wj@gmail.com>
2020-01-08 19:14:05 -0500

    01/08/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/11/demos/APM/src/app/products/product-detail.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/11/demos/APM/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -8,14 +9,24 @@ import { IProduct } from './product';
   	styleUrls: ['./product-detail.component.css']
 })
 
-export class ProductDetailComponent implements OnInit {
-
+export class ProductDetailComponent implements OnInit, OnDestroy {
+	productId: number;
+	private sub: any;
     pageTitle: string = 'Product Detail';
     product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	this.sub = this.route.params.subscribe(params => {
+       this.productId = params['productId']; // (+) converts string 'id' to a number
+
+       // In a real app: dispatch action to load the details here.
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
