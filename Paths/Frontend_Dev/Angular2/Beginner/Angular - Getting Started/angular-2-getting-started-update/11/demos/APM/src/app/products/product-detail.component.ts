b0fa8860b1a567976efc78f78798383ae277bf8f
specter01wj <specter01wj@gmail.com>
2020-01-08 20:43:29 -0500

    01/08/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/11/demos/APM/src/app/products/product-detail.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update/11/demos/APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {Location} from '@angular/common';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
@@ -19,7 +20,12 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
     product: IProduct;
 
   	constructor(private route: ActivatedRoute, 
-  				private _productService: ProductService) { }
+  				private _productService: ProductService,
+  				private _location: Location) { }
+
+  	backClicked() {
+	    this._location.back();
+	}
 
 	ngOnInit() {
 	  	this.sub = this.route.params.subscribe(params => {
@@ -27,7 +33,13 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
 
         	this._productService.getProducts()
                 .subscribe(products => {
-                    console.log(products);
+                    // console.log(products);
+                    for(var i = 0; i <= products.length; i++) {
+                    	if(products[i].productId == this.productId) {
+                    		this.product = products[i];
+                    		break;
+                    	}
+                    }
                 },
                     error => this.errorMessage = <any>error);
 	    });
