70aaf8b8bea81a5d81553060bb80f4b27b7d08b6
specter01wj <specter01wj@gmail.com>
2020-01-09 23:25:02 -0500

    01/06/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-2020update/13/demos/src/app/products/product.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-2020update/13/demos/src/app/products/product.module.ts
@@ -5,7 +5,6 @@ import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-
 import { ProductDetailGuard } from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
 
