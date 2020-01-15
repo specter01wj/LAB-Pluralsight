49ab0bda458832594850c1642463cce775eecbfd
specter01wj <specter01wj@gmail.com>
2020-01-14 19:32:50 -0500

    01/14/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/03/demos/02-CreatingAndCommunicatingBetweenAngularComponents/c3-create-component/src/app/event-thumbnail/event-thumbnail.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/03/demos/02-CreatingAndCommunicatingBetweenAngularComponents/c3-create-component/src/app/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-thumbnail',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventThumbnailComponent implements OnInit {
 	@Input() event:any
-	
+
   constructor() { }
 
   ngOnInit() {
