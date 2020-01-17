f4fdb93db3b28c83dbf6cdaf55252cca8b2dc759
specter01wj <specter01wj@gmail.com>
2020-01-17 11:56:53 -0700

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/04/demos/03-CreatingReusableAngularServices/c4-template-syntax/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/04/demos/03-CreatingReusableAngularServices/c4-template-syntax/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsListComponent } from './events-list/events-list.component';
+import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule,
