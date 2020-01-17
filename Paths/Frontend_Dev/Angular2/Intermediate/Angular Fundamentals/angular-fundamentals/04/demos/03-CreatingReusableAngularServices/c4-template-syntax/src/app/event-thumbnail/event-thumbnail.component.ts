154a73acf56071d46a44dc50bb87e827163b21ca
specter01wj <specter01wj@gmail.com>
2020-01-17 12:02:37 -0700

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/04/demos/03-CreatingReusableAngularServices/c4-template-syntax/src/app/event-thumbnail/event-thumbnail.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/04/demos/03-CreatingReusableAngularServices/c4-template-syntax/src/app/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-thumbnail',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventThumbnailComponent implements OnInit {
 
+	@Input() event:any
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+   
+
+  getStartTimeStyle():any {
+    if (this.event && this.event.time === '8:00 am')
+      return {color: '#003300', 'font-weight': 'bold'}
+    return {}
   }
 
 }
