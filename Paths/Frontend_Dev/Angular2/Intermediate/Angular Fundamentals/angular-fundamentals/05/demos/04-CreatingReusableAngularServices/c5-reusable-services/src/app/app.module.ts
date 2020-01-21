a14fb8958f61aeb8a63ec475b0d769ced3b9872b
specter01wj <specter01wj@gmail.com>
2020-01-20 22:23:04 -0700

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/05/demos/04-CreatingReusableAngularServices/c5-reusable-services/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/05/demos/04-CreatingReusableAngularServices/c5-reusable-services/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { EventsListComponent } from './events-list/events-list.component';
+import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule,
