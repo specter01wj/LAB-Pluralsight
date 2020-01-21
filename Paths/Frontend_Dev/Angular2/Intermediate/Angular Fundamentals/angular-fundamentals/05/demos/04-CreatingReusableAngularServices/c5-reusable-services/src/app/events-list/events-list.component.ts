77a71338f1458307296471f134fe615e11c12944
specter01wj <specter01wj@gmail.com>
2020-01-21 00:02:51 -0700

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/05/demos/04-CreatingReusableAngularServices/c5-reusable-services/src/app/events-list/events-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/05/demos/04-CreatingReusableAngularServices/c5-reusable-services/src/app/events-list/events-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EventService } from './service/event.service'
+import { ToastrService } from '../service/toastr.service'
+
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html',
   styleUrls: ['./events-list.component.less']
 })
 export class EventsListComponent implements OnInit {
+  events:any[]
 
-  constructor() { }
+  constructor(private eventService: EventService, private toastr: ToastrService) {
+    
+  }
 
   ngOnInit() {
+    this.events = this.eventService.getEvents()
+  }
+
+  handleThumbnailClick(eventName) {
+    this.toastr.success(eventName)
   }
 
 }
