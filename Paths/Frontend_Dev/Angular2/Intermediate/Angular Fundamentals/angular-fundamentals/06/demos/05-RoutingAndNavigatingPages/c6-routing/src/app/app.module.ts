41034572536914dd18c6728c0bb0c39ae4b897fe
specter01wj <specter01wj@gmail.com>
2020-01-22 12:06:16 -0700

    01/22/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/06/demos/05-RoutingAndNavigatingPages/c6-routing/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/06/demos/05-RoutingAndNavigatingPages/c6-routing/src/app/app.module.ts
@@ -3,12 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProfileComponent } from './user/profile.component';
-import { EventsListComponent } from './events/events-list/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event/create-event.component';
+
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventsListResolverService
+} from './events/index';
+
+// import { EventsListComponent } from './events/events-list/events-list.component';
+// import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
+// import { EventDetailsComponent } from './events/event-details/event-details.component';
+// import { CreateEventComponent } from './events/create-event/create-event.component';
+import { E404Component } from './errors/e404.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +31,8 @@ import { CreateEventComponent } from './events/create-event/create-event.compone
     EventsListComponent,
     EventThumbnailComponent,
     EventDetailsComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    E404Component
   ],
   imports: [
     BrowserModule,
---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/06/demos/05-RoutingAndNavigatingPages/c6-routing/src/app/events/index.ts
@@ -0,0 +1,7 @@
+export * from './create-event/create-event.component';
+export * from './event-thumbnail/event-thumbnail.component';
+export * from './events-list/events-list.component';
+
+export * from './service/index';
+
+export * from './event-details/index';
