57230a602665ea42ef50e60b06943296cc1e07ed
specter01wj <specter01wj@gmail.com>
2020-01-24 00:37:25 -0700

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/login.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
+  username
+  password
+  mouseoverLogin
 
-  constructor() { }
+
+  constructor(private authService:AuthService, private router:Router) { }
 
   ngOnInit() {
+  }
+
+  login(formValues) {
+    this.authService.loginUser(formValues.userName, formValues.password);
+    this.router.navigate(['events']);
+  }
+
+  cancel() {
+    this.router.navigate(['events'])
   }
 
 }
