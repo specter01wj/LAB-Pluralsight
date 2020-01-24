955f437c2643238a8ae863fc1b59bc4e343659c2
specter01wj <specter01wj@gmail.com>
2020-01-24 00:54:06 -0700

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/user-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/user-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './login.component';
 
 const routes: Routes = [
-	{path: 'profile', component: ProfileComponent}
+	{path: 'profile', component: ProfileComponent},
+	{path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/user.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/user.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { ProfileComponent } from './profile.component';
-import { LoginComponent } from './login.component'
+import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { LoginComponent } from './login.component'
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class UserModule { }
