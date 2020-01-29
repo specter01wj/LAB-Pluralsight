da67872a6244f7c26fe924aa06f303eac2487f5d
specter01wj <specter01wj@gmail.com>
2020-01-29 18:57:20 -0500

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/profile.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/profile.component.ts
@@ -11,6 +11,13 @@ export class ProfileComponent implements OnInit {
   constructor(private _router: Router) { }
 
   ngOnInit() {
+  	this.firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
+    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
+
+    this.profileForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName
+    })
   }
 
   onCancel(): void {
