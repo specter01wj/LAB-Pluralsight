4a0e977ce549030856384e8286ee20ba28084d07
specter01wj <specter01wj@gmail.com>
2020-01-29 23:55:36 -0500

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/profile.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/user/profile.component.ts
@@ -20,7 +20,7 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
   	this.firstName = new FormControl(
   		this.authService.currentUser.firstName, 
-  		// [Validators.required, Validators.pattern('[a-zA-Z].*')]
+  		[Validators.required, Validators.pattern('[a-zA-Z].*')]
   	);
     
     this.lastName = new FormControl(
