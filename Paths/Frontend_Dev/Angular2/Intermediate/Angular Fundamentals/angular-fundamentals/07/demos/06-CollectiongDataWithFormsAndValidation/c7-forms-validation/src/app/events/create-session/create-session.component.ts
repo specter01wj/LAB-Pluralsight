97a6c986900f9956ff5f9ac44764a79cb8fd87df
specter01wj <specter01wj@gmail.com>
2020-01-31 03:52:51 -0500

    01/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/events/create-session/create-session.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/07/demos/06-CollectiongDataWithFormsAndValidation/c7-forms-validation/src/app/events/create-session/create-session.component.ts
@@ -9,12 +9,12 @@ import { ISession, restrictedWords } from '../service/index';
   styleUrls: ['./create-session.component.less']
 })
 export class CreateSessionComponent implements OnInit {
-  newSessionForm: FormGroup
-  name: FormControl
-  presenter: FormControl
-  duration: FormControl
-  level: FormControl
-  abstract: FormControl
+  newSessionForm: FormGroup;
+  name: FormControl;
+  presenter: FormControl;
+  duration: FormControl;
+  level: FormControl;
+  abstract: FormControl;
 
   constructor() { }
 
