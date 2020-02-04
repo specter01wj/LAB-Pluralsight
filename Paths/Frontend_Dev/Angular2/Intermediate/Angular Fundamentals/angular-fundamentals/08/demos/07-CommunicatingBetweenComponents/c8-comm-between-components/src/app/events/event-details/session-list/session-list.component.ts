0177a2e7972e86fdd38d55921eda3984d9ed2f5d
specter01wj <specter01wj@gmail.com>
2020-02-03 20:19:40 -0500

    02/03/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/08/demos/07-CommunicatingBetweenComponents/c8-comm-between-components/src/app/events/event-details/session-list/session-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/08/demos/07-CommunicatingBetweenComponents/c8-comm-between-components/src/app/events/event-details/session-list/session-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ISession } from '../../service/index';
 
 @Component({
   selector: 'app-session-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./session-list.component.less']
 })
 export class SessionListComponent implements OnInit {
+	@Input() sessions:ISession[];
+
 
   constructor() { }
 
