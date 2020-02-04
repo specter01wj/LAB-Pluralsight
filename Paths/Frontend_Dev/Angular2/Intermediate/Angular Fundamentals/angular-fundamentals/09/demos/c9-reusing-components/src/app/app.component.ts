3a4d012aa27d68f0364ee51d3754884cfb985ed8
specter01wj <specter01wj@gmail.com>
2020-02-04 18:40:42 -0500

    02/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/09/demos/c9-reusing-components/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/09/demos/c9-reusing-components/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'c8-comm-between-components';
+  title = 'c90-reusing-components';
 }
