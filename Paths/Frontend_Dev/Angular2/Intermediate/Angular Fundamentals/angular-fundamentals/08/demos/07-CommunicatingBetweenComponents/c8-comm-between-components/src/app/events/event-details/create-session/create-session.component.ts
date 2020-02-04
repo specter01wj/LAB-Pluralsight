db031791f7bbd11c23d58363cba3d3ece0c09580
specter01wj <specter01wj@gmail.com>
2020-02-03 20:14:00 -0500

    02/03/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/08/demos/07-CommunicatingBetweenComponents/c8-comm-between-components/src/app/events/event-details/create-session/create-session.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/08/demos/07-CommunicatingBetweenComponents/c8-comm-between-components/src/app/events/event-details/create-session/create-session.component.ts
@@ -22,6 +22,19 @@ export class CreateSessionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	this.name = new FormControl('', Validators.required)
+    this.presenter = new FormControl('', Validators.required)
+    this.duration = new FormControl('', Validators.required)
+    this.level = new FormControl('', Validators.required)
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])])
+
+    this.newSessionForm = new FormGroup({
+      name: this.name,
+      presenter: this.presenter,
+      duration: this.duration,
+      level: this.level,
+      abstract: this.abstract
+    })
   }
 
 }
