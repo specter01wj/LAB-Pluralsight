d4b5fe548a6a4cba4a7653f9cc1ba8d87f1aebb7
specter01wj <specter01wj@gmail.com>
2020-02-03 20:13:13 -0500

    02/03/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/08/demos/07-CommunicatingBetweenComponents/c8-comm-between-components/src/app/events/event-details/create-session/create-session.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/08/demos/07-CommunicatingBetweenComponents/c8-comm-between-components/src/app/events/event-details/create-session/create-session.component.ts
@@ -11,12 +11,12 @@ export class CreateSessionComponent implements OnInit {
   @Output() saveNewSession = new EventEmitter();
   @Output() cancelAddSession = new EventEmitter();
 
-  newSessionForm: FormGroup
-  name: FormControl
-  presenter: FormControl
-  duration: FormControl
-  level: FormControl
-  abstract: FormControl
+  newSessionForm: FormGroup;
+  name: FormControl;
+  presenter: FormControl;
+  duration: FormControl;
+  level: FormControl;
+  abstract: FormControl;
 
 
   constructor() { }
