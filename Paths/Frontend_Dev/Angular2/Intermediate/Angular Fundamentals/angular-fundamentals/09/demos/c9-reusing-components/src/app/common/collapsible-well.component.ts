be188617dd23dc7560d20eff4eccbfaf18ed13bd
specter01wj <specter01wj@gmail.com>
2020-02-05 19:32:04 -0500

    02/05/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/09/demos/c9-reusing-components/src/app/common/collapsible-well.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/09/demos/c9-reusing-components/src/app/common/collapsible-well.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CollapsibleWellComponent implements OnInit {
 	@Input() title: string;
 
+	visible: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
