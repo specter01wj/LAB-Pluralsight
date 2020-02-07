d6972b5c2300415760e19c3a84aa8550ed0cfc7f
specter01wj <specter01wj@gmail.com>
2020-02-06 20:26:31 -0500

    02/06/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/10/demos/c10-data-pipes/src/app/events/event-details/session-list/session-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/10/demos/c10-data-pipes/src/app/events/event-details/session-list/session-list.component.ts
@@ -37,9 +37,13 @@ export class SessionListComponent implements OnInit {
 }
 
 function sortByNameAsc(s1: ISession, s2: ISession) {
-  if(s1.name > s2.name) return 1
-  else if(s1.name === s2.name) return 0
-  else return -1
+  if(s1.name > s2.name) {
+  	return 1;
+  } else if(s1.name === s2.name) {
+  	return 0;
+  } else {
+  	return -1;
+  }
 }
 
 function sortByVotesDesc(s1: ISession, s2: ISession) {
