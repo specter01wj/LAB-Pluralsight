61b6c368dc285a7dc188721f35c0504c09643ab5
specter01wj <specter01wj@gmail.com>
2020-02-06 20:10:38 -0500

    02/06/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/10/demos/c10-data-pipes/src/app/events/event-details/session-list/session-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/10/demos/c10-data-pipes/src/app/events/event-details/session-list/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { ISession } from '../../service/index';
 
 @Component({
@@ -7,12 +7,31 @@ import { ISession } from '../../service/index';
   styleUrls: ['./session-list.component.less']
 })
 export class SessionListComponent implements OnInit {
-	@Input() sessions:ISession[];
-
+  @Input() sessions:ISession[];
+  @Input() filterBy: string;
+  @Input() sortBy: string;
+  visibleSessions: ISession[] = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(filter) {
+    if(this.sessions) {
+      this.filterSessions(this.filterBy);
+      // this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
+    }
+  }
+
+  filterSessions(filter) {
+    if(filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      })
+    }
   }
 
 }
