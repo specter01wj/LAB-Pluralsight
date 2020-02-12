c4f71c735b68cf3f66b975e50365bcdf4c623bca
specter01wj <specter01wj@gmail.com>
2020-02-12 18:24:00 -0500

    02/12/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/12/demos/c12-directive-component/src/app/events/service/event.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/12/demos/c12-directive-component/src/app/events/service/event.service.ts
@@ -33,6 +33,26 @@ export class EventService {
     EVENTS[index] = event;
   }
 
+  searchSessions(searchTerm: string) {
+    var term = searchTerm.toLocaleLowerCase();
+    var results: ISession[] = [];
+
+    EVENTS.forEach(event => {
+      var matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
+      matchingSessions = matchingSessions.map((session:any) => {
+        session.eventId = event.id;
+        return session;
+      })
+      results = results.concat(matchingSessions);
+    })
+
+    var emitter = new EventEmitter(true);
+    setTimeout(() => {
+      emitter.emit(results);
+    }, 100);
+    return emitter;
+  }
+
 }
 
 const EVENTS: IEvent[] = 
