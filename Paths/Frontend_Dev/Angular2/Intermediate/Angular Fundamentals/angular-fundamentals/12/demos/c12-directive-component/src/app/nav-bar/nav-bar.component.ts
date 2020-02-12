ed15a415fc7628b2b1f2381a6fff8b9e33dcbdfc
specter01wj <specter01wj@gmail.com>
2020-02-12 18:20:07 -0500

    02/12/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/12/demos/c12-directive-component/src/app/nav-bar/nav-bar.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/12/demos/c12-directive-component/src/app/nav-bar/nav-bar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../user/auth.service';
 
+import { ISession, EventService } from '../events/index';
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
