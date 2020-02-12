0a4e9828d04c6c1e5002f2b63855ce7a3cdd1887
specter01wj <specter01wj@gmail.com>
2020-02-12 18:24:17 -0500

    02/12/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/12/demos/c12-directive-component/src/app/events/service/event.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Fundamentals/angular-fundamentals/12/demos/c12-directive-component/src/app/events/service/event.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
 import { IEvent } from './event.model';
 
