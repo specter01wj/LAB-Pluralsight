57a1dd206e101e551b54396f07c37f4b99c406b8
specter01wj <specter01wj@gmail.com>
2020-02-19 18:43:17 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch4_module/ch4_code5_properties.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch4_module/ch4_code5_properties.js
@@ -1,147 +1,26 @@
 'use strict';
 
-/*class Project {
+class Project {
 	constructor() {
-		console.log('constructing Project');
-	}
-}
-
-class SoftwareProject extends Project {
-
-}
-
-let p = new SoftwareProject();*/
-
-
-
-/*class Project {
-	constructor(name) {
-		console.log('constructing Project: ' + name);
-	}
-}
-
-class SoftwareProject extends Project {
-
-}
-
-let p = new SoftwareProject('Maza');*/
-
-
-
-/*class Project {
-	constructor() {
-		console.log('constructing Project');
+		this.location = 'Maza';
 	}
 }
 
 class SoftwareProject extends Project {
 	constructor() {
 		super();
-		console.log('constructing SoftwareProject');
-	}
-}
-
-let p = new SoftwareProject();*/
-
-
-
-/*class Project {
-	constructor() {
-		console.log('constructing Project');
-	}
-}
-
-class SoftwareProject extends Project {
-	constructor() {
-		// super();
-		console.log('constructing SoftwareProject');
-	}
-}
-
-let p = new SoftwareProject();*/
-
-
-
-/*class Project {
-	// constructor() {
-	// 	console.log('constructing Project');
-	// }
-}
-
-class SoftwareProject extends Project {
-	constructor() {
-		// super();
-		console.log('constructing SoftwareProject');
-	}
-}
-
-let p = new SoftwareProject();*/
-
-
-
-/*class Project {
-	getTaskCount() {
-		return 50;
-	}
-}
-
-class SoftwareProject extends Project {
-	
-}
-
-let p = new SoftwareProject();
-console.log(p.getTaskCount());*/
-
-
-
-/*class Project {
-	getTaskCount() {
-		return 50;
-	}
-}
-
-class SoftwareProject extends Project {
-	getTaskCount() {
-		return 66;
 	}
 }
 
 let p = new SoftwareProject();
-console.log(p.getTaskCount());*/
+console.log(p.location);
 
 
 
-/*class Project {
-	getTaskCount() {
-		return 50;
-	}
-}
-
-class SoftwareProject extends Project {
-	getTaskCount() {
-		return super.getTaskCount() + 6;
-	}
-}
-
-let p = new SoftwareProject();
-console.log(p.getTaskCount());*/
 
 
 
-let project = {
-	getTaskCount() {
-		return 50;
-	}
-}
 
-let softwareProject = {
-	getTaskCount() {
-		return super.getTaskCount() + 7;
-	}
-}
-
-Object.setPrototypeOf(softwareProject, project);
-console.log(softwareProject.getTaskCount());
 
 
 
