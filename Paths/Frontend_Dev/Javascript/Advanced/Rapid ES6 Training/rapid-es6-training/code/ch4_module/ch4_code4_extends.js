a4ad78fab383468fa92c7d8b69a9a869147b611d
specter01wj <specter01wj@gmail.com>
2020-02-19 03:37:36 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch4_module/ch4_code4_extends.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch4_module/ch4_code4_extends.js
@@ -1,105 +1,48 @@
 'use strict';
 
-/*class Task {
-
-}
-
-console.log(typeof Task);
-
-
-
-let task = new Task();
-console.log(typeof task);
-
-
-
-console.log(task instanceof Task);*/
-
-
-
-/*class Task {
-	showId() {
-		console.log('99');
+/*class Project {
+	constructor() {
+		console.log('constructing Project');
 	}
 }
-let task = new Task();
-task.showId();
 
-console.log(task.showId ===  Task.prototype.showId);*/
+class SoftwareProject extends Project {
 
-
-
-/*class Task {
-	constructor() {
-		console.log('constructing Task');
-	};
-	showId() {
-		console.log('99');
-	};
-}
-let task = new Task();*/
-
-
-
-/*class Task {
-	let taskId = 9000;
-	constructor() {
-		console.log('constructing Task');
-	};
-	showId() {
-		console.log('99');
-	};
-}
-let task = new Task();*/
-
-
-
-/*let task = new Task();
-class Task {
-	constructor() {
-		console.log('constructing Task');
-	};
-}*/
-
-
-
-/*let newClass = class Task {
-	constructor() {
-		console.log('constructing Task');
-	};
 }
 
-new newClass();*/
+let p = new SoftwareProject();*/
 
 
 
-/*let Task = function() {
-	console.log('constructing Task');
-};
-let task = {};
-Task.call(task);*/
-
-
-
-/*class Task {
-	constructor() {
-		console.log('constructing Task');
+class Project {
+	constructor(name) {
+		console.log('constructing Project: ' + name);
 	}
-};
-let task = {};
-Task.call(task);*/
+}
+
+class SoftwareProject extends Project {
+
+}
+
+let p = new SoftwareProject('Maza');
 
 
 
-/*function Project() {};
-
-console.log(window.Project === Project);*/
 
 
 
-class Task {};
 
-console.log(window.Task === Task);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
