1bb421c69b17244a7e72eb0cfca9b4cfcdc87543
specter01wj <specter01wj@gmail.com>
2020-02-20 13:40:15 -0500

    02/20/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code2_wellknown.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code2_wellknown.js
@@ -23,6 +23,18 @@ console.log([].concat(values));
 
 
 
+let values2 = [8, 12, 16];
+values2[Symbol.isConcatSpreadable] = false;
+console.log([].concat(values2));
+
+
+
+let values3 = [8, 12, 16];
+let sum = values3 + 100;
+console.log(sum);
+
+
+
 
 
 
