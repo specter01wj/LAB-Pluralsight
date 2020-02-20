619843b9b4ccaa5d9d2cc225fb3f6c73745bc139
specter01wj <specter01wj@gmail.com>
2020-02-20 13:34:05 -0500

    02/20/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code2_wellknown.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code2_wellknown.js
@@ -9,6 +9,15 @@ console.log(blog.toString());
 
 
 
+let Blog2 = function() {
+
+};
+Blog2.prototype[Symbol.toStringTag] = 'Blog Class';
+let blog2 = new Blog2();
+console.log(blog2.toString());
+
+
+
 
 
 
