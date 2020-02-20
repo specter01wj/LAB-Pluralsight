6a5de3348a0c6ace395fe650f4d28b7ffc22b45a
specter01wj <specter01wj@gmail.com>
2020-02-20 01:13:36 -0500

    02/08/2020 v1

---
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code1_symbols.js
@@ -0,0 +1,36 @@
+// import { projectId, projectName } from './module1.js';
+// import { projectId as id, projectName } from './module1.js';
+// import someValue from './module1.js';
+// import { default as myProjectName } from './module1.js';
+// import someValue from './module1.js';
+import * as values from './module1.js';
+
+'use strict';
+
+console.log('Hello World!  From a module.');
+
+
+// projectId = 99;
+// console.log(projectId);
+
+
+// console.log(`${projectName} has id: ${projectId}`);
+
+
+// console.log(`${projectName} has id: ${id}`);
+
+
+// console.log(someValue);
+
+
+// console.log(myProjectName);
+
+// console.log(someValue);
+
+
+console.log(values);
+
+
+
+
+
