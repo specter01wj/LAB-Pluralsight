5ca28507ad2602c3544db4f9a8fe5736d4406c5d
specter01wj <specter01wj@gmail.com>
2020-02-19 19:51:28 -0500

    02/08/2020 v1

---
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch4_module/ch4_code7_new.target.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/*class Project {
+	static getDefaultId() {
+		return 0;
+	}
+}
+
+console.log(Project.getDefaultId());*/
+
+
+
+/*class Project {
+	static getDefaultId() {
+		return 0;
+	}
+}
+
+var p = new Project();
+console.log(p.getDefaultId());*/
+
+
+
+/*class Project {
+	static let id = 0;
+}
+
+console.log(Project.id);*/
+
+
+
+class Project {
+
+}
+
+Project.id = 99;
+console.log(Project.id);
+
+
+
+
+
+
+
+
+
+
+
+
+
