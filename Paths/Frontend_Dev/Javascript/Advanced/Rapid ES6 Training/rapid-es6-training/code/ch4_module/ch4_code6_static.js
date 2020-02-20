88c4a2be707fdbee4bc444bb8271a6c09fd01d96
specter01wj <specter01wj@gmail.com>
2020-02-19 19:47:17 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch4_module/ch4_code6_static.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch4_module/ch4_code6_static.js
@@ -1,12 +1,31 @@
 'use strict';
 
-class Project {
+/*class Project {
 	static getDefaultId() {
 		return 0;
 	}
 }
 
-console.log(Project.getDefaultId());
+console.log(Project.getDefaultId());*/
+
+
+
+/*class Project {
+	static getDefaultId() {
+		return 0;
+	}
+}
+
+var p = new Project();
+console.log(p.getDefaultId());*/
+
+
+
+class Project {
+	static let id = 0;
+}
+
+console.log(Project.id);
 
 
 
