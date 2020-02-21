4294b48f0a318e3e69171b0c29b1acad9ddfd789
specter01wj <specter01wj@gmail.com>
2020-02-20 19:52:03 -0500

    02/20/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code3_object.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code3_object.js
@@ -1,47 +1,17 @@
 
 'use strict';
 
-let Blog = function() {
-
-};
-let blog = new Blog();
-console.log(blog.toString());
 
 
 
-let Blog2 = function() {
-
-};
-Blog2.prototype[Symbol.toStringTag] = 'Blog Class';
-let blog2 = new Blog2();
-console.log(blog2.toString());
 
 
 
-let values = [8, 12, 16];
-console.log([].concat(values));
 
 
 
-let values2 = [8, 12, 16];
-values2[Symbol.isConcatSpreadable] = false;
-console.log([].concat(values2));
 
 
-
-let values3 = [8, 12, 16];
-let sum = values3 + 100;
-console.log(sum);
-
-
-
-let values4 = [8, 12, 16];
-values4[Symbol.toPrimitive] = function(hint) {
-	console.log(hint);
-	return 42;
-}
-let sum2 = values4 + 100;
-console.log(sum2);
 
 
 
