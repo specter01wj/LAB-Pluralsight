6e030f7e2e5dbe2fa1fc27a965913f982351121e
specter01wj <specter01wj@gmail.com>
2020-02-20 20:26:13 -0500

    02/20/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code3_object.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code3_object.js
@@ -14,6 +14,33 @@ console.log(a.y);
 
 
 
+let a2 = {
+	a_2: 1
+};
+
+let b2 = {
+	b_2: 2
+};
+let target = {};
+Object.assign(target, a2, b2);
+console.log(target);
+
+
+
+let a3 = {
+	a_3: 1
+};
+
+let b3 = {
+	a_3: 5,
+	b_3: 2
+};
+let target2 = {};
+Object.assign(target2, a2, b2);
+console.log(target2);
+
+
+
 
 
 
