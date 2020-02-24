adbb3bc47cd7769799801b94570bfb75437a7735
specter01wj <specter01wj@gmail.com>
2020-02-24 03:19:28 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code7_regexp.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code7_regexp.js
@@ -24,6 +24,23 @@ console.log(pattern4.test('\u{1f3c4}Surfer'));
 
 
 
+let pattern5 = /900/y;
+console.log(pattern5.lastIndex);
+console.log(pattern5.test('800900'));
+
+
+
+let pattern6 = /900/y;
+pattern6.lastIndex = 3;
+console.log(pattern6.test('800900'));
+
+
+
+let pattern7 = /900/yg;
+console.log(pattern7.flags);
+
+
+
 
 
 
