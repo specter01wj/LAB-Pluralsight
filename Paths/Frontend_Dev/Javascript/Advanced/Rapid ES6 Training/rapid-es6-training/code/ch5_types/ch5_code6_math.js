4ec96850795b26b3067ac0d50cacccdb105ff358
specter01wj <specter01wj@gmail.com>
2020-02-24 02:57:38 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code6_math.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code6_math.js
@@ -2,46 +2,24 @@
 'use strict';
 
 
-console.log(Number.parseInt === parseInt);
+console.log(Math.sign(0));
+console.log(Math.sign(-0));
+console.log(Math.sign(-20));
+console.log(Math.sign(20));
+console.log(Math.sign(NaN));
 
 
-console.log(Number.parseFloat === parseFloat);
+
+console.log(Math.cbrt(27));
 
 
-let str1 = 'NaN';
-console.log(isNaN(str1));
-console.log(Number.isNaN(str1));
+
+console.log(Math.trunc(27.1));
+console.log(Math.trunc(-27.9));
 
 
-let str2 = '8000';
-console.log(isFinite(str2));
-console.log(Number.isFinite(str2));
 
 
-let str3 = 408.2;
-console.log(Number.isInteger(str3));
-
-
-console.log('----------------');
-
-console.log(Number.isInteger(NaN));
-console.log(Number.isInteger(Infinity));
-console.log(Number.isInteger(undefined));
-console.log(Number.isInteger(3));
-
-
-console.log('----------------');
-
-let a1 = Math.pow(2, 53) - 1;
-console.log(Number.isSafeInteger(a1));
-console.log(a1);
-a1 = Math.pow(2, 53);
-console.log(Number.isSafeInteger(a1));
-
-
-console.log(Number.EPSILON);
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(Number.MIN_SAFE_INTEGER);
 
 
 
