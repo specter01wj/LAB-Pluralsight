617e3425ec2043dcb01bb42e34d0b7205fa5715a
specter01wj <specter01wj@gmail.com>
2020-02-23 21:34:38 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code4_string.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code4_string.js
@@ -24,14 +24,30 @@ console.log(Array.from(surfer2).length);
 console.log(surfer2);
 
 
+let title3 = "Mazatla\u0301n";
+console.log(title3 + ' ' + title3.length);
 
 
+let title4 = "Mazatla\u0301n";
+console.log(title4 + ' ' + title4.normalize().length);
 
 
+let title5 = "Mazatla\u0301n";
+console.log(title5.normalize().codePointAt(7).toString(16));
 
 
+console.log(String.fromCodePoint(0x1f3c4));
 
 
+let title6 = "Surfer";
+let output = String.raw`${title6}\u{1f3c4}\n`;
+console.log(output);
+
+
+let wave = "\u{1f30a}";
+console.log(wave.repeat(10));
+let wave2 = "\u{1f40a}";
+console.log(wave2.repeat(20));
 
 
 
