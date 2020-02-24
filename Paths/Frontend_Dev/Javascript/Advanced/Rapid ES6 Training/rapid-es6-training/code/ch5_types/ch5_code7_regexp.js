53131bffb036805085a0c1f5c5de17466de3485d
specter01wj <specter01wj@gmail.com>
2020-02-24 03:10:12 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code7_regexp.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code7_regexp.js
@@ -2,20 +2,25 @@
 'use strict';
 
 
-console.log(Math.sign(0));
-console.log(Math.sign(-0));
-console.log(Math.sign(-20));
-console.log(Math.sign(20));
-console.log(Math.sign(NaN));
+console.log('\u{1f3c4}');
+
+let pattern1 = /\u{1f3c4}/;
+console.log(pattern1.test('\u{1f3c4}'));
 
 
 
-console.log(Math.cbrt(27));
+let pattern2 = /\u{1f3c4}/u;
+console.log(pattern2.test('\u{1f3c4}'));
 
 
 
-console.log(Math.trunc(27.1));
-console.log(Math.trunc(-27.9));
+let pattern3 = /^.Surfer/;
+console.log(pattern3.test('\u{1f3c4}Surfer'));
+
+
+
+let pattern4 = /^.Surfer/u;
+console.log(pattern4.test('\u{1f3c4}Surfer'));
 
 
 
