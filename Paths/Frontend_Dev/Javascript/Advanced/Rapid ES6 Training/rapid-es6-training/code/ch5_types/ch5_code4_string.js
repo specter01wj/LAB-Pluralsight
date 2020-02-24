79aa86b3d6c3effc9ccd759d48d66f34a03ed2d6
specter01wj <specter01wj@gmail.com>
2020-02-23 21:20:03 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code4_string.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code4_string.js
@@ -11,7 +11,17 @@ console.log(title.endsWith('an'));
 console.log(title.includes('es'));
 
 
+let title2 = "Jin's \u{1f3c4} Blog";
+console.log(title2);
 
+
+let surfer = "\u{1f3c4}";
+console.log(surfer.length);
+
+
+let surfer2 = "\u{1f30a}\u{1f3c4}\u{1f40b}";
+console.log(Array.from(surfer2).length);
+console.log(surfer2);
 
 
 
