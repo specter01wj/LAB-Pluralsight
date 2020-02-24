8e2eba0a002a6a2c2bab0c827539e6a4f9fb80b3
specter01wj <specter01wj@gmail.com>
2020-02-24 03:27:20 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code8_function.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code8_function.js
@@ -16,8 +16,22 @@ console.log(fn2.name);
 
 
 
+let fn3 = function() {
+	return 0;
+};
+let newFn3 = fn3;
+console.log(newFn3.name);
 
 
+
+class Calculator {
+	constructor() {}
+
+	add() {}
+}
+let c1 = new Calculator();
+console.log(Calculator.name);
+console.log(c1.add.name);
 
 
 
