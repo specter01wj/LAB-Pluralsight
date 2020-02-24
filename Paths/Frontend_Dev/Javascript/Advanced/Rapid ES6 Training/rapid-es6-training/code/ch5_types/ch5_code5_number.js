75e1b31388d16cfee465dedb953a0f1d1d969f8b
specter01wj <specter01wj@gmail.com>
2020-02-23 22:16:11 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code5_number.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch5_types/ch5_code5_number.js
@@ -2,10 +2,24 @@
 'use strict';
 
 
+console.log(Number.parseInt === parseInt);
 
 
+console.log(Number.parseFloat === parseFloat);
 
 
+let str1 = 'NaN';
+console.log(isNaN(str1));
+console.log(Number.isNaN(str1));
+
+
+let str2 = '8000';
+console.log(isFinite(str2));
+console.log(Number.isFinite(str2));
+
+
+let str3 = 408.2;
+console.log(Number.isInteger(str3));
 
 
 
