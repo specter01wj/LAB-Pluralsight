83add0d623d0b9efd5d6325772d4577a6375e869
specter01wj <specter01wj@gmail.com>
2020-02-25 18:09:25 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code5_promises.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code5_promises.js
@@ -12,7 +12,21 @@ function doAsync1() {
 	});
 	return p;
 }
-let promise1 = doAsync1();
+// let promise1 = doAsync1();
+
+
+
+function doAsync2() {
+	let p = new Promise(function(resolve, reject) {
+		console.log('in promise code');
+		setTimeout(function() {
+			console.log('resolving...');
+			reject();
+		}, 2000);
+	});
+	return p;
+}
+let promise2 = doAsync2();
 
 
 
