0856dcec365bf9c40893b497df362031e30ec5bf
specter01wj <specter01wj@gmail.com>
2020-02-25 02:56:59 -0500

    02/25/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code3_yield.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code3_yield.js
@@ -10,6 +10,34 @@ console.log(it1.next());
 
 
 
+function *process2() {
+	yield;
+}
+let it2 = process2();
+console.log(it2.next());
+
+
+
+function *process3() {
+	let result = yield;
+	console.log(`result is ${result}`);
+}
+let it3 = process3();
+it3.next();
+it3.next(200);
+
+
+
+function *process4() {
+	let result = yield;
+	console.log(`result is ${result}`);
+}
+let it4 = process4();
+it4.next();
+console.log(it4.next(200));
+
+
+
 
 
 
