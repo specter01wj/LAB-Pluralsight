3c81da0ad3dc5955e0161b6e4fc422fb91269a28
specter01wj <specter01wj@gmail.com>
2020-02-24 21:08:31 -0500

    02/24/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code3_yield.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code3_yield.js
@@ -11,54 +11,6 @@ console.log(it1.next());
 
 
 
-function *process2() {
-	yield 8000;
-	yield 8001;
-}
-let it2 = process2();
-it2.next();
-console.log(it2.next());
-
-
-
-function *process3() {
-	yield 8000;
-	yield 8001;
-}
-let it3 = process3();
-it3.next();
-it3.next();
-console.log(it3.next());
-
-
-
-function *process4() {
-	let nextId = 7000;
-	while(true) {
-		yield(nextId++);
-	}
-}
-let it4 = process4();
-it4.next();
-console.log(it4.next().value);
-
-
-
-function *process5() {
-	let nextId = 7000;
-	while(true) {
-		yield(nextId++);
-	}
-}
-
-for(let id1 of process5()) {
-	if(id1 > 7002) break;
-	console.log(id1);
-}
-
-
-
-
 
 
 
