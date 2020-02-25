32d4ab4e23e5ffbb8a4e907f64b9798d8152364b
specter01wj <specter01wj@gmail.com>
2020-02-25 13:36:14 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code4_throw.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code4_throw.js
@@ -19,7 +19,7 @@ console.log(it1.next());
 
 
 
-function *process2() {
+/*function *process2() {
 	yield 9000;
 	yield 9001;
 	yield 9002;
@@ -27,7 +27,19 @@ function *process2() {
 let it2 = process2();
 console.log(it2.next().value);
 console.log(it2.throw('foo'));
-console.log(it2.next());
+console.log(it2.next());*/
+
+
+
+function *process3() {
+	yield 9000;
+	yield 9001;
+	yield 9002;
+}
+let it3 = process3();
+console.log(it3.next().value);
+console.log(it3.return('foo'));
+console.log(it3.next());
 
 
 
