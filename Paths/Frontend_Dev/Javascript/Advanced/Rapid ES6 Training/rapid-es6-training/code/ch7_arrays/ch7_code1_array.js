1607d3a061428b7b5a65e5928a82086e25c7c1eb
specter01wj <specter01wj@gmail.com>
2020-02-26 13:19:58 -0500

    02/26/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code1_array.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code1_array.js
@@ -11,6 +11,28 @@ console.log(salaries2.length);
 
 
 
+let amounts1 = [800, 810, 820];
+let salaries3 = Array.from(amounts1, v => v + 100);
+console.log(salaries3);
+
+
+
+let amounts2= [800, 810, 820];
+let salaries4 = Array.from(amounts2, function(v) {
+	return v + this.adjustment;
+}, { adjustment: 50 });
+console.log(salaries4);
+
+
+
+/*let amounts3= [800, 810, 820];
+let salaries5 = Array.from(amounts3, v => {
+	return v + this.adjustment;
+}, { adjustment: 50 });
+console.log(salaries5);*/
+
+
+
 
 
 
