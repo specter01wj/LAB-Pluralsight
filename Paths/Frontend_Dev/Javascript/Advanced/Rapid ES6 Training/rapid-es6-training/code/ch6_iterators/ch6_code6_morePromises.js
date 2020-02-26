a02aa85462171d2afcdcc72eb01c50bf6c7a1634
specter01wj <specter01wj@gmail.com>
2020-02-26 03:19:08 -0500

    02/08/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code6_morePromises.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch6_iterators/ch6_code6_morePromises.js
@@ -2,81 +2,33 @@
 'use strict';
 
 
-function doAsync1() {
+/*function doAsync1() {
 	let p = new Promise(function(resolve, reject) {
 		console.log('in promise code');
 		setTimeout(function() {
-			console.log('resolving...');
-			resolve();
+			resolve(getAnotherPromise());
 		}, 2000);
 	});
 	return p;
 }
-let promise1 = doAsync1();
+
+doAsync1().then(function() {
+	console.log('OK');
+}, function() {
+	console.log('Nope');
+});*/
 
 
 
 function doAsync2() {
-	let p = new Promise(function(resolve, reject) {
-		console.log('in promise code');
-		setTimeout(function() {
-			console.log('rejecting...');
-			reject();
-		}, 2000);
-	});
-	return p;
-}
-let promise2 = doAsync2();
-
-
-
-/*function doAsync3() {
-
+	return Promise.resolve('Some String');
 }
 
-doAsync3().then(function() {
-	console.log('Fullfilled!');
-},
-function() {
-	console.log('Rejected!');
-});*/
-
-
-
-/*function doAsync4() {
-
-}
-
-doAsync4().then(function(value) {
-	console.log('Fullfilled!' + value);
-},
-function(reason) {
-	console.log('Rejected!' + reason);
-});*/
-
-
-
-/*function doAsync5() {
-
-}
-
-doAsync5().then(function(value) {
-	console.log('Fullfilled!' + value);
-	return 'For Sure';
-}).then(function(value) {
-	console.log('Rejected!' + value);
-});*/
-
-
-
-/*function doAsync6() {
-
-}
-
-doAsync6().catch(function(reason) {
-	console.log('Error: ' + reason);
-});*/
-
+doAsync2().then(function(value) {
+	console.log('OK: ' + value);
+}, function(reason) {
+	console.log('Nope: ' + reason);
+});
 
 
 
