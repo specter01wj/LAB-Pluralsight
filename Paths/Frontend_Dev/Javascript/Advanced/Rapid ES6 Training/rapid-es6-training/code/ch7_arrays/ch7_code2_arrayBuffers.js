a7e978931108f84a8e65b5879bce93f803a79148
specter01wj <specter01wj@gmail.com>
2020-02-26 17:05:47 -0500

    02/26/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code2_arrayBuffers.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code2_arrayBuffers.js
@@ -1,7 +1,14 @@
 
 'use strict';
 
+let buffer1 = new ArrayBuffer(1024);
+console.log(buffer1.byteLength);
 
+
+
+let buffer2 = new ArrayBuffer(1024);
+buffer2[0] = 0xff;
+console.log(buffer2[0]);
 
 
 
