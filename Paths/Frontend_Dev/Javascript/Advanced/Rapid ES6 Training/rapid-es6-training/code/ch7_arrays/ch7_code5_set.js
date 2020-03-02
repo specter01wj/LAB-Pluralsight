4a55d3fbe32bd3956350004ab215599a1fb64be8
specter01wj <specter01wj@gmail.com>
2020-03-01 21:35:36 -0500

    03/01/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code5_set.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code5_set.js
@@ -4,15 +4,26 @@
 let perks1 = new Set();
 perks1.add('Car');
 perks1.add('Long Vacation');
+
+console.log(perks1);
 console.log(perks1.size);
 
 
 
+perks1.add('Car');
+console.log(perks1);
+console.log(perks1.size);
 
 
 
+let perks2 = new Set(['Car', 'Tank', 'Plane']);
+console.log(perks2.size);
 
 
+
+let newPerks1 = new Set(perks2);
+console.log(newPerks1);
+console.log(newPerks1.size);
 
 
 
