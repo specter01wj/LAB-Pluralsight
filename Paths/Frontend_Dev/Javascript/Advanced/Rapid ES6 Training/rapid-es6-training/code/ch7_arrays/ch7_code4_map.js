9d22a1a661876441728c3b49fad4e1b4231ab6e0
specter01wj <specter01wj@gmail.com>
2020-03-01 20:53:46 -0500

    03/01/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code4_map.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code4_map.js
@@ -13,10 +13,32 @@ console.log(employees.get(employee1));
 
 
 
+console.log(employees.size);
 
 
 
+employees.delete(employee2);
+console.log(employees.size);
 
+
+employees.clear();
+console.log(employees.size);
+
+
+
+let employee3 = {name: 'Jin'};
+let employee4 = {name: 'Mike'};
+let arr1 = [
+	[employee3, 'DEF'],
+	[employee4, '456']
+];
+let employees_2 = new Map(arr1);
+console.log(employees_2);
+console.log(employees_2.size);
+
+
+
+console.log(employees_2.has(employee4));
 
 
 
