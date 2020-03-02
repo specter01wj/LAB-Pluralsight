a83c4673baf7ad80cf7db5eb20b3a23e0f29e377
specter01wj <specter01wj@gmail.com>
2020-03-01 22:10:00 -0500

    03/01/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code6_subclass.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code6_subclass.js
@@ -6,18 +6,37 @@ class Perks extends Array {
 }
 
 let a1 = Perks.from([5, 10, 15]);
+
+console.log(a1);
 console.log(a1 instanceof Perks);
 
 
 
+console.log(a1.length);
 
 
 
+let newArr1 = a1.reverse();
+console.log(newArr1);
+console.log(newArr1 instanceof Perks);
 
 
 
+console.log(newArr1 instanceof Array);
 
 
+
+class Perks2 extends Array {
+	sum() {
+		let total = 0;
+		this.map( v => total += v );
+		return total;
+	}
+}
+
+let a2 = Perks2.from([5, 10, 15]);
+console.log(a2);
+console.log(a2.sum());
 
 
 
