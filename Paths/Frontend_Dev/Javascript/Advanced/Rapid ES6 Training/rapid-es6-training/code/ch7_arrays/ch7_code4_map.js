5d7339b3c876554ce821e704e748348a29065bae
specter01wj <specter01wj@gmail.com>
2020-03-01 20:46:57 -0500

    03/01/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code4_map.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch7_arrays/ch7_code4_map.js
@@ -1,33 +1,15 @@
 
 'use strict';
 
-let buffer1 = new ArrayBuffer(1024);
-let dv1 = new DataView(buffer1);
-console.log(dv1.byteLength);
+let employee1 = {name: 'James'};
+let employee2 = {name: 'Kim'};
 
+let employees = new Map();
+employees.set(employee1, 'ABC');
+employees.set(employee2, '123');
 
-
-let buffer2 = new ArrayBuffer(1024);
-let dv2 = new DataView(buffer2, 0, 32);
-console.log(dv2.byteLength);
-
-
-
-let buffer3 = new ArrayBuffer(1024);
-let dv3 = new DataView(buffer3);
-dv3.setUint8(0, 1);
-console.log(dv3.getUint16(0));
-
-
-
-let buffer4 = new ArrayBuffer(1024);
-let dv4 = new DataView(buffer4);
-dv4.setUint8(0, 1);
-console.log(dv3.getUint16(0, true));
-
-
-
-
+console.log(employees);
+console.log(employees.get(employee1));
 
 
 
