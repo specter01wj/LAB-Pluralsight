918fe5b0116e65cce09107f34f8c1b7ebcbdabf6
specter01wj <specter01wj@gmail.com>
2020-03-08 23:47:33 -0400

    03/06/2020 v1

---
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/05/mysol_1/operators.ts
@@ -0,0 +1,11 @@
+import { Observable, from } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+let num$ = from([-2, -1, 0, 1, 2]);
+
+let observer = {
+	next: value => console.log(value),
+    error: err => console.log(`ERROR: ${err}`),
+    complete: () => console.log('All done.')
+};
+
