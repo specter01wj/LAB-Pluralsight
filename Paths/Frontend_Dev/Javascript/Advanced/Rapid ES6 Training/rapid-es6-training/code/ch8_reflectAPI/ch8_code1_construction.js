0c33cfba897a69b8396b959c179d0653d6a6b7b0
specter01wj <specter01wj@gmail.com>
2020-04-12 19:04:09 -0600

    04/03/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch8_reflectAPI/ch8_code1_construction.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch8_reflectAPI/ch8_code1_construction.js
@@ -6,12 +6,23 @@ console.log(typeof Reflect);
 
 
 
-class Restaurant_1 {
+/*class Restaurant_1 {
 
 }
 
 let r1 = Reflect.construct(Restaurant_1);
-console.log(r1 instanceof Restaurant_1);
+console.log(r1 instanceof Restaurant_1);*/
+
+
+
+class Restaurant_2 {
+	constructor(name, city) {
+		console.log(`${name} in ${city}`);
+	}
+}
+
+let r2 = Reflect.construct(Restaurant_2, ["Jim", "New York"]);
+
 
 
 
