c4a97ab66e10e280a65f791a94bc5039cf2da8a5
specter01wj <specter01wj@gmail.com>
2020-04-12 22:58:15 -0600

    04/03/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch8_reflectAPI/ch8_code2_prototypes.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch8_reflectAPI/ch8_code2_prototypes.js
@@ -2,67 +2,17 @@
 'use strict';
 
 
-console.log(typeof Reflect);
-
-
-
-/*class Restaurant_1 {
-
-}
-
-let r1 = Reflect.construct(Restaurant_1);
-console.log(r1 instanceof Restaurant_1);*/
-
-
-
-class Restaurant_2 {
-	constructor(name, city) {
-		console.log(`${name} in ${city}`);
-	}
-}
-
-let r2 = Reflect.construct(Restaurant_2, ["Jim", "New York"]);
-
-
-
-class Restaurant_3 {
+class Location_1 {
 	constructor() {
-		console.log(`new target: ${new.target}`);
-	}
-}
-function restaurantMaker_1() {
-	console.log('in restaurantMaker');
-}
-
-let r3 = Reflect.construct(Restaurant_3, ["James", "Baltimore"], restaurantMaker_1);
-
-
-
-/*class Restaurant_4 {
-	constructor() {
-		this.id = 33;
-	}
-	show() {
-		console.log(this.id);
+		console.log('constructing Location');
 	}
 }
 
-Reflect.apply(Restaurant_4.prototype.show, {id: 99});*/
+class Restaurant_1 extends Location {
 
-
-
-class Restaurant_5 {
-	constructor() {
-		this.id = 33;
-	}
-	show(prefix) {
-		console.log(prefix + this.id);
-	}
 }
 
-Reflect.apply(Restaurant_5.prototype.show, {id: 22}, ['REST']);
-
-
+console.log(Reflect.getPrototypeOf(Restaurant_1));
 
 
 
