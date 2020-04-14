f9fdd0a568fb1bfa00df8bb6f8114296d8391a6c
specter01wj <specter01wj@gmail.com>
2020-04-14 15:51:00 -0600

    04/14/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch9_proxyAPI/ch9_code1_getByProxy.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch9_proxyAPI/ch9_code1_getByProxy.js
@@ -19,6 +19,50 @@ console.log(p1.salary);
 
 
 
+function Employee_2() {
+	this.name = 'Mike Bolt';
+	this.salary = 10;
+}
+
+var e2 = new Employee_2();
+
+var p2 = new Proxy(e2, {
+	get: function(target, prop, receiver) {
+		return Reflect.get(target, prop, receiver);
+	}
+});
+
+console.log(p2.salary);
+
+
+
+function Employee_3() {
+	this.name = 'Lisa King';
+	this.salary = 20;
+}
+
+var e3 = new Employee_3();
+
+var p3 = new Proxy(e3, {
+	get: function(target, prop, receiver) {
+		if(prop === 'salary') {
+			return 'Denied';
+		}
+		return Reflect.get(target, prop, receiver);
+	}
+});
+
+console.log(p3.salary);
+console.log(p3.name);
+
+
+
+
+
+
+
+
+
 
 
 
