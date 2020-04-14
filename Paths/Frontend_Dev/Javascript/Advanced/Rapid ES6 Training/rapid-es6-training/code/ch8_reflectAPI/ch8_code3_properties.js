5f806f34ec8cb486f42a39e62e4f566b45bff846
specter01wj <specter01wj@gmail.com>
2020-04-14 00:20:10 -0600

    04/13/2020 v1

--- Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch8_reflectAPI/ch8_code3_properties.js
+++ Paths/Frontend_Dev/Javascript/Advanced/Rapid ES6 Training/rapid-es6-training/code/ch8_reflectAPI/ch8_code3_properties.js
@@ -14,6 +14,21 @@ console.log(Reflect.get(r1, 'id'));
 
 
 
+class Restaurant_2 {
+	constructor() {
+		this._id = 9000;
+	}
+	get id() {
+		return this._id;
+	}
+}
+
+let r2 = new Restaurant_2();
+
+console.log(Reflect.get(r2, 'id', {_id: 88}));
+
+
+
 
 
 
