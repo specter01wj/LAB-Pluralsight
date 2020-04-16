526ae799e454b320f69551910b8ee765be12ee7b
specter01wj <specter01wj@gmail.com>
2020-04-15 22:32:19 -0600

    04/15/2020 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/05/mysol_rxjsdemo/operators_1/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/05/mysol_rxjsdemo/operators_1/index.ts
@@ -3,6 +3,12 @@ import { pluck } from 'rxjs/operators';
 
 let clicks$ = fromEvent(document, 'click');
 
+/*clicks$.subscribe(
+	(value) => console.log(value),
+	(err) => console.log(`ERROR: ${err}`),
+	() => console.log('All done.')
+);*/
+
 clicks$.pipe(
 	pluck('clientX')
 )
