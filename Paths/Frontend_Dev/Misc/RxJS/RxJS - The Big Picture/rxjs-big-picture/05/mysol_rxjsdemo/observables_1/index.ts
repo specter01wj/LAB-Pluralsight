6dda54b388b1f4614069c7f50160376e187ef39b
specter01wj <specter01wj@gmail.com>
2020-04-15 22:07:30 -0600

    04/15/2020 v1

---
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/05/mysol_rxjsdemo/observables_1/index.ts
@@ -0,0 +1,15 @@
+import { Observable, from } from 'rxjs';
+
+let nums = [2, 4, 6, 8, 10];
+
+let numsObservable$ = from(nums);
+
+let observer = {
+	next: (value) => console.log(value),
+	error: (err) => console.log(`ERROR: ${err}`),
+	complete: () => console.log('All done.')
+};
+
+numsObservable$.subscribe(observer);
+
+
