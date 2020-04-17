7d9d7a05d85c30bee825d74d82ed08f4d69fc8ad
specter01wj <specter01wj@gmail.com>
2020-04-16 21:52:38 -0600

    04/16/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/demos/mysol_1/BookTracker/src/app/services/data.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/demos/mysol_1/BookTracker/src/app/services/data.service.ts
@@ -15,6 +15,24 @@ export class DataService {
 
   constructor(private loggerService: LoggerService) { }
 
-  
+  getAllReaders(): Reader[] {
+    return allReaders;
+  }
+
+  getReaderById(id: number): Reader {
+    return allReaders.find(reader => reader.readerID === id);
+  }
+
+  getAllBooks(): Book[] {
+    return allBooks;
+  }
+
+  getBookById(id: number): Book {
+    return allBooks.find(book => book.bookID === id);
+  }  
+
+  setMostPopularBook(popularBook: Book): void {
+    this.mostPopularBook = popularBook;
+  }
   
 }
