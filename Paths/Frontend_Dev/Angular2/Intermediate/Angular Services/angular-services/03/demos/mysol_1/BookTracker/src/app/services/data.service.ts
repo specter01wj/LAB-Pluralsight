86a0683f544c3701552a6de1e41bb444f9d09287
specter01wj <specter01wj@gmail.com>
2020-04-16 21:48:15 -0600

    04/16/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/demos/mysol_1/BookTracker/src/app/services/data.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/demos/mysol_1/BookTracker/src/app/services/data.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 
+import { LoggerService } from './logger.service';
+
+import { Reader } from 'app/models/reader';
+import { allReaders, allBooks } from 'app/data';
+import { Book } from 'app/models/book';
+
 @Injectable({
   providedIn: 'root'
 })
