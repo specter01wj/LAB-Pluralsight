0259eef0a89e33b84c1df78cd43c1e31caa19113
specter01wj <specter01wj@gmail.com>
2020-04-17 18:08:29 -0600

    04/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/04/demos/mysol_1/booktracker/src/app/services/plain-logger.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/04/demos/mysol_1/booktracker/src/app/services/plain-logger.service.ts
@@ -9,4 +9,14 @@ import { LoggerService } from './logger.service';
 export class PlainLoggerService implements LoggerService {
 
   constructor() { }
+
+  log(message: string): void {
+    const timeString: String = new Date().toLocaleTimeString();
+    console.log(`${message} (${timeString})`);
+  }
+
+  error(message: string): void {
+    console.error(`ERROR: ${message}`);
+  }
+  
 }
