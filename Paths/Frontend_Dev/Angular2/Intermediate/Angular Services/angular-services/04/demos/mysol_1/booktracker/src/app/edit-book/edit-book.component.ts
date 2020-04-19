233d3dcff013e522768e50a3896026c34eae5896
specter01wj <specter01wj@gmail.com>
2020-04-18 22:18:08 -0600

    04/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/04/demos/mysol_1/booktracker/src/app/edit-book/edit-book.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/04/demos/mysol_1/booktracker/src/app/edit-book/edit-book.component.ts
@@ -4,24 +4,20 @@ import { ActivatedRoute } from '@angular/router';
 import { Book } from 'app/models/book';
 import { DataService } from 'app/services/data.service';
 import { LoggerService } from 'app/services/logger.service';
-import { BadgeService } from 'app/services/badge.service';
 
 @Component({
   selector: 'app-edit-book',
   templateUrl: './edit-book.component.html',
-  styles: [],
-  providers: [BadgeService]
+  styles: []
   // providers: [DataService]
 })
 export class EditBookComponent implements OnInit {
 
   selectedBook: Book;
-  currentBadge: string;
 
   constructor(private route: ActivatedRoute,
               private dataService: DataService,
-              private loggerService: LoggerService,
-              private badgeService: BadgeService) { }
+              private loggerService: LoggerService) { }
 
   ngOnInit() {
     let bookID: number = parseInt(this.route.snapshot.params['id']);
@@ -31,7 +27,6 @@ export class EditBookComponent implements OnInit {
   setMostPopular(): void {
     this.dataService.setMostPopularBook(this.selectedBook);
     this.loggerService.log(`New most popular book: ${this.selectedBook.title}`);
-    this.currentBadge = this.badgeService.getReaderBadge(this.selectedBook.totalMinutesRead);
   }
 
   saveChanges(): void {
