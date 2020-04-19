0451b1b44dcda374ea00324e49afe385b0d19d73
specter01wj <specter01wj@gmail.com>
2020-04-18 22:17:22 -0600

    04/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/04/demos/mysol_1/booktracker/src/app/edit-reader/edit-reader.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/04/demos/mysol_1/booktracker/src/app/edit-reader/edit-reader.component.ts
@@ -3,22 +3,27 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Reader } from "app/models/reader";
 import { DataService } from 'app/services/data.service';
+import { BadgeService } from 'app/services/badge.service';
 
 @Component({
   selector: 'app-edit-reader',
   templateUrl: './edit-reader.component.html',
-  styles: []
+  styles: [],
+  providers: [BadgeService]
 })
 export class EditReaderComponent implements OnInit {
 
   selectedReader: Reader;
+  currentBadge: string;
 
   constructor(private route: ActivatedRoute,
-              private dataService: DataService) { }
+              private dataService: DataService,
+              private badgeService: BadgeService) { }
 
   ngOnInit() {
     let readerID: number = parseInt(this.route.snapshot.params['id']);
     this.selectedReader = this.dataService.getReaderById(readerID);
+    this.currentBadge = this.badgeService.getReaderBadge(this.selectedReader.totalMinutesRead);
   }
 
   saveChanges() {
