1055c13850d5c787c4de8fda0f62480457552a46
specter01wj <specter01wj@gmail.com>
2020-04-18 22:05:13 -0600

    04/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/04/demos/mysol_1/booktracker/src/app/services/badge.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/04/demos/mysol_1/booktracker/src/app/services/badge.service.ts
@@ -4,4 +4,19 @@ import { Injectable } from '@angular/core';
 export class BadgeService {
 
   constructor() { }
+
+  getReaderBadge(minutesRead: number): string {
+
+    if (minutesRead > 5000) {
+      return 'Book Worm';
+    }
+    else if (minutesRead > 2500) {
+      return 'Page Turner';
+    }
+    else {
+      return 'Getting Started';
+    }
+    
+  }
+  
 }
