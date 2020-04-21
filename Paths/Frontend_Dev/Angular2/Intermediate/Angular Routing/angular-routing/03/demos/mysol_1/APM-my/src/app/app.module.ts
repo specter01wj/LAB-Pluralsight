9da058f823fc9c9abcf7b43c89adfc6b0134de60
specter01wj <specter01wj@gmail.com>
2020-04-20 22:41:26 -0600

    04/20/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/03/demos/mysol_1/APM-my/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/03/demos/mysol_1/APM-my/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
