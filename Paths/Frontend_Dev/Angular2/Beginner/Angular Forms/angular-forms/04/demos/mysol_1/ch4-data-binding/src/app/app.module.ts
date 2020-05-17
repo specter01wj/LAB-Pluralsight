be16c04fa6cb1c1a1743fec4248fc8be098d68fc
specter01wj <specter01wj@gmail.com>
2020-05-17 17:27:09 -0600

    05/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/04/demos/mysol_1/ch4-data-binding/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/04/demos/mysol_1/ch4-data-binding/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
