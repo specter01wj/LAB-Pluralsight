8b6aefb754bbc1551566a41f217982f49207fb5c
specter01wj <specter01wj@gmail.com>
2020-05-17 12:59:05 -0600

    05/15/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/03/demos/mysol_1/form-basic/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/03/demos/mysol_1/form-basic/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
@@ -12,6 +14,7 @@ import { UserSettingsFormComponent } from './user-settings-form/user-settings-fo
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
