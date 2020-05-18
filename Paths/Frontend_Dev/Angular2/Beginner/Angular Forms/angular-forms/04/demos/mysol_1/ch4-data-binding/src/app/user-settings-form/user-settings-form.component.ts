8060695b96ff0f0c672c64fc6738c0cc200bdd04
specter01wj <specter01wj@gmail.com>
2020-05-17 18:10:06 -0600

    05/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/04/demos/mysol_1/ch4-data-binding/src/app/user-settings-form/user-settings-form.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/04/demos/mysol_1/ch4-data-binding/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserSettings } from '../services/user-settings';
+
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.less']
 })
 export class UserSettingsFormComponent implements OnInit {
+	originalUserSettings : UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
+
+  userSettings : UserSettings = { ...this.originalUserSettings };
 
   constructor() { }
 
