9ec2cb98faea1858057f74aa27f051d5e1f54eae
specter01wj <specter01wj@gmail.com>
2020-05-18 22:49:16 -0600

    05/17/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/05/demos/mysol_1/ch5-form-valid/src/app/user-settings-form/user-settings-form.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/05/demos/mysol_1/ch5-form-valid/src/app/user-settings-form/user-settings-form.component.ts
@@ -24,4 +24,12 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onBlur(field : NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+  }
+
 }
