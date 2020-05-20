25acc51af788d0d6e91cff2371022bad61a5e689
specter01wj <specter01wj@gmail.com>
2020-05-19 23:47:50 -0600

    05/19/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/06/demos/mysol_1/ch6-http/src/app/services/data.service.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/06/demos/mysol_1/ch6-http/src/app/services/data.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { UserSettings } from './user-settings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSubscriptionTypes(): Observable<string[]> {
+    return of(['Monthly', 'Annual', 'Lifetime']);
+  }
+
+  postUserSettingsForm(userSettings: UserSettings) : Observable<any> {
+    
+    return this.http.post('https://putsreq.com/PIYdcSxJOW3GNDcReHD8', userSettings);
+    
+    //return of(userSettings);
+  }
+  
 }
