24cccada2e00a136174c70f7ea6ff8bc3b08af8f
specter01wj <specter01wj@gmail.com>
2020-05-20 20:49:47 -0600

    05/19/2020 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/07/demos/mysol_1/ch7-thirdParty/src/app/user-settings-form/user-settings-form.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Forms/angular-forms/07/demos/mysol_1/ch7-thirdParty/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { UserSettings } from '../services/user-settings';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,10 +11,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.less']
 })
 export class UserSettingsFormComponent implements OnInit {
+	originalUserSettings : UserSettings = {
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
+  };
 
-  constructor() { }
+  startDate: Date;
+  startTime: Date;
+  userRating = 0;
+  maxRating = 10;
+  userSettings : UserSettings = { ...this.originalUserSettings };
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes: Observable<string[]>;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+  	this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+
+    this.startDate = new Date();
+    this.startTime = new Date();
+  }
+
+  onBlur(field : NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+
+  onHttpError(errorResponse: any) {
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.value);
+
+    // if (form.valid) {
+    //   this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+    //     result => console.log('success: ', result),
+    //     error => this.onHttpError(error)
+    //   );
+    // }
+    // else {
+    //   this.postError = true;
+    //   this.postErrorMessage = "Please fix the above errors"
+    // }
   }
 
 }
