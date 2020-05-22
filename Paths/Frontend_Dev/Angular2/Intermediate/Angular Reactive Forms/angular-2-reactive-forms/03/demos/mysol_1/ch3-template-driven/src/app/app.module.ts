18d12f975402e545853ec5bfb15cf830aacdd2b4
specter01wj <specter01wj@gmail.com>
2020-05-21 23:02:24 -0600

    05/19/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms/03/demos/mysol_1/ch3-template-driven/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms/03/demos/mysol_1/ch3-template-driven/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
 
@@ -12,6 +14,7 @@ import { CustomerComponent } from './customers/customer.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
