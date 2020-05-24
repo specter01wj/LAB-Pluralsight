3373c77503f2e9c984d1274608cea446e4df865e
specter01wj <specter01wj@gmail.com>
2020-05-23 22:14:10 -0600

    05/19/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms/06/demos/mysol_1/ch6-react-change/src/app/customers/customer.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms/06/demos/mysol_1/ch6-react-change/src/app/customers/customer.component.ts
@@ -11,10 +11,41 @@ import { Customer } from '../services/customer';
   styleUrls: ['./customer.component.less']
 })
 export class CustomerComponent implements OnInit {
+	customerForm: FormGroup;
+  customer = new Customer();
+  emailMessage: string;
 
-  constructor() { }
+  private validationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address.'
+  };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  	this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }, { validator: emailMatcher }),
+      phone: '',
+      notification: 'email',
+      rating: [null, ratingRange(1, 5)],
+      sendCatalog: true
+    });
+
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    );
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    );
   }
 
 }
