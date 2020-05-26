f85c6d2cdbcf3f9bf4df33bf6637dd40e6207b18
specter01wj <specter01wj@gmail.com>
2020-05-25 23:07:37 -0600

    05/24/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms/08/demos/mysol_1/ch8-apm-context/src/app/products/product-detail/product-detail.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms/08/demos/mysol_1/ch8-apm-context/src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Product } from '../../interface/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.less']
 })
 export class ProductDetailComponent implements OnInit {
+	pageTitle = 'Product Detail';
+  errorMessage = '';
+  product: Product | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) { }
 
   ngOnInit(): void {
+  	const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
