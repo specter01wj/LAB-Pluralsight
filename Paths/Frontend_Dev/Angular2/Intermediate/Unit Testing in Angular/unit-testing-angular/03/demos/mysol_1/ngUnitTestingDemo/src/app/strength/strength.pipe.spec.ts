f46b43ae6168d49bb2ce6831b1993bb92ac13224
specter01wj <specter01wj@gmail.com>
2020-06-10 23:17:40 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/03/demos/mysol_1/ngUnitTestingDemo/src/app/strength/strength.pipe.spec.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/03/demos/mysol_1/ngUnitTestingDemo/src/app/strength/strength.pipe.spec.ts
@@ -1,25 +1,5 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-/*
- * Show Weak, Strong, Unbelievable
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength
- * Example:
- *   {{ 2 |  exponentialStrength}}
- *   formats to: 1024
-*/
-@Pipe({
-  name: 'strength'
+describe('StrengthPipe', () => {
+  it('should display weak if strength is 5', () => {
+    let pipe = new StrengthPipe();
+  })
 })
-export class StrengthPipe implements PipeTransform {
-  transform(value: number): string {
-    if(value < 10) {
-      return value + " (weak)";
-    } else if(value >= 10 && value < 20) {
-      return value + " (strong)";
-    } else {
-      return value + " (unbelievable)";
-    }
-  }
-}
