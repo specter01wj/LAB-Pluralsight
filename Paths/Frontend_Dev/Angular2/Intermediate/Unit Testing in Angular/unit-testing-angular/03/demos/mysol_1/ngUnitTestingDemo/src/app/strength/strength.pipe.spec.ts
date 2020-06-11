cfa53e18bda06b3d84152e962d6ce62b54b129f9
specter01wj <specter01wj@gmail.com>
2020-06-10 23:18:53 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/03/demos/mysol_1/ngUnitTestingDemo/src/app/strength/strength.pipe.spec.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/03/demos/mysol_1/ngUnitTestingDemo/src/app/strength/strength.pipe.spec.ts
@@ -3,5 +3,7 @@ import ( StrengthPipe ) from "./strength.pipe";
 describe('StrengthPipe', () => {
   it('should display weak if strength is 5', () => {
     let pipe = new StrengthPipe();
+
+    expect(pipe.transform(5)).toEqual('5 (weak');
   })
 })
