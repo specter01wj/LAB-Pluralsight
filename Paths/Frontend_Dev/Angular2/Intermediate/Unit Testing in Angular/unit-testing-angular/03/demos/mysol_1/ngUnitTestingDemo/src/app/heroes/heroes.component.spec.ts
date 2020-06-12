524db2e9556c59e276ec84628a0d649fbe341816
specter01wj <specter01wj@gmail.com>
2020-06-12 00:44:10 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/03/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.spec.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/03/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.spec.ts
@@ -29,6 +29,15 @@ describe('HeroesComponent', () => {
       expect(component.heroes.length).toBe(2);
     });
 
+    it('should call deleteHero', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(mockHeroService.deleteHero).toHaveBeenCalled();
+    });
+
   });
 
 
