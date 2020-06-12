5e314b0fa40c9319a17fdf881dd8501ea75ec489
specter01wj <specter01wj@gmail.com>
2020-06-12 00:28:43 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/03/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.spec.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/03/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.spec.ts
@@ -3,6 +3,7 @@ import { HeroesComponent } from "./heroes.component";
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let HEROES;
+  let mockHeroService;
 
   beforeEach(() => {
     HEROES = [
@@ -10,6 +11,8 @@ describe('HeroesComponent', () => {
       {id:2, name: 'Wonderful Woman', strength: 24},
       {id:3, name: 'SuperDude', strength: 55}
     ];
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
 
     component = new HeroesComponent({});
   });
