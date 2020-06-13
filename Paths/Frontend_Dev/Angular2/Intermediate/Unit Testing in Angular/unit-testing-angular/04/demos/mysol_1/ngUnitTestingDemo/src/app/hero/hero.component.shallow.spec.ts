3b31895792c38c6505ffb23d2a2b6ea7262cf96a
specter01wj <specter01wj@gmail.com>
2020-06-12 18:53:34 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/04/demos/mysol_1/ngUnitTestingDemo/src/app/hero/hero.component.shallow.spec.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/04/demos/mysol_1/ngUnitTestingDemo/src/app/hero/hero.component.shallow.spec.ts
@@ -1,12 +1,21 @@
-
-
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { HeroComponent } from "./hero.component";
 
 describe('HeroComponent (shallow tests)', () => {
+	let fixture: ComponentFixture<HeroComponent>;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			declarations: [HeroComponent];
 		});
+
+		fixture = TestBed.createComponent(HeroComponent);
+	});
+
+	it('should have the correct hero', () => {
+		fixture.componentInstance.hero = {id: 1, name: 'SuperDude', strength: 3};
+
+		expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
 	});
 
 });
