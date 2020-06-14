5677bae90f2a63830a6eb01b29cecbfc4a2dda22
specter01wj <specter01wj@gmail.com>
2020-06-14 00:35:14 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/04/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.shallow.spec.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/04/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { HeroesComponent } from "./heroes.component";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { HeroService } from '../hero.service';
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
@@ -8,18 +9,17 @@ describe('HeroesComponent (shallow tests)', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HeroesComponent],
-      providers: [],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     });
 
     fixture = TestBed.createComponent(HeroesComponent);
-  })
+  });
 
-  describe('delete', () => {
+  it('should do nothing', () => {
+    expect(true).toBe(true);
+  });
 
-    it('should do nothing', () => {
-      
-    });
-
-  })
 })
