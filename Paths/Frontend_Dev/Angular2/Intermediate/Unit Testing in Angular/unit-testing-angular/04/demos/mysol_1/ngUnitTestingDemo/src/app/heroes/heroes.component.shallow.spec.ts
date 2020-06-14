3c2bba35d41cad334650b701227f389ca474351e
specter01wj <specter01wj@gmail.com>
2020-06-14 00:41:48 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/04/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.shallow.spec.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/04/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.shallow.spec.ts
@@ -2,12 +2,20 @@ import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { HeroesComponent } from "./heroes.component";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { HeroService } from '../hero.service';
+import { of } from "rxjs";
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
+  let HEROES;
 
   beforeEach(() => {
+    HEROES = [
+      {id:1, name: 'SpiderDude', strength: 8},
+      {id:2, name: 'Wonderful Woman', strength: 24},
+      {id:3, name: 'SuperDude', strength: 55}
+    ];
+
     TestBed.configureTestingModule({
       declarations: [HeroesComponent],
       providers: [
@@ -21,8 +29,11 @@ describe('HeroesComponent (shallow tests)', () => {
     fixture = TestBed.createComponent(HeroesComponent);
   });
 
-  it('should do nothing', () => {
-    expect(true).toBe(true);
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3);
   });
 
 })
