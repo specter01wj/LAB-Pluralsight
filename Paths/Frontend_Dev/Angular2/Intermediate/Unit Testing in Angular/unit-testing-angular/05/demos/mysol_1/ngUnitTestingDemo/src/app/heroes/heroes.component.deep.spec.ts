83110e80e2266e08684deeea55fa1d8d93c083ab
specter01wj <specter01wj@gmail.com>
2020-06-14 22:32:15 -0600

    06/04/2020 v1

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/05/demos/mysol_1/ngUnitTestingDemo/src/app/heroes/heroes.component.deep.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { HeroesComponent } from "./heroes.component";
+import { HeroComponent } from "../hero/hero.component";
+import { NO_ERRORS_SCHEMA, Component, Input } from "@angular/core";
+import { HeroService } from '../hero.service';
+import { of } from "rxjs";
+import { Hero } from "../hero";
+import { By } from "@angular/platform-browser";
+
+describe('HeroesComponent (deep tests)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+
+  beforeEach(() => {
+    HEROES = [
+      {id:1, name: 'SpiderDude', strength: 8},
+      {id:2, name: 'Wonderful Woman', strength: 24},
+      {id:3, name: 'SuperDude', strength: 55}
+    ];
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent,
+        HeroComponent,
+      ],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ],
+    });
+
+    fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+
+})
+
+
