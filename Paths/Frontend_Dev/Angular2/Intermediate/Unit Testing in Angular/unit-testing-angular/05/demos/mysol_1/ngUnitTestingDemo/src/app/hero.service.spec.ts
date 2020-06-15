b79694cc2e9d2cb8893557d200ac0b965660af64
specter01wj <specter01wj@gmail.com>
2020-06-14 23:55:28 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/05/demos/mysol_1/ngUnitTestingDemo/src/app/hero.service.spec.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/05/demos/mysol_1/ngUnitTestingDemo/src/app/hero.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { TestBed, ComponentFixture, inject } from "@angular/core/testing";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { HeroService } from "./hero.service";
 import { MessageService } from "./message.service";
@@ -6,6 +6,7 @@ import { MessageService } from "./message.service";
 describe('HeroService', () => {
   let mockMessageService;
   let httpTestingController: HttpTestingController;
+  let service: HeroService;
 
   beforeEach(() => {
     mockMessageService = jasmine.createSpyObj(['add']);
@@ -19,9 +20,16 @@ describe('HeroService', () => {
     });
 
     httpTestingController = TestBed.get(HttpTestingController);
-    
+    service = TestBed.get(HeroService);
   });
 
+  describe('getHero', () => {
 
+    it('should call get with the correct URL', () => {
+      service.getHero(4).subscribe();
+      const req = httpTestingController.expectNone('api/heroes/4');
+      req.flush({id: 4, name: 'SuperDude', strength: 100});
+    });
+  });
 
 })
