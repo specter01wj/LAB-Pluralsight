2cd67a73bc29f4ac859ac1dabe3cbb362b299bb8
specter01wj <specter01wj@gmail.com>
2020-06-16 19:07:10 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started/04/demos/mysol_1/APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started/04/demos/mysol_1/APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'pm-product-list',
@@ -35,6 +35,15 @@ export class ProductListComponent implements OnInit, OnDestroy {
       next: (products: Product[]) => this.products = products,
       error: (err: any) => this.errorMessage = err.error
     });
+
+    this.store.pipe(select('products')).subscribe(
+      products => {
+        if(products) {
+          this.displayCode = products.showProductCode;
+        }
+      }
+    );
+
   }
 
   ngOnDestroy(): void {
