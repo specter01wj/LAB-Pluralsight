c79dad54f9bca3caa2823426d422a645310fba3f
specter01wj <specter01wj@gmail.com>
2020-06-16 19:37:58 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started/04/demos/mysol_1/APM-Demo0/src/app/user/user.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started/04/demos/mysol_1/APM-Demo0/src/app/user/user.module.ts
@@ -16,7 +16,7 @@ const userRoutes: Routes = [
   imports: [
     SharedModule,
     RouterModule.forChild(userRoutes),
-    StoreModule.forFeature('user', reducer)
+    StoreModule.forFeature('users', reducer)
   ],
   declarations: [
     LoginComponent
