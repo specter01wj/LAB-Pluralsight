60742f00a1b003913fa3529aa342bb8fb398bf20
specter01wj <specter01wj@gmail.com>
2020-06-16 19:10:59 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started/04/demos/mysol_1/APM-Demo0/src/app/products/state/product.reducer.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started/04/demos/mysol_1/APM-Demo0/src/app/products/state/product.reducer.ts
@@ -5,8 +5,6 @@ export function reducer(state, action) {
   switch(action.type) {
     
     case 'TOGGLE_PRODUCT_CODE':
-      console.log(action);
-      console.log(state);
       return {
         ...state,
         showProductCode: action.payload
