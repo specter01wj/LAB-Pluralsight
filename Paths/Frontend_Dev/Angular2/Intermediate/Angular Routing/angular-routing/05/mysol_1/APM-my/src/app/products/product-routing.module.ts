be4b2d8e246159bc4e98dac9dc1e690f4c8c827e
specter01wj <specter01wj@gmail.com>
2020-06-23 00:05:42 -0600

    06/04/2020 v1

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/05/mysol_1/APM-my/src/app/products/product-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+const routes: Routes = [
+	{
+    path: 'products',
+    component: ProductListComponent,
+    children: [
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [
+  ],
+  exports: [RouterModule]
+})
+export class ProductRoutingModule { }
