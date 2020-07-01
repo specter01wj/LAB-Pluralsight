08623391cb636784b32654f94553af7f3a6f6ff2
specter01wj <specter01wj@gmail.com>
2020-07-01 01:27:06 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/06/mysol_1/APM-my/src/app/products/product-edit/product-edit.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/06/mysol_1/APM-my/src/app/products/product-edit/product-edit.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { MessageService } from '../../messages/message.service';
 
-import { Product } from '../product';
+import { Product, ProductResolved } from '../product';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -24,20 +24,29 @@ export class ProductEditComponent implements OnInit {
   ngOnInit(): void {
     /*const id = +this.route.snapshot.paramMap.get('id');
     this.getProduct(id);*/
-    this.route.paramMap.subscribe(
+    /*this.route.paramMap.subscribe(
       params => {
         const id = +params.get('id');
         this.getProduct(id);
       }
-    );
+    );*/
+    /*const resolvedData: ProductResolved =
+      this.route.snapshot.data['resolvedData'];
+    this.errorMessage = resolvedData.error;
+    this.onProductRetrieved(resolvedData.product);*/
+    this.route.data.subscribe(data => {
+      const resolvedData: ProductResolved = data['resolvedData'];
+      this.errorMessage = resolvedData.error;
+      this.onProductRetrieved(resolvedData.product);
+    });
   }
 
-  getProduct(id: number): void {
+  /*getProduct(id: number): void {
     this.productService.getProduct(id).subscribe({
       next: product => this.onProductRetrieved(product),
       error: err => this.errorMessage = err
     });
-  }
+  }*/
 
   onProductRetrieved(product: Product): void {
     this.product = product;
