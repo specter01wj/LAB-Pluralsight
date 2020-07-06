600f99f6bf0af8bfa82f27b2642cedaeca8105d9
specter01wj <specter01wj@gmail.com>
2020-07-05 23:50:36 -0600

    06/04/2020 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/07/mysol_1/APM-my/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/07/mysol_1/APM-my/src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
   get userName(): string {
     if (this.authService.currentUser) {
       return this.authService.currentUser.userName;
+
       
     }
     return '';
