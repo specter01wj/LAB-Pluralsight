5a34cfc70c86a091becbb8a8d3069a2e46daa51a
specter01wj <specter01wj@gmail.com>
2020-07-30 23:25:27 -0400

    06/04/2020 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/demos/After/RxBookTracker/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/demos/After/RxBookTracker/index.ts
@@ -1,11 +1,5 @@
 import { Observable, of, from, fromEvent, concat } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-
-
-
-
-
-
 import { allBooks, allReaders } from './data';
 
 // let allBooksObservable$ = Observable.create(subscriber => {
