9ead465a982d089f5ddc5a2cc58f23807301ae02
Jin Wang <specter01wj@gmail.com>
2020-11-30 02:15:44 -0500

    11/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript Variables and Types/javascript-variables-types/GetYourLoanApp-master/scripts/site.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript Variables and Types/javascript-variables-types/GetYourLoanApp-master/scripts/site.js
@@ -356,7 +356,10 @@ function generateRickProfile(la) {
         riskProfile = "high";
     }
 
-    var summaryText = "Dear " + la.ApplicantName + ", " + reviewText + " Your risk profile is " + riskProfile;
+    // var summaryText = "Dear " + la.ApplicantName + ", " + reviewText + " Your risk profile is " + riskProfile;
+    var summaryText = `Dear ${la.ApplicantName},
+        your application for ${"$", la.LoanAmount}, ${reviewText}.
+        Your risk profile is ${riskProfile}`;
 
     return summaryText;
 }
