d4708416123a1e40f99bd147054b6f007b54f2d6
Jin Wang <specter01wj@gmail.com>
2020-12-04 10:52:15 -0500

    11/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript Variables and Types/javascript-variables-types/02/mysol_1/scripts/site.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript Variables and Types/javascript-variables-types/02/mysol_1/scripts/site.js
@@ -357,12 +357,27 @@ function generateRickProfile(la) {
     }
 
     // var summaryText = "Dear " + la.ApplicantName + ", " + reviewText + " Your risk profile is " + riskProfile;
-    var summaryText = `Dear ${la.ApplicantName},
+    var summaryText = String.raw `Dear ${la.ApplicantName},
         your application for ${"$" + la.LoanAmount}, ${reviewText}.
-        Your risk profile is ${riskProfile}`;
+        Your risk profile is ${riskProfile}
+        Your unique application code is \t${createApplicationId()}`;
 
     return summaryText;
 }
+
+
+function createApplicationId() {
+    var result = '';
+    var characters = 'ABCDEUVYZabcdrswxyz01789/\\#@$%()*^!';
+    var charactersLength = characters.length;
+    for(var i = 0; i < 8; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+
 
 
 
