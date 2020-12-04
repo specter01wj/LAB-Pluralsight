3eb56a2f7a7b04fa8fec19f0880d7a72f87074d2
Jin Wang <specter01wj@gmail.com>
2020-12-04 10:26:13 -0500

    11/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript Variables and Types/javascript-variables-types/02/mysol_1/scripts/site.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript Variables and Types/javascript-variables-types/02/mysol_1/scripts/site.js
@@ -358,7 +358,7 @@ function generateRickProfile(la) {
 
     // var summaryText = "Dear " + la.ApplicantName + ", " + reviewText + " Your risk profile is " + riskProfile;
     var summaryText = `Dear ${la.ApplicantName},
-        your application for ${"$", la.LoanAmount}, ${reviewText}.
+        your application for ${"$" + la.LoanAmount}, ${reviewText}.
         Your risk profile is ${riskProfile}`;
 
     return summaryText;
