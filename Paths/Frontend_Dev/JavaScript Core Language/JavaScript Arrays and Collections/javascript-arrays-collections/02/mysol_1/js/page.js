7692779cdafb391ace9c5a6538cf3932d8edc3f6
Jin Wang <specter01wj@gmail.com>
2020-12-05 19:38:04 -0500

    12/05/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/02/mysol_1/js/page.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/02/mysol_1/js/page.js
@@ -29,3 +29,24 @@ var monthlySalesChart = new Chart(ctx, {
         }
     }
 });
+
+var deptSalesChart = new Chart(pieCtx, {
+    type: 'pie',
+    data: {
+        labels: ['Hiking', 'Running', 'Hunting'],
+        datasets: [{
+            label: '# of Sales',
+            data: [12, 19, 3],
+            backgroundColor: [
+                'rgba(238, 184, 104, 1)',
+                'rgba(75, 166, 223, 1)',
+                'rgba(239, 118, 122, 1)',
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        
+    }
+});
+
