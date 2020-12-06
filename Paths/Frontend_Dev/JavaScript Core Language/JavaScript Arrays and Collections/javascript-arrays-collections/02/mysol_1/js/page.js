f2e2e375a2ba24095851d6462e7a76f5cbe93175
Jin Wang <specter01wj@gmail.com>
2020-12-05 19:44:15 -0500

    12/05/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/02/mysol_1/js/page.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/02/mysol_1/js/page.js
@@ -2,15 +2,22 @@
 let ctx = document.getElementById('monthlySales').getContext('2d');
 let pieCtx = document.getElementById('deptSales').getContext('2d');
 
+let monthlySales = Array.of(12,9,3);
+let monthlyLabels = Array.of('Oct','Nov','Dec');
+
+let deptSales = Array.of(12,9,3);
+let deptLabels = Array.of('Hiking', 'Running', 'Hunting');
 
 
 var monthlySalesChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Oct', 'Nov', 'Dec'],
+        // labels: ['Oct', 'Nov', 'Dec'],
+        labels: monthlyLabels,
         datasets: [{
             label: '# of Sales',
-            data: [12, 19, 3],
+            // data: [12, 19, 3],
+            data: monthlySales,
             backgroundColor: [
                 'rgba(238, 184, 104, 1)',
                 'rgba(75, 166, 223, 1)',
@@ -33,10 +40,12 @@ var monthlySalesChart = new Chart(ctx, {
 var deptSalesChart = new Chart(pieCtx, {
     type: 'pie',
     data: {
-        labels: ['Hiking', 'Running', 'Hunting'],
+        // labels: ['Hiking', 'Running', 'Hunting'],
+        labels: deptLabels,
         datasets: [{
             label: '# of Sales',
-            data: [12, 19, 3],
+            // data: [12, 19, 3],
+            data: deptSales,
             backgroundColor: [
                 'rgba(238, 184, 104, 1)',
                 'rgba(75, 166, 223, 1)',
