7a521159b5c750e334e53c74341a0827364009ac
Jin Wang <specter01wj@gmail.com>
2020-12-05 19:34:03 -0500

    12/05/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/02/mysol_1/js/page.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/02/mysol_1/js/page.js
@@ -2,3 +2,30 @@
 let ctx = document.getElementById('monthlySales').getContext('2d');
 let pieCtx = document.getElementById('deptSales').getContext('2d');
 
+
+
+var monthlySalesChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: monthlyLabels,
+        datasets: [{
+            label: '# of Sales',
+            data: monthlySales,
+            backgroundColor: [
+                'rgba(238, 184, 104, 1)',
+                'rgba(75, 166, 223, 1)',
+                'rgba(239, 118, 122, 1)',
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
