ed21cf8feee1cae8e999b87030c01829fe38b083
Jin Wang <specter01wj@gmail.com>
2020-12-05 19:36:15 -0500

    12/05/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/02/mysol_1/js/page.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/02/mysol_1/js/page.js
@@ -7,10 +7,10 @@ let pieCtx = document.getElementById('deptSales').getContext('2d');
 var monthlySalesChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: monthlyLabels,
+        labels: ['Oct', 'Nov', 'Dec'],
         datasets: [{
             label: '# of Sales',
-            data: monthlySales,
+            data: [12, 19, 3],
             backgroundColor: [
                 'rgba(238, 184, 104, 1)',
                 'rgba(75, 166, 223, 1)',
