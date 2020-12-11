870e869cfb1aabe4745945b28e5415bac602c8fc
Jin Wang <specter01wj@gmail.com>
2020-12-10 23:18:28 -0500

    12/05/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/05/mysol_1/js/page.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript Arrays and Collections/javascript-arrays-collections/05/mysol_1/js/page.js
@@ -7,6 +7,7 @@ var newMonth = document.getElementById('monthId');
 
 
 
+
 let hikingRadio = document.getElementById('hiking');
 let runningRadio = document.getElementById('running');
 let huntingRadio = document.getElementById('hunting');
