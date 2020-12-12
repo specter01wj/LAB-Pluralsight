d3720f71d4ffb5b9bf9bce2447340462b248cb51
Jin Wang <specter01wj@gmail.com>
2020-12-12 02:20:22 -0500

    12/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/index.js
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/index.js
@@ -57,6 +57,7 @@ readline.on('line', async line => {
                 const position = positions.shift();
 
 
+
                 
                 const result = position(...args);
                 return { value: result, done: false };
