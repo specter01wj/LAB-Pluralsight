005dd2d7c5406bf49a5ee7179fda3fa9b4344946
Jin Wang <specter01wj@gmail.com>
2020-12-13 18:43:16 -0500

    12/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_3.js
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_3.js
@@ -5,8 +5,21 @@ const readline = require('readline').createInterface({
 });
 
 
-readline.question('What would you like to log today?', item => {
+/*readline.question('What would you like to log today?', item => {
 	console.log(item);
+	readline.close();
+});*/
+readline.question('What would you like to log today?', async(item) => {
+	const { data } = await axios.get('http://localhost:3001/food');
+	const it = data[Symbol.iterator]();
+	let position = it.next();
+	while(!position.done) {
+		const food = position.value.name;
+		if(food === item) {
+			console.log(`${item} has ${position.value.calories} calories`);
+		}
+		position = it.next();
+	}
 	readline.close();
 });
 
