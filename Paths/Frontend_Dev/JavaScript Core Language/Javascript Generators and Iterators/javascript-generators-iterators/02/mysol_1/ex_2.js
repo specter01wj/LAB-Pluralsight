791e9696d21891886f3fa96e31ff04139559b94d
Jin Wang <specter01wj@gmail.com>
2020-12-13 12:42:42 -0500

    12/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_2.js
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_2.js
@@ -1,39 +1,14 @@
 
-function myIterator(start, finish) {
-  let index = start;
-  let count = 0;
-
-  return {
-    next() {
-      let result;
-      if(index < finish) {
-        result = { value: index, done: false };
-        index++;
-        count++;
-        return result;
-      }
-      return {
-        value: count,
-        done: true
-      }
-    }
-  }
-
-}
+const arr1 = [0, 3, 4, 6];
 
 
+const it = arr1[Symbol.iterator]();
 
-const it = myIterator(0, 10);
-let res = it.next();
-while(!res.done) {
-  console.log(res.value);
-  res = it.next();
-}
-
-
-
-
-
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
 
 
 
