625442ca4d1df5e5ceb83ff0d35c4b0e24da0922
Jin Wang <specter01wj@gmail.com>
2020-12-13 12:40:57 -0500

    12/10/2020 v1

---
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_2.js
@@ -0,0 +1,43 @@
+
+function myIterator(start, finish) {
+  let index = start;
+  let count = 0;
+
+  return {
+    next() {
+      let result;
+      if(index < finish) {
+        result = { value: index, done: false };
+        index++;
+        count++;
+        return result;
+      }
+      return {
+        value: count,
+        done: true
+      }
+    }
+  }
+
+}
+
+
+
+const it = myIterator(0, 10);
+let res = it.next();
+while(!res.done) {
+  console.log(res.value);
+  res = it.next();
+}
+
+
+
+
+
+
+
+
+
+
+
+
