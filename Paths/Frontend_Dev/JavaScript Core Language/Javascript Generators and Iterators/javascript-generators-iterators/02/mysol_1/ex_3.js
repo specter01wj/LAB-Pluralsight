1dc6c859dcaf933ac27668a1a3512378be776918
Jin Wang <specter01wj@gmail.com>
2020-12-13 18:38:41 -0500

    12/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_3.js
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_3.js
@@ -1,8 +1,14 @@
 
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 
-
-
+readline.question('What would you like to log today?', item => {
+	console.log(item);
+	readline.close();
+});
 
 
 
