16c74ebe6df2e39b49548b2d89b7e6b427ce77a2
Jin Wang <specter01wj@gmail.com>
2020-12-13 12:46:31 -0500

    12/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_2.js
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_2.js
@@ -12,6 +12,21 @@ console.log(it.next());
 
 
 
+const map1 = new Map();
+map.set('key1', 'val1');
+map.set('key2', 'val2');
+const mapIterator = map[Symbol.iterator]();
+console.log(mapIterator.next().value);
+console.log(mapIterator.next().value);
+
+for(let [key, value] of map) {
+  console.log(`${key} and ${value}`);
+}
+
+
+
+
+
 
 
 
