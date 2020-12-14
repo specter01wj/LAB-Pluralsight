b08ec86e5cb3763055640ff8442447eddd9d0f9e
Jin Wang <specter01wj@gmail.com>
2020-12-13 23:40:32 -0500

    12/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_6.js
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/02/mysol_1/ex_6.js
@@ -101,6 +101,7 @@ readline.on('line', async line => {
 
 	        const { data } = await axios.get(`http://localhost:3001/users/1`);
 	        const usersLog = data.log || [];
+
 	        
 	        const putBody = {
 	          ...data,
