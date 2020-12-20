55a9a080070678c800185ff5b23a80594a3f2071
Jin Wang <specter01wj@gmail.com>
2020-12-19 18:15:20 -0600

    12/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/03/mysol_1/ex_1.js
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/03/mysol_1/ex_1.js
@@ -1,20 +1,32 @@
 
-function *timestampGenerator() {
+/*function *timestampGenerator() {
   console.log(Date.now());
   yield;
   console.log('exection continued.');
+}*/
+
+function *timestampGenerator1() {
+  let ts = Date.now();
+  var additionalTime = yield;
+  console.log(additionalTime);
+  if(additionalTime) {
+    ts = ts + additionalTime;
+  }
+  console.log(ts);
 }
 
 // timestampGenerator();
-let it1 = timestampGenerator();
+/*let it1 = timestampGenerator();
 it1.next();
-it1.next();
+it1.next();*/
 
 /*for(let val of it1) {
   console.log(val);
 }*/
 
-
+let it2 = timestampGenerator1();
+it2.next();
+it2.next(1000 * 60);
 
 
 
