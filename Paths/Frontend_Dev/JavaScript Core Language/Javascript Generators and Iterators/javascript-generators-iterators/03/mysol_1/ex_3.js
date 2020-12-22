7ee2c5de1c865dd4b98d8b3f6a06e7152de71362
Jin Wang <specter01wj@gmail.com>
2020-12-22 11:37:13 -0500

    12/22/2020 v1

---
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/03/mysol_1/ex_3.js
@@ -0,0 +1,25 @@
+
+function *errorHandling() {
+	try {
+		yield 1;
+		yield 2;
+		yield 3;
+	} catch(error) {
+		console.error(error);
+	}
+}
+
+
+let errorIt1 = errorHandling();
+
+console.log(errorIt1.next());
+errorIt1.throw('error message here! JW');
+console.log(errorIt1.next());
+
+
+
+
+
+
+
+
