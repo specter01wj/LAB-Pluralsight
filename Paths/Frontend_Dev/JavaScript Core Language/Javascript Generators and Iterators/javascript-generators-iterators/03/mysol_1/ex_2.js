f7021526451e567da104d2bdc1c648962156b9cc
Jin Wang <specter01wj@gmail.com>
2020-12-22 02:16:00 -0500

    12/10/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/03/mysol_1/ex_2.js
+++ Paths/Frontend_Dev/JavaScript Core Language/Javascript Generators and Iterators/javascript-generators-iterators/03/mysol_1/ex_2.js
@@ -2,15 +2,22 @@
 function *gen1() {
   yield 1;
   yield 2;
+
+  return 4;
 }
 
 function *gen2() {
-  yield *gen1();
+  let val = yield *gen1();
+  yield 3;
+  yield val;
 }
 
 
 
 let it1 = gen2();
+
+console.log(it1.next());
+console.log(it1.next());
 console.log(it1.next());
 console.log(it1.next());
 
