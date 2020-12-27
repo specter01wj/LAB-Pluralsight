1ec6afcf61b24f2d7694800680c9d13f10a6231e
Jin Wang <specter01wj@gmail.com>
2020-12-27 12:15:51 -0500

    12/25/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/02/mysol_1/ex_1.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/02/mysol_1/ex_1.js
@@ -14,9 +14,21 @@ let message1 = greetings();
 console.log(message1);
 
 
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+let result1 = sum(2, 3);
+console.log(result1);
 
 
 
+function printAll() {
+  for(let i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
 
-
+printAll(1, 2, 3, 4, 5);
+printAll(10, 20);
 
