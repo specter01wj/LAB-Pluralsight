47b90f9eeef3caaf8a5b788ccc84470d63201a18
Jin Wang <specter01wj@gmail.com>
2020-12-27 12:51:49 -0500

    12/25/2020 v1

---
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/03/mysol_1/ex_1.js
@@ -0,0 +1,28 @@
+
+let greet1 = function greetings(name) {
+  return 'Hello ' + name;
+}
+
+let message1 = greet1('James');
+console.log(message1);
+
+
+
+
+let greet2 = (name) => {
+  return 'Hello ' + name;
+}
+
+let message2 = greet2('James2');
+console.log(message2);
+
+
+
+
+
+
+
+
+
+
+
