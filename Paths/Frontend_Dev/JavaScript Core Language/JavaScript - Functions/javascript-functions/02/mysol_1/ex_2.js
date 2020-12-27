a67b047a8f28389a272b6c19c9dbced35180e884
Jin Wang <specter01wj@gmail.com>
2020-12-27 12:22:06 -0500

    12/25/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/02/mysol_1/ex_2.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/02/mysol_1/ex_2.js
@@ -4,12 +4,20 @@ function greeting() {
 }
 
 greeting();
-console.log(message);
+// console.log(message);
 
 
+function greeting2() {
+	let message = 'Hello';
+	let sayHi = function hi() {
+		let message = 'Hi';
+		console.log(message);
+	}
+	sayHi();
+	console.log(message);
+}
 
-
-
+greeting2();
 
 
 
