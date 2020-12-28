7e1125f1208b0421f7718038da485bc7e1262e91
Jin Wang <specter01wj@gmail.com>
2020-12-28 02:30:52 -0500

    12/25/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/05/mysol_1/ex_3.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/05/mysol_1/ex_3.js
@@ -3,10 +3,22 @@ function greet(person1, person2) {
   console.log('Hello ' + person1 + ' and ' + person2);
 }
 
-let names = ('Mary', 'John', 'Tim');
+let names = ['John', 'Tim'];
 
 
 greet(...names);
+
+
+
+
+function display(char1, char2, char3, char4) {
+  console.log(char1, char2, char3, char4);
+}
+
+let letters = 'abcd';
+
+
+display(...letters);
 
 
 
