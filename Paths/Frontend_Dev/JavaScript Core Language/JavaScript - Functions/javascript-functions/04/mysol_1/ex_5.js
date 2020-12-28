0a618d3fc02b20f47f45485dc0ad2de0942c3440
Jin Wang <specter01wj@gmail.com>
2020-12-27 21:37:07 -0500

    12/25/2020 v1

--- Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/04/mysol_1/ex_5.js
+++ Paths/Frontend_Dev/JavaScript Core Language/JavaScript - Functions/javascript-functions/04/mysol_1/ex_5.js
@@ -1,11 +1,31 @@
 
 let x1 = 1, y1 = 2;
+let s1 = 'abc';
 
 console.log(eval('x1 + y1 + 1'));
 
+console.log(eval('x1 + y1 + s1'));
 
 
 
+console.log(parseInt('F', 16));
+console.log(parseInt('15', 10));
+console.log(parseInt('Hi', 10));
+
+
+console.log(parseFloat('3.99'));
+console.log(parseFloat('3.99e-1'));
+console.log(parseFloat(''));
+
+
+console.log(escape('text'));
+console.log(escape(' '));
+console.log(escape('abc&%'));
+
+
+console.log(unescape('text'));
+console.log(unescape('%20'));
+console.log(unescape('abc%26%25'));
 
 
 
