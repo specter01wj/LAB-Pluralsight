f8232ddeb93407caec68d991ae8033073a2b17bc
Jin Wang <specter01wj@gmail.com>
2021-01-01 17:21:54 -0500

    01/01/2021 v1

--- Paths/Frontend_Dev/Typescript Core Language/Beginner/TypeScript - Getting Started/getting-started-typescript/03/mysol_1/app/app.ts
+++ Paths/Frontend_Dev/Typescript Core Language/Beginner/TypeScript - Getting Started/getting-started-typescript/03/mysol_1/app/app.ts
@@ -4,4 +4,8 @@ function startGame() {
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
 }
 
+function logPlayer(name) {
+	console.log(`New game starting for player: ${name}`);
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
