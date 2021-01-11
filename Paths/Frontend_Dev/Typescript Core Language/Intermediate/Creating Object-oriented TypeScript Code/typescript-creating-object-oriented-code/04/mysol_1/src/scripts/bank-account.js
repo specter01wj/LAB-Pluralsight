b9e0f688399231d657413500a3569a075c5379dd
Jin Wang <specter01wj@gmail.com>
2021-01-11 00:27:09 -0500

    01/01/2021 v1

---
+++ Paths/Frontend_Dev/Typescript Core Language/Intermediate/Creating Object-oriented TypeScript Code/typescript-creating-object-oriented-code/04/mysol_1/src/scripts/bank-account.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BankAccount = void 0;
+var BankAccount = /** @class */ (function () {
+    function BankAccount(accountSettings) {
+        this._balance = 0;
+        this.id = accountSettings.id;
+        this.title = accountSettings.title;
+        this.balance = accountSettings.balance;
+    }
+    BankAccount.prototype.deposit = function (amount) {
+        this.balance += amount;
+    };
+    BankAccount.prototype.withdrawal = function (amount) {
+        this.balance -= amount;
+    };
+    Object.defineProperty(BankAccount.prototype, "balance", {
+        get: function () {
+            return this._balance;
+        },
+        set: function (val) {
+            if (val >= 0) {
+                this._balance = val;
+            }
+            else {
+                throw Error('Balance cannot be negative!');
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return BankAccount;
+}());
+exports.BankAccount = BankAccount;
+//# sourceMappingURL=bank-account.js.map
