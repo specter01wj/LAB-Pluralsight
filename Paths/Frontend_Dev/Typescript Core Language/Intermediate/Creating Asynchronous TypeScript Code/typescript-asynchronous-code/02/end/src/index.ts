e8e6165d12341cb1b72765f5ede162231e41744d
Jin Wang <specter01wj@gmail.com>
2021-01-14 00:24:54 -0500

    01/01/2021 v1

---
+++ Paths/Frontend_Dev/Typescript Core Language/Intermediate/Creating Asynchronous TypeScript Code/typescript-asynchronous-code/02/end/src/index.ts
@@ -0,0 +1,33 @@
+import './design/index.scss';
+import { showMessage } from './lib';
+
+document.querySelector('#bake-cookies').addEventListener('click', async () => {
+  bake();
+});
+
+async function bake() {
+  const title = 'Baking cookies';
+  let counter = 0;
+  console.clear();
+  console.group(title);
+
+  counter++;
+  console.log(`${counter} - Add ingredients`);
+  showMessage(`${counter} - Add ingredients`, title);
+
+  counter++;
+  console.log(`${counter} - Mix ingredients`);
+  showMessage(`${counter} - Mix ingredients`, title, true);
+
+  setTimeout(() => {
+    counter++;
+    console.log(`${counter} - Bake at 325 degrees for 10 minutes`);
+    showMessage(`${counter} - Bake at 325 degrees for 10 minutes`, title, true);
+  }, 1000);
+
+  counter++;
+  console.log(`${counter} - Eat cookies`);
+  showMessage(`${counter} - Eat cookies`, title, true);
+
+  console.groupEnd();
+}
