c2afe173c0b6e0216745b50947a71ab061dce30f
Jin Wang <specter01wj@gmail.com>
2021-01-20 18:56:17 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Typescript Core Language/Intermediate/Configuring, Compiling, and Debugging TypeScript Projects/typescript-projects-configuring-compiling-debugging/02/mysol_1/server/server.ts
+++ Paths/Frontend_Dev/Typescript Core Language/Intermediate/Configuring, Compiling, and Debugging TypeScript Projects/typescript-projects-configuring-compiling-debugging/02/mysol_1/server/server.ts
@@ -10,6 +10,7 @@ const port : string | number = process.env.port || 1337;
 const app = express();
 
 app.listen(port);
+app.use(express.static('public'));
 
 console.log("Listening on port " + port);
 
