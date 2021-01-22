a7655a4f1cde45f2c6549dfb1e627d1f9eed4b1d
Jin Wang <specter01wj@gmail.com>
2021-01-22 17:07:32 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/05/mysol_3/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/05/mysol_3/index.ts
@@ -4,10 +4,14 @@ let nums = [2, 4, 6, 8, 10];
 
 let numsObervable$ = from(nums);
 
+let observer = {
+	next: (value) => console.log(value),
+	error: (error) => console.log(`ERROR: ${error}`),
+	complete: (comp) => console.log(`All done: ${comp}`)
+};
 
 
-
-
+numsObervable$.subscribe(observer);
 
 
 
