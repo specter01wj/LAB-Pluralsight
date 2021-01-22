f0e721ea4af394f07e92f6a0ee10b8c812578e6b
Jin Wang <specter01wj@gmail.com>
2021-01-22 17:57:51 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/05/mysol_4/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/05/mysol_4/index.ts
@@ -1,9 +1,16 @@
 import { Observable, from, fromEvent } from 'rxjs';
 
+let clicks$ = fromEvent(document, 'click');
 
 
+let observer = {
+	next: (value) => console.log(value),
+	error: (error) => console.log(`ERROR: ${error}`),
+	complete: () => console.log(`All done!`)
+};
 
 
+clicks$.subscribe(observer);
 
 
 
