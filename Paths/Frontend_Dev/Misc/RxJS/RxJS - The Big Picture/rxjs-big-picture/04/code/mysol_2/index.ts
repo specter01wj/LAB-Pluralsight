e8c7b6c4686d2068b2e750472e42907c25f11423
Jin Wang <specter01wj@gmail.com>
2021-01-22 17:22:44 -0500

    01/20/2021 v1

---
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/04/code/mysol_2/index.ts
@@ -0,0 +1,27 @@
+import { Observable, from } from 'rxjs';
+
+let bookArray = [
+	{ "bookID": 1, "title": "Goodnight Moon" },
+	{ "bookID": 2, "title": "Winnie-the-Pooh" },
+	{ "bookID": 3, "title": "Where the Wild Things Are" },
+];
+
+let booksObservable$ = from(bookArray);
+
+let allBooks = [];
+
+// let element1 = document.getElementById('t1');
+
+booksObservable$.subscribe(
+	(data: any) => {
+		allBooks = data;
+		console.log(allBooks);
+		// element1.innerHTML = allBooks[0];
+	},
+    (err) => console.log(err),
+    () => console.log('All done getting books.')
+);
+
+
+
+
