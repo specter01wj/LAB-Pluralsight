730255e61541eb6cde32dc949c54d37cefb2826d
Jin Wang <specter01wj@gmail.com>
2021-01-21 23:26:31 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/04/code/mysol_1/arrays.js
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/04/code/mysol_1/arrays.js
@@ -1,27 +1,13 @@
-import { Observable, from } from 'rxjs';
-
-let bookArray = [
-	{ "bookID": 1, "title": "Goodnight Moon" },
-	{ "bookID": 2, "title": "Winnie-the-Pooh" },
-	{ "bookID": 3, "title": "Where the Wild Things Are" },
+"use strict";
+exports.__esModule = true;
+var rxjs_1 = require("rxjs");
+var bookArray = [
+    { "bookID": 1, "title": "Goodnight Moon" },
+    { "bookID": 2, "title": "Winnie-the-Pooh" },
+    { "bookID": 3, "title": "Where the Wild Things Are" },
 ];
-
-let booksObservable$ = from(bookArray);
-
-let allBooks = [];
-
-booksObservable$.subscribe(
-	(data) => allBooks = data,
-    (err) => console.log(err),
-    () => console.log('All done getting books.')
-);
-
-
-
-
-
-
-
-
-
-
+var booksObservable$ = rxjs_1.from(bookArray);
+var allBooks = [];
+booksObservable$.subscribe(function (data) { return allBooks = data; }, function (err) { return console.log(err); }, function () { return console.log('All done getting books.'); });
+var element1 = document.getElementById('t1');
+element1.innerHTML = allBooks[0];
---
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - The Big Picture/rxjs-big-picture/04/code/mysol_1/arrays.ts
@@ -0,0 +1,32 @@
+import { Observable, from } from 'rxjs';
+
+let bookArray = [
+	{ "bookID": 1, "title": "Goodnight Moon" },
+	{ "bookID": 2, "title": "Winnie-the-Pooh" },
+	{ "bookID": 3, "title": "Where the Wild Things Are" },
+];
+
+let booksObservable$ = from(bookArray);
+
+let allBooks = [];
+
+let element1 = document.getElementById('t1');
+
+booksObservable$.subscribe(
+	(data) => {
+		allBooks = data;
+		element1.innerHTML = allBooks[0];
+	},
+    (err) => console.log(err),
+    () => console.log('All done getting books.')
+);
+
+
+
+
+
+
+
+
+
+
