856dc3021103a0bf0adef72c1d970d4874b62ba9
Jin Wang <specter01wj@gmail.com>
2021-01-23 21:43:39 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/mysol_1/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/mysol_1/index.ts
@@ -2,13 +2,21 @@ import { Observable } from 'rxjs';
 import { allBooks } from './data';
 
 
+function subscribe(subscriber) {
+
+	for(let book of allBooks) {
+		subscriber.next(book);
+	}
+
+
+
+}
 
 
 
 
 
-
-
+let allBooksObervable$ = new Observable(subscribe);
 
 
 
