fdd413ce7cb3230e5ed4e0e914ec99026d5535df
Jin Wang <specter01wj@gmail.com>
2021-01-23 22:13:20 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/mysol_2/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/mysol_2/index.ts
@@ -2,8 +2,14 @@ import { Observable, of, from, fromEvent } from 'rxjs';
 import { allBooks, allReaders } from './data';
 
 
-let observer = {
+let observer1 = {
 	next: (value) => console.log(value),
+	error: (error) => console.log(`ERROR: ${error}`),
+	complete: () => console.log(`All done!`)
+};
+
+let observer2 = {
+	next: (value) => console.log(value.title),
 	error: (error) => console.log(`ERROR: ${error}`),
 	complete: () => console.log(`All done!`)
 };
@@ -11,12 +17,12 @@ let observer = {
 
 let source1$ = of('hello', 10, true, allReaders[0].name);
 
-source1$.subscribe(observer);
+source1$.subscribe(observer1);
 
 
-let source2$ = of(allBooks);
+let source2$ = from(allBooks);
 
-source2$.subscribe(observer);
+source2$.subscribe(observer2);
 
 
 
