6ece6c361a0deb66da849ec459682a5896793999
Jin Wang <specter01wj@gmail.com>
2021-01-23 22:38:56 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/mysol_4/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/mysol_4/index.ts
@@ -1,14 +1,19 @@
 import { Observable, of, from, fromEvent, concat } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
 
 let observer = {
 	next: (value) => {
-		console.log(value);
-		let readersDiv = document.getElementById('readers');
+		ajax('/api/readers')
+			.subscribe(ajaxRes => {
+				console.log(ajaxRes);
+			});
+		
+		/*let readersDiv = document.getElementById('readers');
 		for(let reader of allReaders) {
 			readersDiv.innerHTML += reader.name + '<br>';
-		}
+		}*/
 	},
 	error: (error) => console.log(`ERROR: ${error}`),
 	complete: () => console.log(`All done!`)
