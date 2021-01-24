5b92b4a3017fe4f97e273df8cdf80e6a27bf2c25
Jin Wang <specter01wj@gmail.com>
2021-01-24 13:24:14 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/04/mysol_1/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/04/mysol_1/index.ts
@@ -3,72 +3,28 @@ import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
 
-//#region Creating Observables
-
-// let allBooksObservable$ = Observable.create(subscriber => {
-
-//   if (document.title !== 'RxBookTracker') {
-//     subscriber.error('Incorrect page title.');
-//   }
-
-//   for (let book of allBooks) {
-//     subscriber.next(book);
-//   }
-
-//   setTimeout(() => {
-//     subscriber.complete();
-//   }, 2000);
-
-//   return () => console.log('Executing teardown code.');
-  
-// });
-
-// allBooksObservable$.subscribe(book => console.log(book.title));
+let observer = {
+	next: (value) => {
+		ajax('/api/readers')
+			.subscribe(ajaxRes => {
+				console.log(ajaxRes);
+				let readers = ajaxRes.response;
+				let readersDiv = document.getElementById('readers');
+				for(let reader of readers) {
+					readersDiv.innerHTML += reader.name + '<br>';
+				}
+			});
+	},
+	error: (error) => console.log(`ERROR: ${error}`),
+	complete: () => console.log(`All done!`)
+};
 
 
-// let source1$ = of('hello', 10, true, allReaders[0].name);
+let button = document.getElementById('readersBtn');
 
-// //source1$.subscribe(value => console.log(value));
-
-// let source2$ = from(allBooks);
-
-// //source2$.subscribe(book => console.log(book.title));
-
-// concat(source1$, source2$)
-//   .subscribe(value => console.log(value));
+let clicks$ = fromEvent(button, 'click');
 
 
-// let button = document.getElementById('readersButton');
+clicks$.subscribe(observer);
 
-// fromEvent(button, 'click')
-//   .subscribe(event => {
-//     console.log(event);
-
-//     let readersDiv = document.getElementById('readers');
-
-//     for (let reader of allReaders) {
-//       readersDiv.innerHTML += reader.name + '<br>';
-//     }
-//   });
-
-
-// let button = document.getElementById('readersButton');
-
-// fromEvent(button, 'click')
-//   .subscribe(event => {
-//     ajax('/api/readers')
-//       .subscribe(ajaxResponse => {
-//         console.log(ajaxResponse);
-//         let readers = ajaxResponse.response;
-
-//         let readersDiv = document.getElementById('readers');
-
-//         for (let reader of readers) {
-//           readersDiv.innerHTML += reader.name + '<br>';
-//         }
-
-//       });
-//   });
-
-//#endregion
 
