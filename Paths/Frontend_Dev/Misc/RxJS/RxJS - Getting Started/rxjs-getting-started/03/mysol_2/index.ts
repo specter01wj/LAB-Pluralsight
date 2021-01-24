1b9c9788bb211a2b84695c4063d7f03a1641026d
Jin Wang <specter01wj@gmail.com>
2021-01-23 22:09:48 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/mysol_2/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/03/mysol_2/index.ts
@@ -5,7 +5,7 @@ import { allBooks, allReaders } from './data';
 let source1$ = of('hello', 10, true, allReaders[0].name);
 
 let observer = {
-	next: (value) => console.log(value.title),
+	next: (value) => console.log(value),
 	error: (error) => console.log(`ERROR: ${error}`),
 	complete: () => console.log(`All done!`)
 };
