e697efc41f2beae021a72b6cfc81019b5a17b67d
Jin Wang <specter01wj@gmail.com>
2021-01-24 23:21:25 -0500

    01/20/2021 v1

---
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/05/mysol_3/index.ts
@@ -0,0 +1,30 @@
+import { Observable, of, from, fromEvent, concat, interval } from 'rxjs';
+import { mergeMap, map, filter, tap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { allBooks, allReaders } from './data';
+
+
+
+ajax('/api/books')
+	.pipe(
+		mergeMap(ajaxRes => ajaxRes.response),
+		filter(book => book.publicationYear < 1950),
+		tap(oldBook => console.log(`Title: ${oldBook.title}`))
+	)
+	.subscribe(
+		finalValue => console.log(finalValue)
+	);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
