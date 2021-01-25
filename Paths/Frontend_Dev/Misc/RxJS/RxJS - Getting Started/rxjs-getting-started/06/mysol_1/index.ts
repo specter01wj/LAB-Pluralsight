40ae1fe495502c968a612075074302397f01b8ee
Jin Wang <specter01wj@gmail.com>
2021-01-25 10:11:17 -0500

    01/20/2021 v1

---
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/06/mysol_1/index.ts
@@ -0,0 +1,45 @@
+import { Observable, of, from, fromEvent, concat, interval, throwError } from 'rxjs';
+import { mergeMap, map, filter, tap, catchError, take, takeUntil } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { allBooks, allReaders } from './data';
+
+
+
+let timesDiv = document.getElementById('times');
+let button = document.getElementById('timerBtn');
+
+let timer$ = new Observable(subscriber => {
+  let i = 0;
+  let intervalID = setInterval(() => {
+    subscriber.next(i++);
+  }, 1000);
+
+  return () => {
+    console.log('Executing teardown code.');
+    clearInterval(intervalID);
+  }
+});
+
+
+let cancelTimer$ = fromEvent(button, 'click');
+
+
+let timerSubscription = timer$.pipe(
+	// take(3)
+	takeUntil(cancelTimer$)
+)
+.subscribe(
+	value => timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br>`,
+	null,
+	() => console.log('All Done!')
+);
+
+
+
+
+
+
+
+
+
+
