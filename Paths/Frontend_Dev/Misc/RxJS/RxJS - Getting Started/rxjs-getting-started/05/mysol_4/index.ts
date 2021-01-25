70398b5772709efd5f65996326c19bb605eaccae
Jin Wang <specter01wj@gmail.com>
2021-01-24 23:39:40 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/05/mysol_4/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/05/mysol_4/index.ts
@@ -1,28 +1,30 @@
 import { Observable, of, from, fromEvent, concat, interval, throwError } from 'rxjs';
-import { mergeMap, map, filter, tap, catchError } from 'rxjs/operators';
+import { mergeMap, map, filter, tap, catchError, take, takeUntil } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
 
 
-// ajax('/api/books')
-ajax('/api/errors/500')
-	.pipe(
-		mergeMap(ajaxRes => ajaxRes.response),
-		filter(book => book.publicationYear < 1950),
-		tap(oldBook => console.log(`Title: ${oldBook.title}`)),
-		// catchError(err => of({title: 'Corduroy', author: 'James Wang'}))
-		// catchError(err => throw `Something bad happened - ${err.message}` )
-		catchError(err => return throwError(err.message) )
-	)
-	.subscribe(
-		finalValue => console.log(`VALUE: ${finalValue.title}`),
-		error => console.log(`ERROR: ${error}`)
-	);
+let timesDiv = document.getElementById('times');
+let button = document.getElementById('timerButton');
 
+let timer$ = new Observable(subscriber => {
+  let i = 0;
+  let intervalID = setInterval(() => {
+    subscriber.next(i++);
+  }, 1000);
 
+  return () => {
+    console.log('Executing teardown code.');
+    clearInterval(intervalID);
+  }
+});
 
-
+let timerSubscription = timer$.subscribe(
+	value => timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br>`,
+	null,
+	() => console.log('All Done!')
+);
 
 
 
