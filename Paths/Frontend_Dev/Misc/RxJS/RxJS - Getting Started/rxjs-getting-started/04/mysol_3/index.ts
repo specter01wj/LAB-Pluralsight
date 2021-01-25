67d2615d3fd39be6bf46d8db50e441c14319d6e4
Jin Wang <specter01wj@gmail.com>
2021-01-24 19:58:15 -0500

    01/20/2021 v1

---
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/04/mysol_3/index.ts
@@ -0,0 +1,64 @@
+import { Observable, of, from, fromEvent, concat } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { allBooks, allReaders } from './data';
+
+
+let books$ = from(allBooks);
+
+
+function timeSubscribe(subscriber) {
+	const timeString = new Date().toLocaleTimeString();
+
+	if(timeString === null) {
+		subscriber.error('Incorrect timeString.');
+	}
+
+	subscriber.next(timeString);
+
+	setTimeout(() => {
+		subscriber.complete();
+	}, 2000);
+
+
+}
+
+
+let timeObserver = {
+	next: (value) => console.log(`Observer ${cnt}: ${value}`),
+	error: (error) => console.log(`ERROR: ${error}`),
+	complete: () => console.log(`All done!`)
+};
+
+
+let currentTime$ = new Observable(timeSubscribe);
+
+
+let cnt = 1;
+currentTime$.subscribe(timeObserver);
+
+
+
+setTimeout(() => {
+	cnt++;
+	currentTime$.subscribe(timeObserver);
+}, 1000);
+
+
+setTimeout(() => {
+	cnt++;
+	currentTime$.subscribe(timeObserver);
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
