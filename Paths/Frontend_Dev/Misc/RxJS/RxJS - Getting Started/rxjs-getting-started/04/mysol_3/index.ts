ede5410d554fd490b8b23048c7ba0cf97500e432
Jin Wang <specter01wj@gmail.com>
2021-01-24 20:05:24 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/04/mysol_3/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/04/mysol_3/index.ts
@@ -1,4 +1,4 @@
-import { Observable, of, from, fromEvent, concat } from 'rxjs';
+import { Observable, of, from, fromEvent, concat, interval } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
@@ -6,56 +6,18 @@ import { allBooks, allReaders } from './data';
 let timesDiv = document.getElementById('times');
 let button = document.getElementById('timerBtn');
 
-
-let books$ = from(allBooks);
-
-
-function timeSubscribe(subscriber) {
-	const timeString = new Date().toLocaleTimeString();
-
-	if(timeString === null) {
-		subscriber.error('Incorrect timeString.');
-	}
-
-	subscriber.next(timeString);
-
-	setTimeout(() => {
-		subscriber.complete();
-	}, 2000);
+let timer$ = interval(1000);
 
 
-}
-
-
-let timeObserver = {
-	next: (value) => console.log(`Observer ${cnt}: ${value}`),
+let timerObserver = {
+	next: (value) => timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br>`,
 	error: (error) => console.log(`ERROR: ${error}`),
 	complete: () => console.log(`All done!`)
 };
 
 
-let currentTime$ = new Observable(timeSubscribe);
 
-
-let cnt = 1;
-currentTime$.subscribe(timeObserver);
-
-
-
-setTimeout(() => {
-	cnt++;
-	currentTime$.subscribe(timeObserver);
-}, 1000);
-
-
-setTimeout(() => {
-	cnt++;
-	currentTime$.subscribe(timeObserver);
-}, 2000);
-
-
-
-
+timer$.subscribe(timerObserver);
 
 
 
