1e824c38d291a9a364b95766b2a649e80146d178
Jin Wang <specter01wj@gmail.com>
2021-01-24 21:30:09 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/05/mysol_2/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/05/mysol_2/index.ts
@@ -1,32 +1,28 @@
 import { Observable, of, from, fromEvent, concat, interval } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { mergeMap, map, filter, tap } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
 
-let source$ = of(1, 2, 3, 4, 5);
 
-/*let doubler = map(value => value * 2);
-
-let doubled$ = doubler(source$);
-
-doubled$.subscribe(
-	value => console.log(value)
-);*/
-
-/*source$
-	.map(value => value * 2)
-	.filter(mappedValue => mappedValue > 5)
+ajax('/api/books')
+	.pipe(
+		mergeMap(ajaxRes => ajaxRes.response)
+	)
 	.subscribe(
 		finalValue => console.log(finalValue)
-	);*/
+	);
 
 
-source$.pipe(
-	map(value => value * 2),
-	filter(mappedValue => mappedValue > 5)
-)
-.subscribe(
-	finalValue => console.log(finalValue)
-);
+
+
+
+
+
+
+
+
+
+
+
 
