1068d55fc2f8f01d4daeab87a24f142c75e58055
Jin Wang <specter01wj@gmail.com>
2021-01-26 16:05:37 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/06/mysol_1/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/06/mysol_1/index.ts
@@ -5,56 +5,6 @@ import { allBooks, allReaders } from './data';
 
 
 
-function grabAndLogClassics(year, log) {
-  return source$ => {
-    return new Observable(subscriber => {
-      return source$.subscribe(
-        book => {
-          if(book.publicationYear < year) {
-            subscriber.next(book);
-            if(log) {
-              console.log(`Classic: ${book.title}`);
-            }
-          }
-        },
-        err => subscriber.error(err),
-        () => subscriber.complete()
-      );
-    });
-  }
-}
-
-
-
-function grabClassics(year) {
-  return filter(book => book.publicationYear < year);
-}
-
-
-function grabAndLogClassicsWithPipe(year, log) {
-  return source$ => source$.pipe(
-    filter(book => book.publicationYear < year),
-    tap(classicBook => log ? console.log(`Title: ${classicBook.title}`) : null)
-  );
-}
-
-
-ajax('/api/books')
-  .pipe(
-    mergeMap(ajaxRes => ajaxRes.response),
-    // filter(book => book.publicationYear < 1950),
-    // tap(oldBook => console.log(`Title: ${oldBook.title}`)),
-    // grabAndLogClassics(1940, false)
-    // grabClassics(1950)
-    grabAndLogClassicsWithPipe(1950, true)
-  )
-  .subscribe(
-    finalValue => console.log(`VALUE: ${finalValue.title}`),
-    error => console.log(`ERROR: ${error}`)
-  );
-
-
-
 
 
 
