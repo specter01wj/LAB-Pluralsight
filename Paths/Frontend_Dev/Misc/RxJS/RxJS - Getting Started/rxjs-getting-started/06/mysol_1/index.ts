24d55b1f3afb7b3d0b4bda3a093a778b5c5be2f7
Jin Wang <specter01wj@gmail.com>
2021-01-26 02:03:45 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/06/mysol_1/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/06/mysol_1/index.ts
@@ -8,7 +8,7 @@ import { allBooks, allReaders } from './data';
 function grabAndLogClassics(year, log) {
   return source$ => {
     return new Observable(subscriber => {
-      source$.subscribe(
+      return source$.subscribe(
         book => {
           if(book.publicationYear < year) {
             subscriber.next(book);
@@ -26,26 +26,17 @@ function grabAndLogClassics(year, log) {
 
 
 
-
-
-
-
-
-
 ajax('/api/books')
   .pipe(
     mergeMap(ajaxRes => ajaxRes.response),
-    filter(book => book.publicationYear < 1950),
-    tap(oldBook => console.log(`Title: ${oldBook.title}`)),
+    // filter(book => book.publicationYear < 1950),
+    // tap(oldBook => console.log(`Title: ${oldBook.title}`)),
+    grabAndLogClassics(1940, false)
   )
   .subscribe(
     finalValue => console.log(`VALUE: ${finalValue.title}`),
     error => console.log(`ERROR: ${error}`)
   );
-
-
-
-
 
 
 
