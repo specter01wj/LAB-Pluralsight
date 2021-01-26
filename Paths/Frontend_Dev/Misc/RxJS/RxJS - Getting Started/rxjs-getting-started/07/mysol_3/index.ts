1d3fcbb11d18cdd0eb3bbd3cbe3c7fff18421590
Jin Wang <specter01wj@gmail.com>
2021-01-26 18:49:17 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/07/mysol_3/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/07/mysol_3/index.ts
@@ -1,20 +1,22 @@
 import { Observable, of, from, fromEvent, concat, interval, throwError, Subject } from 'rxjs';
-import { mergeMap, map, filter, tap, catchError, take, takeUntil } from 'rxjs/operators';
+import { mergeMap, map, filter, tap, catchError, take, takeUntil,
+				 multicast, refCount, publish, share } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
 
 
 let source$ = interval(1000).pipe(
-	take(4)
+	take(4),
+	multicast(new Subject())
 );
 
 
-let subject$ = new Subject();
-source$.subscribe(subject$);
+// let subject$ = new Subject();
+// source$.subscribe(subject$);
 
 
-/*source$.subscribe(
+source$.subscribe(
   value => console.log(`Observer 1: ${value}`),
 );
 
@@ -29,32 +31,13 @@ setTimeout(() => {
 
 setTimeout(() => {
 	source$.subscribe(
-	  value => console.log(`Observer 3: ${value}`),
-	);
-}, 2000);*/
-
-subject$.subscribe(
-  value => console.log(`Observer 1: ${value}`),
-);
-
-
-
-setTimeout(() => {
-	subject$.subscribe(
-	  value => console.log(`Observer 2: ${value}`),
-	);
-}, 1000);
-
-
-setTimeout(() => {
-	subject$.subscribe(
 	  value => console.log(`Observer 3: ${value}`),
 	);
 }, 2000);
 
 
 
-
+source$.connect();
 
 
 
