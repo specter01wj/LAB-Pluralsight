1a91805233c9e4b4aa657acd71fbfdde2ae3e4d3
Jin Wang <specter01wj@gmail.com>
2021-01-26 16:15:35 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/07/mysol_1/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/07/mysol_1/index.ts
@@ -1,60 +1,23 @@
-import { Observable, of, from, fromEvent, concat, interval, throwError } from 'rxjs';
+import { Observable, of, from, fromEvent, concat, interval, throwError, Subject } from 'rxjs';
 import { mergeMap, map, filter, tap, catchError, take, takeUntil } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
 
 
-function grabAndLogClassics(year, log) {
-  return source$ => {
-    return new Observable(subscriber => {
-      return source$.subscribe(
-        book => {
-          if(book.publicationYear < year) {
-            subscriber.next(book);
-            if(log) {
-              console.log(`Classic: ${book.title}`);
-            }
-          }
-        },
-        err => subscriber.error(err),
-        () => subscriber.complete()
-      );
-    });
-  }
-}
+let subject$ = new Subject();
 
 
+subject$.subscribe(
+  value => console.log(`Observer 1: ${value}`),
+);
 
-function grabClassics(year) {
-  return filter(book => book.publicationYear < year);
-}
-
-
-function grabAndLogClassicsWithPipe(year, log) {
-  return source$ => source$.pipe(
-    filter(book => book.publicationYear < year),
-    tap(classicBook => log ? console.log(`Title: ${classicBook.title}`) : null)
-  );
-}
+subject$.subscribe(
+  value => console.log(`Observer 1: ${value}`),
+);
 
 
-ajax('/api/books')
-  .pipe(
-    mergeMap(ajaxRes => ajaxRes.response),
-    // filter(book => book.publicationYear < 1950),
-    // tap(oldBook => console.log(`Title: ${oldBook.title}`)),
-    // grabAndLogClassics(1940, false)
-    // grabClassics(1950)
-    grabAndLogClassicsWithPipe(1950, true)
-  )
-  .subscribe(
-    finalValue => console.log(`VALUE: ${finalValue.title}`),
-    error => console.log(`ERROR: ${error}`)
-  );
-
-
-
+subject$.next('Hello!');
 
 
 
