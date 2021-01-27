1a5dd499a9d4fc22431ea5280908bac0f37f46b0
Jin Wang <specter01wj@gmail.com>
2021-01-27 02:33:35 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/08/mysol_1/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/08/mysol_1/index.ts
@@ -1,4 +1,5 @@
-import { Observable, of, from, fromEvent, concat, interval, throwError, Subject } from 'rxjs';
+import { Observable, of, from, fromEvent, concat, interval, throwError, Subject,
+				 asyncScheduler, asapScheduler, queueScheduler, merge } from 'rxjs';
 import { mergeMap, map, filter, tap, catchError, take, takeUntil,
 				 multicast, refCount, publish, share,
 				 publishLast, publishBehavior, publishReplay } from 'rxjs/operators';
@@ -7,54 +8,17 @@ import { allBooks, allReaders } from './data';
 
 
 
-let source$ = interval(1000).pipe(
-	take(4),
-	// multicast(new Subject()),
-	// publish(),
-	// publishLast(),
-	// publishBehavior(42),
-	publishReplay(),
-	refCount(),
-	// share()
-);
+console.log('Start script.');
 
+let queue$ = of('QueueScheduler (synchronous)', queueScheduler);
 
-// let subject$ = new Subject();
-// source$.subscribe(subject$);
+let asap$ = of('AsapScheduler (async micro task)', asapScheduler);
 
-
-source$.subscribe(
-  value => console.log(`Observer 1: ${value}`),
-);
+let async$ = of('AsyncScheduler (async task)', asyncScheduler);
 
 
 
-setTimeout(() => {
-	source$.subscribe(
-	  value => console.log(`Observer 2: ${value}`),
-	);
-}, 1000);
-
-
-setTimeout(() => {
-	source$.subscribe(
-	  value => console.log(`Observer 3: ${value}`),
-	);
-}, 2000);
-
-
-setTimeout(() => {
-	source$.subscribe(
-	  value => console.log(`Observer 4: ${value}`),
-	  null,
-	  () => console.log('Observer 4 complete.')
-	);
-}, 5000);
-
-
-
-// source$.connect();
-
+console.log('End script.');
 
 
 
