375b965088bc5a3010320164d117984157aad983
Jin Wang <specter01wj@gmail.com>
2021-01-27 18:26:08 -0500

    01/20/2021 v1

---
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/09/mysol_1/index.ts
@@ -0,0 +1,26 @@
+import { Observable, of, from, fromEvent, concat, interval, throwError, Subject,
+				 asyncScheduler, asapScheduler, queueScheduler, merge } from 'rxjs';
+import { mergeMap, map, filter, tap, catchError, take, takeUntil,
+				 multicast, refCount, publish, share,
+				 publishLast, publishBehavior, publishReplay,
+				 observeOn } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { allBooks, allReaders } from './data';
+
+
+
+console.log('Start script.');
+
+from([1,2,3,4], queueScheduler).pipe(
+  tap(value => console.log(`Value: ${value}`)),
+  observeOn(asyncScheduler),
+  tap(value => console.log(`Doubled value: ${value * 2}`))
+)
+.subscribe();
+
+console.log('End script.');
+
+
+
+
+
