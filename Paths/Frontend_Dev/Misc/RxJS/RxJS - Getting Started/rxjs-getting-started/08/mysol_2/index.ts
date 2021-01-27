3016bea483975d5d54df40ac9227ece9a42ff4d7
Jin Wang <specter01wj@gmail.com>
2021-01-27 02:45:13 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/08/mysol_2/index.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/08/mysol_2/index.ts
@@ -2,7 +2,8 @@ import { Observable, of, from, fromEvent, concat, interval, throwError, Subject,
 				 asyncScheduler, asapScheduler, queueScheduler, merge } from 'rxjs';
 import { mergeMap, map, filter, tap, catchError, take, takeUntil,
 				 multicast, refCount, publish, share,
-				 publishLast, publishBehavior, publishReplay } from 'rxjs/operators';
+				 publishLast, publishBehavior, publishReplay,
+				 observeOn } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
@@ -10,19 +11,11 @@ import { allBooks, allReaders } from './data';
 
 console.log('Start script.');
 
-let queue$ = of('QueueScheduler (synchronous)', queueScheduler);
-
-let asap$ = of('AsapScheduler (async micro task)', asapScheduler);
-
-let async$ = of('AsyncScheduler (async task)', asyncScheduler);
-
-
-
-merge(async$, asap$, queue$)
-	.subscribe(
-		value => console.log(value)
-	);
-
+from([1,2,3,4], queueScheduler).pipe(
+  tap(value => console.log(`Value: ${value}`)),
+  tap(value => console.log(`Doubled value: ${value * 2}`))
+)
+.subscribe();
 
 console.log('End script.');
 
