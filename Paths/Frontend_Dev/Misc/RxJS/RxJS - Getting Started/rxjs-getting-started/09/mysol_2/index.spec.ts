b271e514a54aa1f434f255e4e1619d0ed2fc071c
Jin Wang <specter01wj@gmail.com>
2021-01-28 02:16:09 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/09/mysol_2/index.spec.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS - Getting Started/rxjs-getting-started/09/mysol_2/index.spec.ts
@@ -1,4 +1,5 @@
 import { TestScheduler } from 'rxjs/testing';
+import { delay, take } from 'rxjs/operators';
 import { expect } from 'chai';
 
 describe('RxBookTracker Tests', () => {
@@ -17,6 +18,17 @@ describe('RxBookTracker Tests', () => {
 			const expected = 'a|';
 
 			helpers.expectObservable(source$).toBe(expected);
+		});
+	});
+	
+	it('should delay the values produced', () => {
+		scheduler.run(helpers => {
+			const source$ = helpers.cold('-a-b-c-d|');
+			const expected = '------a-b-c-d|';
+
+			helpers.expectObservable(source$.pipe(
+				delay(5)
+			)).toBe(expected);
 		});
 	});
 
