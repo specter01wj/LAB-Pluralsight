0c2f0f28896093daf83fbe253d5935823415944b
Jin Wang <specter01wj@gmail.com>
2021-01-30 00:11:06 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/03/mysol_1/src/app/app.component.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/03/mysol_1/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   pageTitle = 'Acme Product Management';
+
+
+  ngOnInit() {
+  	of(2, 4, 6, 8).subscribe(console.log);
+  }
+
+
 }
