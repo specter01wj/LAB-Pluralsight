fbc9063f79af29961137e11306f5fba20c082046
Jin Wang <specter01wj@gmail.com>
2021-02-09 18:47:51 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/04/mysol_1/my-apm/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/04/mysol_1/my-apm/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ch4TestComponent } from './ch4-test/ch4-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ch4TestComponent
   ],
   imports: [
     BrowserModule,
