0b6782404518ae95a5bcfa5946a8d59875cc815c
Jin Wang <specter01wj@gmail.com>
2021-02-11 23:51:49 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/04/mysol_1/my-apm/src/app/ch4-test/ch4-test.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/04/mysol_1/my-apm/src/app/ch4-test/ch4-test.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-ch4-test',
   templateUrl: './ch4-test.component.html',
   styleUrls: ['./ch4-test.component.less']
+
   
 })
 export class Ch4TestComponent implements OnInit {
