7da831c4cb47a42173fa8688796bd0d32fc2b84b
Jin Wang <specter01wj@gmail.com>
2021-02-15 23:36:32 -0500

    01/20/2021 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/04/mysol_1/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/04/mysol_1/my-apm/src/app/app-routing.module.ts
@@ -5,26 +5,6 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
