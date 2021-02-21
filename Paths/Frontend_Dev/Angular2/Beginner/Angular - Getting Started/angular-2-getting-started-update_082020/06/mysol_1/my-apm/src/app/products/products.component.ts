5a4740ba81643f6e142fb6da8e04f36e52545c1f
Jin Wang <specter01wj@gmail.com>
2021-02-20 19:34:10 -0500

    02/12/2021 v1

---
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/06/mysol_1/my-apm/src/app/products/products.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../interface/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.less']
+})
+export class ProductsComponent implements OnInit {
+	pageTitle: string = 'Product List';
+  products: Product[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2019",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2019",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
