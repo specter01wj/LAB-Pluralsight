a2c23bafdd7b574cff25b50b7489d5bf21d4aefb
Jin Wang <specter01wj@gmail.com>
2021-02-21 20:50:06 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/07/mysol_1/my-apm/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/07/mysol_1/my-apm/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { ConvertToSpacesPipe } from './service/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
