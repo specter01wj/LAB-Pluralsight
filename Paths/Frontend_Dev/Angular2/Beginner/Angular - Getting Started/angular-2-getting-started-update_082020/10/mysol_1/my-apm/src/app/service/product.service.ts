ecaf6f717e450736c5f875309db2807898b93d95
Jin Wang <specter01wj@gmail.com>
2021-02-24 18:36:07 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/10/mysol_1/my-apm/src/app/service/product.service.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/10/mysol_1/my-apm/src/app/service/product.service.ts
@@ -14,7 +14,22 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productUrl);
+    return this.http.get<Product[]>(this.productUrl)
+      .pipe(
+        tap(data => console.log('All: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 
 }
