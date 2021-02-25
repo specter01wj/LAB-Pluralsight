53f541729bf69ce4ca8edf0d4031fc6b1349148f
Jin Wang <specter01wj@gmail.com>
2021-02-25 00:01:08 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/11/mysol_1/my-apm/src/app/products/product-detail/product-detail.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/11/mysol_1/my-apm/src/app/products/product-detail/product-detail.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.less']
 })
 export class ProductDetailComponent implements OnInit {
+	pageTitle: string = 'Product Detail';
+  product: IProduct;
 
   constructor() { }
 
