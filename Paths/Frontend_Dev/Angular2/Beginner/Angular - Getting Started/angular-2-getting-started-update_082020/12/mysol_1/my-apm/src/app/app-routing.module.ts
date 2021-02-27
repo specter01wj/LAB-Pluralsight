39af028c9faf93186bd120bc3b2af24e4887e114
Jin Wang <specter01wj@gmail.com>
2021-02-27 11:30:32 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/12/mysol_1/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/12/mysol_1/my-apm/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
     { path: 'welcome', component: WelcomeComponent },
 
 
+
     
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
