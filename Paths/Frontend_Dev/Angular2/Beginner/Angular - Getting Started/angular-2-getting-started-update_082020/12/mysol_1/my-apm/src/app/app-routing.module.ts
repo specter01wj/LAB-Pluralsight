79b2fb3a104859740920a50daf051ef3ff6332f6
Jin Wang <specter01wj@gmail.com>
2021-02-28 11:31:21 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/12/mysol_1/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_082020/12/mysol_1/my-apm/src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ const routes: Routes = [
 
 
 
+
 		
     { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
     { path: 'welcome', component: WelcomeComponent },
