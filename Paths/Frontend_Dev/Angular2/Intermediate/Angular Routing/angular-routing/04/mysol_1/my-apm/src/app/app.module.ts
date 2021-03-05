df0871683cd3958d7dd08192910b4778cf462f42
Jin Wang <specter01wj@gmail.com>
2021-03-05 02:36:31 -0500

    02/12/2021 v1

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
