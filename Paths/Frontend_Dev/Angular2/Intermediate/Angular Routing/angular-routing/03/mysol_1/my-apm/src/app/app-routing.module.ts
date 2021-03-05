2459b46d23464a3da470d7285ad90c07df06a940
Jin Wang <specter01wj@gmail.com>
2021-03-04 23:47:06 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/03/mysol_1/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/03/mysol_1/my-apm/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { WelcomeComponent } from './home/welcome.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 const routes: Routes = [
