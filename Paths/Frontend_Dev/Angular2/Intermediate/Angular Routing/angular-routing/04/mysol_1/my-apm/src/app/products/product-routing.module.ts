9dd192f44c0d580cc6204b3b019fa76f53700062
Jin Wang <specter01wj@gmail.com>
2021-03-06 00:04:42 -0500

    02/12/2021 v1

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/products/product-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { ProductListComponent } from './product-list/product-list.component';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductListComponent,
+    children: [
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [
+  	ProductListComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ProductRoutingModule { }
