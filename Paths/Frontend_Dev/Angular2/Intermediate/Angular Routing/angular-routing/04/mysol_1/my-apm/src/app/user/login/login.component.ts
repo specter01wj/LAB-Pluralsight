8fcf97eab2ec9af3238d1099a283603db287b6d7
Jin Wang <specter01wj@gmail.com>
2021-03-07 02:24:06 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/user/login/login.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
+	errorMessage: string;
+  pageTitle = 'Log In';
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(loginForm: NgForm) {
+    if (loginForm && loginForm.valid) {
+      const userName = loginForm.form.value.userName;
+      const password = loginForm.form.value.password;
+      this.authService.login(userName, password);
+
+      // Navigate to the Product List page after log in.
+      this.router.navigate(['/products']);
+    } else {
+      this.errorMessage = 'Please enter a user name and password.';
+    }
   }
 
 }
