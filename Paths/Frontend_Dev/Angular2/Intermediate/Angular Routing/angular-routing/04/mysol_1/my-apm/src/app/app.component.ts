dd17993b1b97b7ef5c953492669436afe9b591fc
Jin Wang <specter01wj@gmail.com>
2021-03-07 03:03:50 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { AuthService } from './user/auth.service';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
