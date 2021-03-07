0ce37f9ba6716f52cbde74771c7abd88ace872c1
Jin Wang <specter01wj@gmail.com>
2021-03-07 02:29:04 -0500

    02/12/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/shared/shared.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { StarComponent } from './star.component';
+import { StarComponent } from './star/star.component';
 
 
 @NgModule({
