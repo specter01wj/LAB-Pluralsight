c02027de83ad0b06fb08d73183daeafa24946192
Jin Wang <specter01wj@gmail.com>
2021-04-11 23:43:21 -0400

    02/12/2021 v1

---
+++ Paths/Frontend_Dev/Misc/CSS/Hands on Responsive Design Using a CSS Preprocessor/hands-on-responsive-design-css-preprocessor/4-hands-on-responsive-design-css-preprocessor-m4-exercise-files/mysol_1/Aspen Dental Web Site/webpack.config.js
@@ -0,0 +1,24 @@
+{
+  "name": "my-apm",
+  "version": "0.0.0",
+  "scripts": {
+    "ng": "ng",
+    "start": "ng serve",
+    "build": "ng build",
+    "test": "ng test",
+    "lint": "ng lint",
+    "e2e": "ng e2e",
+    "build": "webpack --config webpack.config.js --mode='production'"
+  },
+  "private": true,
+  "dependencies": {
+    
+  },
+  "devDependencies": {
+    "file-loader": "^5.0.2",
+    "node-sass": "^4.13.1",
+    "sass-loader": "^8.0.2",
+    "webpack": "^4.41.5",
+    "webpack-cli": "^3.3.10"
+  }
+}
