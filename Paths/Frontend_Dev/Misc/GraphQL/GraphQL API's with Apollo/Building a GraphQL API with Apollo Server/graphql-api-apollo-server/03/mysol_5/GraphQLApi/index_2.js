5f4a51ce7623d935559332fd0f6449a3200932c4
Jin Wang <specter01wj@gmail.com>
2021-04-23 02:14:25 -0400

    04/17/2021 v1

---
+++ Paths/Frontend_Dev/Misc/GraphQL/GraphQL API's with Apollo/Building a GraphQL API with Apollo Server/graphql-api-apollo-server/03/mysol_5/GraphQLApi/index_2.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const SessionAPI = require('./datasources/sessions');
+// const sessions = require('./data/sessions.json');
+
+const typeDefs = gql`
+type Query {
+  sessions(
+    id: ID
+    title: String,
+    description:String,
+    startsAt:String,
+    endsAt:String,
+    room:String,
+    day:String,
+    format: String,
+    track:String,
+    level:String
+  ):[Session],
+  sessionById(id:ID): Session
+}
+type Session {
+  id: ID!
+  title: String!,
+  description:String,
+  startsAt:String,
+  endsAt:String,
+  room:String,
+  day:String,
+  format: String,
+  track:String @deprecated(reason: "reason1"),
+  level:String
+}`
+
+const resolvers = {
+  Query: {
+    sessions: (parent, args, {dataSources}, info) => {
+      return dataSources.sessionAPI.getSessions(args);
+    },
+    sessionById: (parent, {id}, {dataSources}, info) => {
+      return dataSources.sessionAPI.getSessionById(id);
+    }
+  }
+};
+
+const dataSources = () => ({
+  sessionAPI: new SessionAPI()
+});
+
+const server = new ApolloServer({ typeDefs, resolvers, dataSources });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log('Now browse to http://localhost:4000' + server.graphqlPath)
+);
