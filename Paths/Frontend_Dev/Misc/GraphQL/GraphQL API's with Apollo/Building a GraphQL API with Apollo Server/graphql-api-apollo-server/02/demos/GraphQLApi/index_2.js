e15d42492bd0627e1a4a0992ff102c12e37d8fdd
Jin Wang <specter01wj@gmail.com>
2021-04-22 23:45:19 -0400

    04/17/2021 v1

---
+++ Paths/Frontend_Dev/Misc/GraphQL/GraphQL API's with Apollo/Building a GraphQL API with Apollo Server/graphql-api-apollo-server/02/demos/GraphQLApi/index_2.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Query {
+  sessions:[Session]
+}
+type Session {
+  id: ID!
+  title: String!,
+  description:String,
+  startsAt:String,
+  endsAt:String,
+  room:String,
+  day:String,
+  format: String,
+  track:String,
+  level:String
+}`
+
+const server = new ApolloServer({ typeDefs });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log('Now browse to http://localhost:4000' + server.graphqlPath)
+);
