dc7345f80764d2e5784117ee3bf3a126e0187789
Jin Wang <specter01wj@gmail.com>
2021-05-15 23:44:27 -0400

    05/07/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/04/mysol_1/APM-ngrx_4/src/app/products/product-list/product-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/04/mysol_1/APM-ngrx_4/src/app/products/product-list/product-list.component.ts
@@ -34,6 +34,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
     this.productService.getProducts().subscribe({
       next: (products: Product[]) => this.products = products,
 
+
       
       error: err => this.errorMessage = err
     });
