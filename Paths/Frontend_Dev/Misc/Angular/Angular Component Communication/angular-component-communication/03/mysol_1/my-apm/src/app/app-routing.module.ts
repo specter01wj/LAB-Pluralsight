4931126d2e0f88e8ed3a0579f2b6a6932382c3d2
Jin Wang <specter01wj@gmail.com>
2021-05-26 23:20:54 -0400

    05/25/2021 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/app-routing.module.ts
@@ -14,27 +14,6 @@ const routes: Routes = [
           },
           { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
   },
   { path: '**', component: PageNotFoundComponent }
 ];
