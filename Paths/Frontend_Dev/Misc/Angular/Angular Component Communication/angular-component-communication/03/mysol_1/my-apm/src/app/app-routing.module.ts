98a8e9b247be9fd2df2637143170bee0f529b29b
Jin Wang <specter01wj@gmail.com>
2021-05-27 22:46:18 -0400

    05/25/2021 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/app-routing.module.ts
@@ -12,27 +12,6 @@ const routes: Routes = [
               // canActivate: [AuthGuard],
               loadChildren: './products/product.module#ProductModule'
           },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          
           { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       ]
   },
