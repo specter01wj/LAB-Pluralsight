99c2a9e5e2be4f3960feed09749bcb9dba68c7db
Jin Wang <specter01wj@gmail.com>
2021-05-31 00:48:41 -0400

    05/25/2021 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/home/shell.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/home/shell.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pm-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.css']
+  styleUrls: ['./shell.component.less']
 })
 export class ShellComponent implements OnInit {
 
