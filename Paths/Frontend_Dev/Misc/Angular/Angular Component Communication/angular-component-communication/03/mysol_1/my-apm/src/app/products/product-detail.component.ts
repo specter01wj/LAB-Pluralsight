4d90b6dbd251e95027444ac51322c25a975c5f1a
Jin Wang <specter01wj@gmail.com>
2021-05-30 23:44:23 -0400

    05/25/2021 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/products/product-detail.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/products/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { IProduct } from './product';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.less']
 })
 export class ProductDetailComponent implements OnInit {
+	pageTitle: string = 'Product Detail';
+  product: IProduct;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private productService: ProductService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  	const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+        const id = +param;
+        this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number) {
+      this.productService.getProduct(id).subscribe(
+        product => this.product = product,
+        error => this.errorMessage = <any>error
+      );
+  }
+
+  onBack(): void {
+      this.router.navigate(['/products']);
   }
 
 }
