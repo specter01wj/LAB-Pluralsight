3309087e228a8cf5555fc28a598f20f895a43539
Jin Wang <specter01wj@gmail.com>
2021-05-30 23:36:24 -0400

    05/25/2021 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/products/product.module.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/products/product.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { ProductRoutingModule } from './product-routing.module';
 
 import { SharedModule } from '../shared/shared.module';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductListComponent, ProductDetailComponent],
   imports: [
     CommonModule,
     FormsModule,
