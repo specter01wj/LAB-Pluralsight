f5d1ccc47e11c18544e96b402213f677518c4de5
Jin Wang <specter01wj@gmail.com>
2021-05-31 00:40:57 -0400

    05/25/2021 v1

---
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_1/my-apm/src/app/products/product-shell/product-shell-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-shell-detail',
+  templateUrl: './product-shell-detail.component.html',
+  styleUrls: ['./product-shell-detail.component.less']
+})
+export class ProductShellDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
