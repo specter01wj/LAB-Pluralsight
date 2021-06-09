947e03d4ecbf93e3dbad064b337e5cd1dda5ff86
Jin Wang <specter01wj@gmail.com>
2021-06-08 22:34:40 -0600

    06/04/2021 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/04/mysol_1/my-apm/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/04/mysol_1/my-apm/src/app/products/product-list.component.ts
@@ -60,27 +60,6 @@ export class ProductListComponent implements OnInit, AfterViewInit {
 
   onFilterChange(filter: string): void {
     this.listFilter = filter;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
     this.performFilter(this.listFilter);
   }
 
