b9787a41f4f6beba82408f45ced4c6d735d9243a
Jin Wang <specter01wj@gmail.com>
2021-06-25 19:08:01 -0400

    06/04/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/04/mysol_1/APM-ngrx_4/src/app/products/product-list/product-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/04/mysol_1/APM-ngrx_4/src/app/products/product-list/product-list.component.ts
@@ -40,6 +40,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
       products => {
         if(products) {
           this.displayCode = products.showProductCode;
+          
         }
       }
     );
