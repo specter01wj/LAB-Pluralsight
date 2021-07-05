4a67d08d0a6f31f5e687731ec9e44db6a806f604
Jin Wang <specter01wj@gmail.com>
2021-07-05 19:21:49 -0400

    06/04/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/03/mysol_1/apm-form/src/app/customers/customers.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/03/mysol_1/apm-form/src/app/customers/customers.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CustomersRoutingModule } from './customers-routing.module';
+
+import { CustomersComponent } from './customers.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CustomersComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    CustomersRoutingModule,
   ]
 })
 export class CustomersModule { }
