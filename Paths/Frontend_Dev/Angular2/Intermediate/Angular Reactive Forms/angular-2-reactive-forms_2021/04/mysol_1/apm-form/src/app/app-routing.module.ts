8f026ae1f5a90b4b92de0ad552845b11adec194f
Jin Wang <specter01wj@gmail.com>
2021-07-08 23:52:08 -0400

    06/04/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/04/mysol_1/apm-form/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/04/mysol_1/apm-form/src/app/app-routing.module.ts
@@ -8,26 +8,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
