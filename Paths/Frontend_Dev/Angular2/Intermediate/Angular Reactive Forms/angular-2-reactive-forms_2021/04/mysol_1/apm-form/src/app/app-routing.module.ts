9866557b39d05eee179365a07f7cfaa370dac111
Jin Wang <specter01wj@gmail.com>
2021-07-14 23:20:11 -0400

    06/04/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/04/mysol_1/apm-form/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/04/mysol_1/apm-form/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'customers', pathMatch: 'full' },
   { path: '**', redirectTo: 'customers', pathMatch: 'full' },
+  
 ];
 
 @NgModule({
