3d9986ab82635b96939727ef5bf8ace29258dab9
Jin Wang <specter01wj@gmail.com>
2021-07-22 23:56:18 -0400

    06/04/2021 v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/04/mysol_1/apm-form/src/app/customers/customers.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/04/mysol_1/apm-form/src/app/customers/customers.component.ts
@@ -6,6 +6,7 @@ import { Customer } from '../models/customer';
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
+  
   styleUrls: ['./customers.component.scss']
 })
 export class CustomersComponent implements OnInit {
