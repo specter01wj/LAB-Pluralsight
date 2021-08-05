6fc8d0545ea3c84a55761541d17b05d146a3e391
Jin Wang <specter01wj@gmail.com>
2021-08-05 01:07:54 -0400

    06/04/2021 v1

---
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/05/mysol_1/my-apm/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Ch4TestComponent } from './ch4-test/ch4-test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Ch4TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/05/mysol_1/my-apm/src/app/ch4-test/ch4-test.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ch4-test',
+  templateUrl: './ch4-test.component.html',
+  styleUrls: ['./ch4-test.component.scss']
+})
+export class Ch4TestComponent implements OnInit {
+	pageTitle: string = "Acme Product Management";
+	
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
