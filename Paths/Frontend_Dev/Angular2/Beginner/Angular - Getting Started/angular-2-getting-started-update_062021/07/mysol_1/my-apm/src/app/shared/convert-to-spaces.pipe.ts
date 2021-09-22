3ea0473c4bcca0719594e0486e494d0724290b5e
Jin Wang <specter01wj@gmail.com>
2021-09-22 01:14:03 -0400

    v12

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/07/mysol_1/my-apm/src/app/shared/convert-to-spaces.pipe.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/07/mysol_1/my-apm/src/app/shared/convert-to-spaces.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertToSpacesPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, character: string): string {
+    return value.replace(character, ' ');
   }
 
 }
