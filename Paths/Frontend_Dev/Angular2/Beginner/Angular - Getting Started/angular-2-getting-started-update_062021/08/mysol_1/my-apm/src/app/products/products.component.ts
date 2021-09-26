f85c29ea3b02c476630664c516bc8f0c13738a87
Jin Wang <specter01wj@gmail.com>
2021-09-25 23:00:23 -0400

    v8

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/08/mysol_1/my-apm/src/app/products/products.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/08/mysol_1/my-apm/src/app/products/products.component.ts
@@ -60,6 +60,7 @@ export class ProductsComponent implements OnInit {
 
 
 
+
     
     return this.products.filter((product: Product) => 
       product.productName.toLocaleLowerCase().includes(filterBy));
