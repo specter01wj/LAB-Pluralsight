a466c3b8f4592ea31c0ef460f9083694a498f4cc
Jin Wang <specter01wj@gmail.com>
2021-09-26 13:35:00 -0400

    v5

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/08/mysol_1/my-apm/src/app/shared/star/star.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/08/mysol_1/my-apm/src/app/shared/star/star.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.scss']
 })
-export class StarComponent implements OnInit {
+export class StarComponent implements OnInit, OnChanges {
+	@Input() rating: number = 0;
+	cropWidth: number = 75;
+	@Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+  	this.cropWidth = this.rating * 75 / 5;
+  }
+
+  onClick(): void {
+  	this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
   }
 
 }
