1a8fd2958b658308b657e5282fb8157cd1bb0eb0
Jin Wang <specter01wj@gmail.com>
2021-09-30 16:36:55 -0700

    v2

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/10/mysol_1/my-apm/src/app/products/products.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/10/mysol_1/my-apm/src/app/products/products.component.ts
@@ -13,6 +13,8 @@ export class ProductsComponent implements OnInit {
 	pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
+
+
   
   showImage: boolean = false;
 
