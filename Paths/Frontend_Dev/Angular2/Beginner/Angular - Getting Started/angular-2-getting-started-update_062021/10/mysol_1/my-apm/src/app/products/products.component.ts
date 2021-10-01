b4effcc9c9e0d9780e6a9b36b3b39e19df87cedd
Jin Wang <specter01wj@gmail.com>
2021-10-01 11:43:25 -0700

    v11

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/10/mysol_1/my-apm/src/app/products/products.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/10/mysol_1/my-apm/src/app/products/products.component.ts
@@ -13,36 +13,6 @@ export class ProductsComponent implements OnInit {
 	pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-  
   showImage: boolean = false;
 
   // listFilter: string = 'cart';
