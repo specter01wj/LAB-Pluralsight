b7348c4785e34f6f9b8221a4f360970e62cfdf08
Jin Wang <specter01wj@gmail.com>
2021-10-02 12:23:06 -0700

    v7

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/10/mysol_1/my-apm/src/app/products/products.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/10/mysol_1/my-apm/src/app/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Product } from '../model/product';
 
