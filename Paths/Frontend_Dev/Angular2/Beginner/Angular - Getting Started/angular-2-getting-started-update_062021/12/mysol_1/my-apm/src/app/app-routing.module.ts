9b6d1b42d82cb40e794bdcdef13d937e1f1e3f10
Jin Wang <specter01wj@gmail.com>
2021-10-06 02:05:17 -0500

    v16

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/12/mysol_1/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/12/mysol_1/my-apm/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProductDetailGuard } from './guards/product-detail.guard';
+
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 		{ path: 'products', component: ProductsComponent },
-    { path: 'products/:id', component: ProductDetailComponent },
+    { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
     { path: 'home', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
