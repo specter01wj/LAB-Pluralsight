73c1c7aec2c09e99b69991a6ac57383e44652977
Jin Wang <specter01wj@gmail.com>
2021-10-09 00:43:26 -0400

    v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/13/mysol_1/my-apm/src/app/products/products-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/13/mysol_1/my-apm/src/app/products/products-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { ProductDetailGuard } from '../guards/product-detail.guard';
+
+import { ProductsComponent } from './products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  	ProductsComponent,
+    ProductDetailComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class ProductsRoutingModule { }
--- Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/13/mysol_1/my-apm/src/app/products/products.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular - Getting Started/angular-2-getting-started-update_062021/13/mysol_1/my-apm/src/app/products/products.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { ProductsRoutingModule } from './products-routing.module';
 
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { ProductsComponent } from './products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  	ConvertToSpacesPipe,
+  	ProductsComponent,
+  	ProductDetailComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ProductsRoutingModule
   ]
 })
 export class ProductsModule { }
