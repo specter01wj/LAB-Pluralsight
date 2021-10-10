32c3aaf73bc9e923b79c3c4c04e171097861e941
Jin Wang <specter01wj@gmail.com>
2021-10-10 00:33:51 -0400

    v11

--- Paths/Frontend_Dev/Angular2/Beginner/Styling Angular Applications/styling-angular-applications/03/mysol_1/my-style/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Styling Angular Applications/styling-angular-applications/03/mysol_1/my-style/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavComponent
   ],
   imports: [
     BrowserModule,
