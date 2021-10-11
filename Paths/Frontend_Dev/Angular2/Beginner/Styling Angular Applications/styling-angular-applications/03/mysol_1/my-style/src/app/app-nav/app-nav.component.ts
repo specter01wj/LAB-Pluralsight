87b39b29bba52325920a9d2b1644a9e742b1525b
Jin Wang <specter01wj@gmail.com>
2021-10-11 00:13:11 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Beginner/Styling Angular Applications/styling-angular-applications/03/mysol_1/my-style/src/app/app-nav/app-nav.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Styling Angular Applications/styling-angular-applications/03/mysol_1/my-style/src/app/app-nav/app-nav.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-app-nav',
-  encapsulation: ViewEncapsulation.None,
+  // encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.ShadowDom,
   templateUrl: './app-nav.component.html',
   styleUrls: ['./app-nav.component.scss']
 })
