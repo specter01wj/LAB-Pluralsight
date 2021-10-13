991970b3e94ee32eea69aeea5950fca255479b86
Jin Wang <specter01wj@gmail.com>
2021-10-12 23:52:29 -0400

    v16

--- Paths/Frontend_Dev/Angular2/Beginner/Styling Angular Applications/styling-angular-applications/04/mysol_3/my-style3/src/app/shared/components/content/accordion/accordion-pane/accordion-pane.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Styling Angular Applications/styling-angular-applications/04/mysol_3/my-style3/src/app/shared/components/content/accordion/accordion-pane/accordion-pane.component.ts
@@ -8,15 +8,5 @@ import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
 
 export class AccordionPaneComponent {
     @ViewChild(TemplateRef, { static: true }) template: TemplateRef<unknown>;
-
-
-
-
-
-
-
-
-
-    
     @Input() title: string;
 }
