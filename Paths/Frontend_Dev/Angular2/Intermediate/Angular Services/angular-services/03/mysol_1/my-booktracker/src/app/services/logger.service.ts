e1da81615c8f077811b25546683275d002b272f7
Jin Wang <specter01wj@gmail.com>
2021-10-15 17:56:13 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/mysol_1/my-booktracker/src/app/services/logger.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/mysol_1/my-booktracker/src/app/services/logger.service.ts
@@ -9,11 +9,12 @@ export class LoggerService {
 
   log(message: string): void {
     const timeString: String = new Date().toLocaleTimeString();
+    
     console.log(`${message} (${timeString})`);
   }
 
   error(message: string): void {
     console.error(`ERROR: ${message}`);
   }
-  
+
 }
