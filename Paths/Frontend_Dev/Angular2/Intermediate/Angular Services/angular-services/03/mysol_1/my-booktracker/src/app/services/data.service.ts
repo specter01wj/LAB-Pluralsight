ede02f9ab747b5b46e4d9a16db2ed618ea447cf4
Jin Wang <specter01wj@gmail.com>
2021-10-16 02:03:12 -0400

    v11

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/mysol_1/my-booktracker/src/app/services/data.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/mysol_1/my-booktracker/src/app/services/data.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { allBooks, allReaders } from 'app/api/data';
+import { Book } from 'app/models/book';
+import { Reader } from 'app/models/reader';
+import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root'
