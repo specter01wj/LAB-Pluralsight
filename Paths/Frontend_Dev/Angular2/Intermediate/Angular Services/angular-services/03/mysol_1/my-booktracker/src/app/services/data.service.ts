0863f486ca184bf04624903b5da8dc7fa5282a0d
Jin Wang <specter01wj@gmail.com>
2021-10-16 02:03:53 -0400

    v12

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/mysol_1/my-booktracker/src/app/services/data.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/angular-services/03/mysol_1/my-booktracker/src/app/services/data.service.ts
@@ -8,6 +8,28 @@ import { LoggerService } from './logger.service';
   providedIn: 'root'
 })
 export class DataService {
+  mostPopularBook: Book = allBooks[0];
 
-  constructor() { }
+  constructor(private loggerService: LoggerService) { }
+
+  setMostPopularBook(popularBook: Book): void {
+    this.mostPopularBook = popularBook;
+  }
+
+  getAllReaders(): Reader[] {
+    return allReaders;    
+  }
+
+  getReaderById(id: number): Reader {
+    return allReaders.find(reader => reader.readerID === id);
+  }
+
+  getAllBooks(): Book[] {
+    return allBooks;
+  }
+
+  getBookById(id: number): Book {
+    return allBooks.find(book => book.bookID === id);
+  }
+
 }
