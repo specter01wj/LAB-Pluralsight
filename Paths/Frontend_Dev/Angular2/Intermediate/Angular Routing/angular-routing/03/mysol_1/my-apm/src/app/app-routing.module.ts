e3017864a8e3536b11cf9fd8ee1f5f423159d1f5
Jin Wang <specter01wj@gmail.com>
2021-10-17 13:43:12 -0400

    v12

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/03/mysol_1/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/03/mysol_1/my-apm/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/03/mysol_1/my-apm/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/03/mysol_1/my-apm/src/app/app.module.ts
@@ -4,12 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './not-found/page-not-found.component';
+
+import { ProductModule } from './products/product.module';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './messages/message.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    ProductModule,
+    UserModule,
+    MessageModule,
     AppRoutingModule
   ],
   providers: [],
