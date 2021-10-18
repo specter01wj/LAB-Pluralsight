42bce5f2adbd296b922a597545ee7e9d4314af7a
Jin Wang <specter01wj@gmail.com>
2021-10-17 23:59:05 -0400

    v12

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/service/product.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/04/mysol_1/my-apm/src/app/service/product.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+import { Product } from '../interface/product';
 
 @Injectable({
   providedIn: 'root'
