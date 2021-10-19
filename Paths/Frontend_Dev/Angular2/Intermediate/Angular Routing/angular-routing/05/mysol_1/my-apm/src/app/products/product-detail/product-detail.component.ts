d3c781bb83b860d06d5876518007eaac8142bdc8
Jin Wang <specter01wj@gmail.com>
2021-10-19 01:24:59 -0400

    v12

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/05/mysol_1/my-apm/src/app/products/product-detail/product-detail.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/05/mysol_1/my-apm/src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product } from '../../interface/product';
+import { ProductService } from '../../service/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle = 'Product Detail';
+  product: Product | undefined;
+  errorMessage: string | undefined;
 
-  constructor() { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = +(this.route.snapshot.paramMap.get('id') || NaN);
+    this.getProduct(id);
+  }
+
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.onProductRetrieved(product),
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onProductRetrieved(product: Product): void {
+    this.product = product;
+
+    if (this.product) {
+      this.pageTitle = `Product Detail: ${this.product.productName}`;
+    } else {
+      this.pageTitle = 'No product found';
+    }
   }
 
 }
