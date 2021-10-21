faad97dadda92c8e61bf43364ab84134e736832a
Jin Wang <specter01wj@gmail.com>
2021-10-20 23:51:02 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/06/mysol_1/my-apm/src/app/service/product-resolver.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/06/mysol_1/my-apm/src/app/service/product-resolver.service.ts
@@ -12,5 +12,26 @@ import { ProductService } from './product.service';
 })
 export class ProductResolver implements Resolve<ProductResolved> {
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
+
+  resolve(route: ActivatedRouteSnapshot,
+          state: RouterStateSnapshot): Observable<ProductResolved> {
+    const id = route.paramMap.get('id');
+    if (isNaN(+id)) {
+      const message = `Product id was not a number: ${id}`;
+      console.error(message);
+      return of({ product: null, error: message });
+    }
+
+    return this.productService.getProduct(+id)
+      .pipe(
+        map(product => ({ product: product })),
+        catchError(error => {
+          const message = `Retrieval error: ${error}`;
+          console.error(message);
+          return of({ product: null, error: message });
+        })
+      );
+  }
+
 }
