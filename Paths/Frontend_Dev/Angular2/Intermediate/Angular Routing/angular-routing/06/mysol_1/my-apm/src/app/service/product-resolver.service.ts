9188712a113ce244920e7cf46ff489a4bf799537
Jin Wang <specter01wj@gmail.com>
2021-10-20 23:46:44 -0400

    v12

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/06/mysol_1/my-apm/src/app/service/product-resolver.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/06/mysol_1/my-apm/src/app/service/product-resolver.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { ProductResolved } from './product';
+import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductResolverService {
+export class ProductResolver implements Resolve<ProductResolved> {
 
   constructor() { }
 }
