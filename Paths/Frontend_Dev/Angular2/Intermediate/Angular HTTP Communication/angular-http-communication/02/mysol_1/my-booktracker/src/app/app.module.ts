8908ae3c3cb48975790439c1a7df183adbe975dd
Jin Wang <specter01wj@gmail.com>
2021-10-25 13:36:16 -0400

    v7

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Communication/angular-http-communication/02/mysol_1/my-booktracker/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Communication/angular-http-communication/02/mysol_1/my-booktracker/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AddBookComponent } from './add-book/add-book.component';
+import { AddReaderComponent } from './add-reader/add-reader.component';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { EditReaderComponent } from './edit-reader/edit-reader.component';
+import { BookTrackerErrorHandlerService } from './core/book-tracker-error-handler.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddBookComponent,
+    EditReaderComponent,
+    EditBookComponent,
+    AddReaderComponent
+  ],
+  providers: [
+    { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService }
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
