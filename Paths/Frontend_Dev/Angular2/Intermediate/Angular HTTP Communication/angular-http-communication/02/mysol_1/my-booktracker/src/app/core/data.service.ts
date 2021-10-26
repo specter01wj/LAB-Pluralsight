347ae190b96b37fcedb885563e6ae950b7885ef2
Jin Wang <specter01wj@gmail.com>
2021-10-25 23:53:03 -0400

    v12

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Communication/angular-http-communication/02/mysol_1/my-booktracker/src/app/core/data.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Communication/angular-http-communication/02/mysol_1/my-booktracker/src/app/core/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from "app/models/reader";
