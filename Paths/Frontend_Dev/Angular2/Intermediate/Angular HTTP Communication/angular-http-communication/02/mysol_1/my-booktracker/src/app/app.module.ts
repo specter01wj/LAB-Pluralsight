acaedb415395dcdade49f1f670c71be5696cd884
Jin Wang <specter01wj@gmail.com>
2021-10-25 23:53:49 -0400

    v6

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Communication/angular-http-communication/02/mysol_1/my-booktracker/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Communication/angular-http-communication/02/mysol_1/my-booktracker/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AddBookComponent } from './add-book/add-book.component';
 import { AddReaderComponent } from './add-reader/add-reader.component';
