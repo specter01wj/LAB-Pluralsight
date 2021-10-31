ad503fa670461b59435e7f3836a8db25cf3f076f
Jin Wang <specter01wj@gmail.com>
2021-10-31 02:45:33 -0400

    v16

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Communication/angular-http-communication/03/mysol_1/my-booktracker/src/app/core/data.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Communication/angular-http-communication/03/mysol_1/my-booktracker/src/app/core/data.service.ts
@@ -18,29 +18,6 @@ export class DataService {
 
   setMostPopularBook(popularBook: Book): void {
     this.mostPopularBook = popularBook;
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
   }
 
   getAllReaders(): Reader[] {
