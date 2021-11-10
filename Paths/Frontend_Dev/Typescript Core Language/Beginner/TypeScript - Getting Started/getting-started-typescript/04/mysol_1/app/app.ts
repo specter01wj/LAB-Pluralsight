97e42759b64feca124214bb0b8c50e2923f91790
Jin Wang <specter01wj@gmail.com>
2021-11-09 23:55:33 -0500

    v11

--- Paths/Frontend_Dev/Typescript Core Language/Beginner/TypeScript - Getting Started/getting-started-typescript/04/mysol_1/app/app.ts
+++ Paths/Frontend_Dev/Typescript Core Language/Beginner/TypeScript - Getting Started/getting-started-typescript/04/mysol_1/app/app.ts
@@ -11,6 +11,7 @@ function startGame() {
 
 function logPlayer(name: string): void {
   console.log(`New game starting for player: ${name}`);
+
   
 }
 document.getElementById('startGame')!.addEventListener('click', startGame);
