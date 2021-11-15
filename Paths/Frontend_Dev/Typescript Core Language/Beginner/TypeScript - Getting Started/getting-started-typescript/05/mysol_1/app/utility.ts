f7e7914591ec646a883e9bceb8f82905df9fc770
Jin Wang <specter01wj@gmail.com>
2021-11-14 23:49:19 -0500

    v2

--- Paths/Frontend_Dev/Typescript Core Language/Beginner/TypeScript - Getting Started/getting-started-typescript/05/mysol_1/app/utility.ts
+++ Paths/Frontend_Dev/Typescript Core Language/Beginner/TypeScript - Getting Started/getting-started-typescript/05/mysol_1/app/utility.ts
@@ -1,6 +1,14 @@
 
-interface Person {
-  name: string;
-  age?: number;
-  formatName: () => string;  
+class Utility {
+  static getInputValue(elementID: string): string | undefined {
+
+	  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+	  if (inputElement.value === '') {
+	    return undefined;
+	  }
+	  else {
+	    return inputElement.value;
+	  }  
+	}
 }
