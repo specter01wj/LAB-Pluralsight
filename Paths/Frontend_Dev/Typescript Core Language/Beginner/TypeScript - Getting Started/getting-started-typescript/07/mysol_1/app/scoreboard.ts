bdce6452a493309a079505e12f99d9d16cf51dad
Jin Wang <specter01wj@gmail.com>
2021-11-21 01:09:42 -0500

    v6

--- Paths/Frontend_Dev/Typescript Core Language/Beginner/TypeScript - Getting Started/getting-started-typescript/07/mysol_1/app/scoreboard.ts
+++ Paths/Frontend_Dev/Typescript Core Language/Beginner/TypeScript - Getting Started/getting-started-typescript/07/mysol_1/app/scoreboard.ts
@@ -21,6 +21,7 @@ export class Scoreboard {
 
 
 
+
       
       output += result.playerName + ': ' + result.score + '/' + result.problemCount + ' for factor ' + result.factor;
       output += '</h4>';
