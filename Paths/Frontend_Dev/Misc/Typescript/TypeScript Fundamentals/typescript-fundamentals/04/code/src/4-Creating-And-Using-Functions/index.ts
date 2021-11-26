fe3dc8c522fbb9c834821ef5e38db941c4bbb448
Jin Wang <specter01wj@gmail.com>
2021-11-26 18:52:26 -0500

    v11

--- Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/04/code/src/4-Creating-And-Using-Functions/index.ts
+++ Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/04/code/src/4-Creating-And-Using-Functions/index.ts
@@ -1,4 +1,5 @@
 // import updateOutput from './output_typedPara';
-import updateOutput from './output_defineFun';
+// import updateOutput from './output_defineFun';
+import updateOutput from './output_returnVal';
 
 updateOutput('output');
