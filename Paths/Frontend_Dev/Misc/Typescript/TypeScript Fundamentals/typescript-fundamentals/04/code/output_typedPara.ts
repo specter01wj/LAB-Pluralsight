3efcfd8921d4194eba25b39d67c5cf6045d4ed9e
Jin Wang <specter01wj@gmail.com>
2021-11-26 18:19:45 -0500

    v3

--- Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/04/code/output_typedPara.ts
+++ Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/04/code/output_typedPara.ts
@@ -6,12 +6,16 @@ export default async function updateOutput(id: string) {
 
 }
 
+runTheLearningSamples();
+
 function runTheLearningSamples() {
 
   function displayProductInfo(id: number, name: string) {
     console.log(`${prefix} typed parameters`);
     console.log(`Product id=${id.toString()} and name=${name}`);
   }
+
+  displayProductInfo(10, 'Pizza');
 
 }
 
