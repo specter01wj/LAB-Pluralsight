ff4d97d135a095f55d1df8b55cc808ee4855707c
Jin Wang <specter01wj@gmail.com>
2021-11-28 00:50:19 -0500

    v12

--- Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/06/code/src/lib/product_create.ts
+++ Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/06/code/src/lib/product_create.ts
@@ -3,18 +3,21 @@ class FoodProduct {
 	id = 0;
 	name = '';
 	icon = '';
+
+	constructor(id: number, name: string, icon: string) {}
 	
 }
 
 
-let fp = new FoodProduct();
+// properties
+/*let fp = new FoodProduct();
 fp.id = 1;
 fp.name = 'Pizza';
-fp.icon = 'icon.jpg';
+fp.icon = 'icon.jpg';*/
 
 
-
-
+// constructor
+let fp = new FoodProduct(1, 'Pizza', 'icon.jpg');
 
 
 
