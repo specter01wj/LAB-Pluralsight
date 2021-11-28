137605a53ed722c225409b69ab7f06dbb57f744d
Jin Wang <specter01wj@gmail.com>
2021-11-27 22:10:22 -0500

    v12

--- Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/05/code/src/lib/index.ts
+++ Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/05/code/src/lib/index.ts
@@ -1,4 +1,5 @@
 export * from './config';
-export * from './interfaces';
+// export * from './interfaces';
+export * from './define_interfaces';
 export * from './products';
 export * from './product';
