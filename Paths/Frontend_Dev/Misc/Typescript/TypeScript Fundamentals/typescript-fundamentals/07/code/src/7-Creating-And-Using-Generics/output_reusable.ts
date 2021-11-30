4bc2c1fd5e596d56b44d100a7f9370b5ff661c22
Jin Wang <specter01wj@gmail.com>
2021-11-30 00:02:39 -0500

    v3

--- Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/07/code/src/7-Creating-And-Using-Generics/output_reusable.ts
+++ Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/07/code/src/7-Creating-And-Using-Generics/output_reusable.ts
@@ -150,18 +150,6 @@ async function runTheLearningSamples() {
 
   const foodModel: FoodModel = new FoodModel();
   await foodModel.getItems();
-
-
-
-
-
-
-
-
-
-
-
-  
   console.log(`${prefix} Generic Interface`);
   console.table(foodModel.items);
 
