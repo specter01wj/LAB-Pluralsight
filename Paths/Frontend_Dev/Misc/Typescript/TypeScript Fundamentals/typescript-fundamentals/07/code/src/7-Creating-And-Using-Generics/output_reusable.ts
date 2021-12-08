e90d5008d0214cce510fe80c0e466ae17d71e920
Jin Wang <specter01wj@gmail.com>
2021-12-07 23:30:50 -0500

    v12

--- Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/07/code/src/7-Creating-And-Using-Generics/output_reusable.ts
+++ Paths/Frontend_Dev/Misc/Typescript/TypeScript Fundamentals/typescript-fundamentals/07/code/src/7-Creating-And-Using-Generics/output_reusable.ts
@@ -89,16 +89,6 @@ async function runTheLearningSamples() {
 
   function whatIsIt_string(arg: string): string {
     return arg;
-
-
-
-
-
-
-
-
-
-    
   }
   console.log(whatIsIt_string('john'));
 
