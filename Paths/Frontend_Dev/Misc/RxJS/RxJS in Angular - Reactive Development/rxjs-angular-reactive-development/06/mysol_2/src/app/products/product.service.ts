a6bb67c71a8066d222f3be7ed373d82ddf58ed78
Jin Wang <specter01wj@gmail.com>
2021-12-24 15:51:43 -0500

    v11

--- Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/06/mysol_2/src/app/products/product.service.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/06/mysol_2/src/app/products/product.service.ts
@@ -19,7 +19,12 @@ export class ProductService {
   products$ = this.http.get<Product[]>(this.productsUrl)
                 .pipe(
                   // map(products => products),
-                  map(products => products.map(product => product.price * 1.5)),
+                  // map(products => products.map(product => product.price * 1.5)),
+                  map(products => products.map(product => ({
+                    ...product,
+                    price: product.price * 12.5,
+                    searchKey: [product.productName]
+                  }) as Product)),
                   tap(data => console.log('Products: ', JSON.stringify(data))),
                   catchError(this.handleError)
                 );
