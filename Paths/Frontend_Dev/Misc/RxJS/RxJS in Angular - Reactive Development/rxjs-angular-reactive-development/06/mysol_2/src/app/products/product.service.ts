c1d6c36563dbd8c1414c540d116e90c9540d8dc3
Jin Wang <specter01wj@gmail.com>
2021-12-24 22:10:51 -0500

    v15

--- Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/06/mysol_2/src/app/products/product.service.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/06/mysol_2/src/app/products/product.service.ts
@@ -33,6 +33,7 @@ export class ProductService {
 
 
 
+
                     searchKey: [product.productName]
                   }) as Product)),
                   tap(data => console.log('Products: ', JSON.stringify(data))),
