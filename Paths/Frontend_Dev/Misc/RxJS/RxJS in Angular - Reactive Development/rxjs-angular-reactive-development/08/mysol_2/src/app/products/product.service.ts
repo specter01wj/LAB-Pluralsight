1500cd61a331eb4cbf3fb2a20b50ac6046fb8301
Jin Wang <specter01wj@gmail.com>
2021-12-28 23:48:39 -0500

    v16

--- Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/08/mysol_2/src/app/products/product.service.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/08/mysol_2/src/app/products/product.service.ts
@@ -38,20 +38,6 @@ export class ProductService {
       products.map(product => ({
         ...product,
         price: product.price * 1.5,
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         category: categories.find(c => product.categoryId === c.id).name,
         searchKey: [product.productName]
       }) as Product)
