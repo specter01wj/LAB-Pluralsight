61f932d2fe593c503fd9246a1b3834b963703f46
Jin Wang <specter01wj@gmail.com>
2021-12-31 15:46:36 -0500

    v1

--- Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/11/mysol_2_demo1/src/app/suppliers/supplier.service.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/11/mysol_2_demo1/src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Supplier } from './supplier';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,18 @@ import { throwError, Observable } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  suppliersWithMap$ = of(1, 5, 8)
+    .pipe(
+      map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`)
+      )
+    );
+
+  constructor(private http: HttpClient) {
+    this.suppliersWithMap$
+      .subscribe(o => o.subscribe(
+        item => console.log('map result', item)
+      ));
+  }
 
   private handleError(err: any): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
