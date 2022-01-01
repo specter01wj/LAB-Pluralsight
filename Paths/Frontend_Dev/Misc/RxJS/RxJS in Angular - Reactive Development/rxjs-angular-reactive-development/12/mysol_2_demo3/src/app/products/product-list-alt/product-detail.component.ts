d3d96af22cde49fa758af2e07b77245e0e9e90a8
Jin Wang <specter01wj@gmail.com>
2021-12-31 23:57:57 -0500

    v15

--- Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/12/mysol_2_demo3/src/app/products/product-list-alt/product-detail.component.ts
+++ Paths/Frontend_Dev/Misc/RxJS/RxJS in Angular - Reactive Development/rxjs-angular-reactive-development/12/mysol_2_demo3/src/app/products/product-list-alt/product-detail.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { EMPTY, Subject } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { ProductService } from '../product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'pm-product-detail',
@@ -10,7 +11,7 @@ import { ProductService } from '../product.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
+  // pageTitle = 'Product Detail';
   private errorMessageSubject = new Subject<string>();
   errorMessage$ = this.errorMessageSubject.asObservable();
 
@@ -20,6 +21,12 @@ export class ProductDetailComponent {
         this.errorMessageSubject.next(err);
         return EMPTY;
       })
+    );
+
+  pageTitle$ = this.product$
+    .pipe(
+      map((p: Product) =>
+        p ? `Product Detail for: ${p.productName}` : null)
     );
 
   productSuppliers$ = this.productService.selectedProductSuppliers$
