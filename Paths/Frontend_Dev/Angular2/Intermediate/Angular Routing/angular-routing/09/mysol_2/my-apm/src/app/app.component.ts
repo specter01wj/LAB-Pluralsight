4f9a772fef037baa580e0f262b76d535b2976ef9
Jin Wang <specter01wj@gmail.com>
2022-02-03 03:24:47 -0500

    v12

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/09/mysol_2/my-apm/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/09/mysol_2/my-apm/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 import { slideInAnimation } from './api/app.animation';
 
 import { AuthService } from './service/auth.service';
@@ -12,10 +12,14 @@ import { AuthService } from './service/auth.service';
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
-
+  loading = true;
 
   constructor(private authService: AuthService,
-              private router: Router) { }
+              private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
 
 
   get isLoggedIn(): boolean {
@@ -27,6 +31,18 @@ export class AppComponent {
       return this.authService.currentUser.userName;
     }
     return '';
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
   }
 
   logOut(): void {
