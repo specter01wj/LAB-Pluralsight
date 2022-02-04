061e850357fa0cf510ffbb7656b785e47dde574c
Jin Wang <specter01wj@gmail.com>
2022-02-04 02:53:32 -0500

    v2

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/10/mysol_2/my-apm/src/app/messages/message.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/10/mysol_2/my-apm/src/app/messages/message.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MessageComponent } from './message/message.component';
+import { FormsModule } from '@angular/forms';
 
-import { SharedModule } from '../shared/shared.module';
+import { MessageRoutingModule } from './message-routing.module';
 
 @NgModule({
-  declarations: [MessageComponent],
+  declarations: [],
   imports: [
     CommonModule,
-    SharedModule
+    FormsModule,
+    MessageRoutingModule
   ]
 })
 export class MessageModule { }
