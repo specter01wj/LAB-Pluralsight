476f4e678180fb4949cbbf831fa3e0923af50c5e
Jin Wang <specter01wj@gmail.com>
2022-02-09 23:50:41 -0500

    v25

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_2/my-apm/src/app/service/product.service.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_2/my-apm/src/app/service/product.service.ts
@@ -72,29 +72,6 @@ export class ProductService {
 
   private createProduct(product: IProduct, headers: HttpHeaders): Observable<IProduct> {
       product.id = null;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       return this.http.post<IProduct>(this.productsUrl, product,  { headers: headers} )
                       .pipe(
                           tap(data => console.log('createProduct: ' + JSON.stringify(data))),
