f814c4e38554300159f0e0741c1b8b5d7716a8b2
Jin Wang <specter01wj@gmail.com>
2022-02-09 23:48:09 -0500

    v9

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_2/my-apm/src/app/service/product.service.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/03/mysol_2/my-apm/src/app/service/product.service.ts
@@ -80,6 +80,7 @@ export class ProductService {
 
 
 
+
       return this.http.post<IProduct>(this.productsUrl, product,  { headers: headers} )
                       .pipe(
                           tap(data => console.log('createProduct: ' + JSON.stringify(data))),
