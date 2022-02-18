fff9ebe1017e21fa1cf33e3abe34fee100da387b
Jin Wang <specter01wj@gmail.com>
2022-02-17 20:53:57 -0500

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/products/product-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/products/product-routing.module.ts
@@ -12,47 +12,40 @@ import { ProductResolver } from '../service/product-resolver.service';
 import { ProductEditInfoComponent } from './product-edit/product-edit-info/product-edit-info.component';
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags/product-edit-tags.component';
 
-import { AuthGuard } from '../user/auth.guard';
+// import { AuthGuard } from '../user/auth.guard';
 import { ProductEditGuard } from '../service/product-edit.guard';
 
 const routes: Routes = [
   {
-    path: 'products',
-    canActivate: [AuthGuard],
+    path: '',
+    component: ProductListComponent
+  },
+  {
+    path: ':id',
+    component: ProductDetailComponent,
+    resolve: { resolvedData: ProductResolver }
+  },
+  {
+    path: ':id/edit',
+    component: ProductEditComponent,
+    canDeactivate: [ProductEditGuard],
+    resolve: { resolvedData: ProductResolver },
     children: [
       {
         path: '',
-        component: ProductListComponent
+        redirectTo: 'info',
+        pathMatch: 'full'
       },
       {
-        path: ':id',
-        component: ProductDetailComponent,
-        resolve: { resolvedData: ProductResolver }
+        path: 'info',
+        component: ProductEditInfoComponent
       },
       {
-        path: ':id/edit',
-        component: ProductEditComponent,
-        canDeactivate: [ProductEditGuard],
-        resolve: { resolvedData: ProductResolver },
-        children: [
-          {
-            path: '',
-            redirectTo: 'info',
-            pathMatch: 'full'
-          },
-          {
-            path: 'info',
-            component: ProductEditInfoComponent
-          },
-          {
-            path: 'tags',
-            component: ProductEditTagsComponent
-          }
-        ]
+        path: 'tags',
+        component: ProductEditTagsComponent
       }
     ]
-  },
-
+  }
 ];
 
 @NgModule({
