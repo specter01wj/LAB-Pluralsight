5bcba20f22db87c62cdc443d6194112022033504
Jin Wang <specter01wj@gmail.com>
2022-02-18 17:50:01 -0500

    v2

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/app-routing.module.ts
@@ -6,20 +6,22 @@ import { PageNotFoundComponent } from './not-found/page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
 import { SelectiveStrategy } from './service/selective-strategy.service';
 
-const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
-  {
-    path: 'products',
-    canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./products/product.module').then(m => m.ProductModule)
-  },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
-], { preloadingStrategy: SelectiveStrategy };
+// const routes: Routes = ;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: 'welcome', component: WelcomeComponent },
+    {
+      path: 'products',
+      canActivate: [AuthGuard],
+
+      data: { preload: true },
+      loadChildren: () =>
+        import('./products/product.module').then(m => m.ProductModule)
+    },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+  ], { preloadingStrategy: SelectiveStrategy })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
