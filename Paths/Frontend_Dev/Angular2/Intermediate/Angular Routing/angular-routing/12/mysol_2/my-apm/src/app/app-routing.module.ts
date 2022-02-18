4413001d53ab5a0d8b2e564ff6a16a83ae00fb92
Jin Wang <specter01wj@gmail.com>
2022-02-18 17:50:31 -0500

    v7

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ import { SelectiveStrategy } from './service/selective-strategy.service';
       canActivate: [AuthGuard],
 
 
+
       data: { preload: true },
       loadChildren: () =>
         import('./products/product.module').then(m => m.ProductModule)
