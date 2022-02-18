2958e9717fc01b5d4affd4dc38776d4092bc7ca9
Jin Wang <specter01wj@gmail.com>
2022-02-18 04:08:04 -0500

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/app-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/app-routing.module.ts
@@ -4,18 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './not-found/page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './service/selective-strategy.service';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   {
     path: 'products',
-    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./products/product.module').then(m => m.ProductModule)
   },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
-];
+], { preloadingStrategy: SelectiveStrategy };
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
