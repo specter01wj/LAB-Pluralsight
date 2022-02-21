5b7d1d6f3272fb08c639d6702db0e1301ed00184
Jin Wang <specter01wj@gmail.com>
2022-02-20 23:16:11 -0500

    v1

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Routing/angular-routing/12/mysol_2/my-apm/src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent {
               private messageService: MessageService) {
     router.events.subscribe((routerEvent: Event) => {
       this.checkRouterEvent(routerEvent);
+
     });
   }
 
