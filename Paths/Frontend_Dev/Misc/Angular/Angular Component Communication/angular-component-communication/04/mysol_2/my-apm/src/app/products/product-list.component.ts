b564c43b47f0f255afa7acb7aa05ef34b08d0ba7
Jin Wang <specter01wj@gmail.com>
2022-02-23 23:59:54 -0500

    v17

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/04/mysol_2/my-apm/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/04/mysol_2/my-apm/src/app/products/product-list.component.ts
@@ -52,21 +52,6 @@ export class ProductListComponent implements OnInit, AfterViewInit {
   onFilterChange(filter: string): void {
     this.listFilter = filter;
     this.performFilter(this.listFilter);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   }
 
   toggleImage(): void {
