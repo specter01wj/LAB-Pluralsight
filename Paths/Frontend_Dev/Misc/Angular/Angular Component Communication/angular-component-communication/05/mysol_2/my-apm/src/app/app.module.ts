63f9afae0b76ac52e21be50b1839ec28a7513742
Jin Wang <specter01wj@gmail.com>
2022-03-08 23:55:06 -0500

    v5

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/app.module.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientInMemoryWebApiModule  } from 'angular-in-memory-web-api';
 
 
 
+
 import { ProductData } from './products/product-data';
 
 import { AppRoutingModule } from './app-routing.module';
