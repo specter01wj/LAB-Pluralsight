9b5cf3734426c90b1db015c2845543f3b894752f
Jin Wang <specter01wj@gmail.com>
2022-03-11 19:46:48 -0500

    v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/app.module.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { UserModule } from './user/user.module';
   declarations: [
     AppComponent,
     ShellComponent,
+    
     MenuComponent,
     WelcomeComponent,
     PageNotFoundComponent
