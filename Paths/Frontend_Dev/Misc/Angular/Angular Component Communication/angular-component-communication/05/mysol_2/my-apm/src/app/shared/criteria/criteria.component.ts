3a41c55bd72a9eb8fb733822234f08c68c6b76df
Jin Wang <specter01wj@gmail.com>
2022-03-18 19:21:06 -0400

    v6

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-criteria',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./criteria.component.less']
 })
 export class CriteriaComponent implements OnInit {
+  listFilter: string;
+
+  @ViewChild('filterElement') filterElementRef: ElementRef;
 
   constructor() { }
 
