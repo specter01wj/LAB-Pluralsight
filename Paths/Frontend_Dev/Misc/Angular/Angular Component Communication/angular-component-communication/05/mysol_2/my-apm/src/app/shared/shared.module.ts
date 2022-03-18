e20c763986de74d2259fb3ad0f89880676c75659
Jin Wang <specter01wj@gmail.com>
2022-03-18 19:15:39 -0400

    v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/shared.module.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { StarComponent } from './star.component';
+import { CriteriaComponent } from './criteria/criteria.component';
 
 @NgModule({
-  declarations: [StarComponent],
+  declarations: [StarComponent, CriteriaComponent],
   imports: [
     CommonModule,
     FormsModule
