76fee17151057f0c318f20fa5fe0a4419f1bc11f
Jin Wang <specter01wj@gmail.com>
2022-03-19 23:41:25 -0400

    v13

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
@@ -15,6 +15,7 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 
 
 
+
   styleUrls: ['./criteria.component.less']
 })
 export class CriteriaComponent implements OnInit, AfterViewInit {
