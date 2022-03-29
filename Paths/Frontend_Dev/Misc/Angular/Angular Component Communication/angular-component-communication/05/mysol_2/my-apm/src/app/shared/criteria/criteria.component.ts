42f6717c0317e916092dca0320540f3b8c35c00a
Jin Wang <specter01wj@gmail.com>
2022-03-29 18:15:50 -0400

    03/26/2022 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
@@ -8,6 +8,7 @@ import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '
 export class CriteriaComponent implements OnInit, AfterViewInit {
   listFilter: string;
   @Input() displayDetail: boolean;
+  @Input() hitCount: number;
 
   @ViewChild('filterElement') filterElementRef: ElementRef;
 
