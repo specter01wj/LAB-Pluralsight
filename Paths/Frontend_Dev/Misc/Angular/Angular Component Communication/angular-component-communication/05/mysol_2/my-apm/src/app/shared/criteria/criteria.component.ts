a72e2174d84ac154244329a7f2e16346c5f4e0c5
Jin Wang <specter01wj@gmail.com>
2022-03-29 18:35:13 -0400

    03/26/2022 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/05/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
@@ -1,20 +1,29 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-criteria',
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.less']
 })
-export class CriteriaComponent implements OnInit, AfterViewInit {
+export class CriteriaComponent implements OnInit, OnChanges, AfterViewInit {
   listFilter: string;
   @Input() displayDetail: boolean;
   @Input() hitCount: number;
+  hitMessage: string;
 
   @ViewChild('filterElement') filterElementRef: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['hitCount'] && !changes['hitCount'].currentValue) {
+      this.hitMessage = 'No matches found';
+    } else {
+      this.hitMessage = 'Hits:' + this.hitCount;
+    }
   }
 
   ngAfterViewInit(): void {
