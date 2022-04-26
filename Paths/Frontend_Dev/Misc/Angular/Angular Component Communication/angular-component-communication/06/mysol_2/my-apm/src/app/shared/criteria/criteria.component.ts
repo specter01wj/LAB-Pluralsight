610989c9ba59c841373c61a78b3fc6bb073aef88
Jin Wang <specter01wj@gmail.com>
2022-04-26 16:24:11 -0400

    04/17/2022 v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/06/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/06/mysol_2/my-apm/src/app/shared/criteria/criteria.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, Output, EventEmitter, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-criteria',
@@ -6,12 +6,22 @@ import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleC
   styleUrls: ['./criteria.component.less']
 })
 export class CriteriaComponent implements OnInit, OnChanges, AfterViewInit {
-  listFilter: string = 'cart';
+  // listFilter: string = 'cart';
   @Input() displayDetail: boolean;
   @Input() hitCount: number;
   hitMessage: string;
+  @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
 
   @ViewChild('filterElement') filterElementRef: ElementRef;
+
+  private _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.valueChange.emit(value);
+  }
 
   constructor() { }
 
