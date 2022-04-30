6793a7a3aabfc8089a953d78341a43e543e21d17
Jin Wang <specter01wj@gmail.com>
2022-04-29 23:57:05 -0400

    v15

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
@@ -10,20 +10,6 @@ import { CriteriaComponent } from '../shared/criteria/criteria.component';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   styleUrls: ['./product-list.component.less']
 })
 export class ProductListComponent implements OnInit, AfterViewInit {
