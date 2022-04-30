78373e47b2334637550532624e54246555aa64e2
Jin Wang <specter01wj@gmail.com>
2022-04-29 23:55:51 -0400

    v5

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ import { CriteriaComponent } from '../shared/criteria/criteria.component';
 
 
 
+
   styleUrls: ['./product-list.component.less']
 })
 export class ProductListComponent implements OnInit, AfterViewInit {
