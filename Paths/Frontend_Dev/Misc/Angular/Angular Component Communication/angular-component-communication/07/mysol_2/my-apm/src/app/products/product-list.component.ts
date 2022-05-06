8387df0fc069f3c008e49cf25489a53249b6a3a9
Jin Wang <specter01wj@gmail.com>
2022-05-05 23:40:21 -0400

    v26

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
@@ -36,8 +36,6 @@ export class ProductListComponent implements OnInit, AfterViewInit {
             this.performFilter(this.parentListFilter);
         },
         (error: any) => this.errorMessage = <any>error
-
-
     );
   }
 
