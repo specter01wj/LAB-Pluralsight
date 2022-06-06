689608df11f450e5b4579991ed3d91f5247dd213
Jin Wang <specter01wj@gmail.com>
2022-06-05 23:50:19 -0400

    v13

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
@@ -62,6 +62,7 @@ export class ProductListComponent implements OnInit, AfterViewInit {
   toggleImage(): void {
       this.showImage = !this.showImage;
 
+
   }
 
   performFilter(filterBy?: string): void {
