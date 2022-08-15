1e3801db49dc8a7abf39bb51aa8fd928f3b28913
Jin Wang <specter01wj@gmail.com>
2022-08-14 23:18:27 -0400

    v1

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
@@ -33,6 +33,7 @@ export class ProductListComponent implements OnInit, AfterViewInit {
   }
   set showImage(value: boolean) {
     this.productParameterService.showImage = value;
+    
   }
 
   constructor(private productService: ProductService,
