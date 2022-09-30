0362fec3df29ace3f707b59863bf31a47267bdab
Jin Wang <specter01wj@gmail.com>
2022-09-29 23:51:05 -0400

    v16

--- Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
+++ Paths/Frontend_Dev/Misc/Angular/Angular Component Communication/angular-component-communication/07/mysol_2/my-apm/src/app/products/product-list.component.ts
@@ -57,6 +57,7 @@ export class ProductListComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.parentListFilter = this.filterComponent.listFilter;
+
   }
 
   toggleImage(): void {
