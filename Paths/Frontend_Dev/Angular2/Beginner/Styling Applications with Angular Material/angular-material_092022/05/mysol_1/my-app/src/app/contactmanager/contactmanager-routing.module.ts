ca51700191327a6f43881fef95c4ce9d4c06f705
Jin Wang <specter01wj@gmail.com>
2022-10-06 23:46:31 -0400

    v15

--- Paths/Frontend_Dev/Angular2/Beginner/Styling Applications with Angular Material/angular-material_092022/05/mysol_1/my-app/src/app/contactmanager/contactmanager-routing.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Styling Applications with Angular Material/angular-material_092022/05/mysol_1/my-app/src/app/contactmanager/contactmanager-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
     path: '', component: ContactmanagerComponent,
     children: [
       { path: '', component: MainContentComponent }
+
     ]
   },
   { path: '**', redirectTo: '' }
