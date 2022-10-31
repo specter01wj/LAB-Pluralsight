1ac55ecaab39200fe9283873d7d7b7edcd30d36a
Jin Wang <specter01wj@gmail.com>
2022-10-31 03:11:58 -0400

    v8

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/04/mysol_1/my-app/src/app/user/login.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/04/mysol_1/my-app/src/app/user/login.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
+import { Store } from '@ngrx/store';
+
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
@@ -13,10 +15,16 @@ export class LoginComponent implements OnInit {
 
   maskUserName: boolean;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private store: Store<any>,
+    private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-
+    this.store.select('users').subscribe(
+      users => {
+        if (users) {
+          this.maskUserName = users.maskUserName;
+        }
+      });
   }
 
   cancel(): void {
@@ -24,7 +32,10 @@ export class LoginComponent implements OnInit {
   }
 
   checkChanged(): void {
-    this.maskUserName = !this.maskUserName;
+    // this.maskUserName = !this.maskUserName;
+    this.store.dispatch(
+      { type: '[User] Mask User Name' }
+    );
   }
 
   login(loginForm: NgForm): void {
