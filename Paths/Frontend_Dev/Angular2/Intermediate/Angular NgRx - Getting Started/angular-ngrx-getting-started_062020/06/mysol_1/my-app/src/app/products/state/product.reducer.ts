d52f59bd77babbefcb41692a75913ae94b0b6e92
Jin Wang <specter01wj@gmail.com>
2022-11-06 23:37:14 -0500

    v2

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/06/mysol_1/my-app/src/app/products/state/product.reducer.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/06/mysol_1/my-app/src/app/products/state/product.reducer.ts
@@ -1,5 +1,13 @@
 import { createReducer, on, createAction } from '@ngrx/store';
 
+import { Product } from '../product';
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
 export const productReducer = createReducer(
   { showProductCode: true },
   on(createAction('[Product] Toggle Product Code'), state => {
