fb5b08c5d7a39cd4f0c3f4fa4da4cf3e2acfd67f
Jin Wang <specter01wj@gmail.com>
2022-11-06 23:58:32 -0500

    v4

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/06/mysol_1/my-app/src/app/products/state/product.reducer.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/06/mysol_1/my-app/src/app/products/state/product.reducer.ts
@@ -1,6 +1,11 @@
 import { createReducer, on, createAction } from '@ngrx/store';
+import * as AppState from '../../state/app.state';
 
 import { Product } from '../product';
+
+export interface State extends AppState.State {
+  products: ProductState;
+}
 
 export interface ProductState {
   showProductCode: boolean;
