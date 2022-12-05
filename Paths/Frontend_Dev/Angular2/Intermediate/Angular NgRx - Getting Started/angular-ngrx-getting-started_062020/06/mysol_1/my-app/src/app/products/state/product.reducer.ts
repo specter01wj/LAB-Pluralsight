9c9c014d9bde990cc59771ed0e30802130c22a71
Jin Wang <specter01wj@gmail.com>
2022-12-04 23:57:42 -0500

    v15

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/06/mysol_1/my-app/src/app/products/state/product.reducer.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/06/mysol_1/my-app/src/app/products/state/product.reducer.ts
@@ -11,20 +11,6 @@ export interface ProductState {
   showProductCode: boolean;
   currentProduct: Product;
   products: Product[];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 export const productReducer = createReducer(
