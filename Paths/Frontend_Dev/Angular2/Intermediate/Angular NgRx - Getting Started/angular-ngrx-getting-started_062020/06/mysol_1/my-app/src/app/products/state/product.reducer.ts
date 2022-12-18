712853d9718fa2e206d40ce2f7f04c1f8364d3d9
Jin Wang <specter01wj@gmail.com>
2022-12-17 23:06:08 -0500

    v23

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/06/mysol_1/my-app/src/app/products/state/product.reducer.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular NgRx - Getting Started/angular-ngrx-getting-started_062020/06/mysol_1/my-app/src/app/products/state/product.reducer.ts
@@ -17,6 +17,7 @@ export const productReducer = createReducer(
   { showProductCode: true },
 
 
+
   on(createAction('[Product] Toggle Product Code'), state => {
     // console.log('original state: ' + JSON.stringify(state));
     return {
