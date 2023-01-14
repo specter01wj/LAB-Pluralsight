ae0d186561e22750f3245be90958347c1daf3dca
Jin Wang <specter01wj@gmail.com>
2023-01-14 18:16:25 -0500

    v2

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/04/mysol_1/apm-form/src/app/customers/customers.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-2-reactive-forms_2021/04/mysol_1/apm-form/src/app/customers/customers.component.ts
@@ -19,6 +19,8 @@ export class CustomersComponent {
   save(customerForm: NgForm): void {
     console.log(customerForm.form);
 
+
+
     console.log('Saved: ' + JSON.stringify(customerForm.value));
   }
 
