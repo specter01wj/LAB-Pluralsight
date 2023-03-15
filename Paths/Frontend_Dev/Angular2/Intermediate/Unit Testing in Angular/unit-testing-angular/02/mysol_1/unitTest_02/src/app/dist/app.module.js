e7e5e166264262e314270471ab40e97b262468f1
Jin Wang <specter01wj@gmail.com>
2023-03-15 19:25:45 -0400

    v7

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/02/mysol_1/unitTest_02/src/app/dist/app.module.js
@@ -0,0 +1,53 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppModule = void 0;
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var forms_1 = require("@angular/forms");
+var http_1 = require("@angular/common/http");
+var angular_in_memory_web_api_1 = require("angular-in-memory-web-api");
+var in_memory_data_service_1 = require("./in-memory-data.service");
+var app_routing_module_1 = require("./app-routing.module");
+var app_component_1 = require("./app.component");
+var dashboard_component_1 = require("./dashboard/dashboard.component");
+var hero_detail_component_1 = require("./hero-detail/hero-detail.component");
+var heroes_component_1 = require("./heroes/heroes.component");
+var hero_search_component_1 = require("./hero-search/hero-search.component");
+var messages_component_1 = require("./messages/messages.component");
+var strength_pipe_1 = require("./strength/strength.pipe");
+var hero_component_1 = require("./hero/hero.component");
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            declarations: [
+                app_component_1.AppComponent,
+                dashboard_component_1.DashboardComponent,
+                heroes_component_1.HeroesComponent,
+                hero_detail_component_1.HeroDetailComponent,
+                messages_component_1.MessagesComponent,
+                hero_search_component_1.HeroSearchComponent,
+                strength_pipe_1.StrengthPipe,
+                hero_component_1.HeroComponent
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule,
+                http_1.HttpClientModule,
+                app_routing_module_1.AppRoutingModule,
+                angular_in_memory_web_api_1.HttpClientInMemoryWebApiModule.forRoot(in_memory_data_service_1.InMemoryDataService, { dataEncapsulation: false })
+            ],
+            providers: [],
+            bootstrap: [app_component_1.AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/02/mysol_1/unitTest_02/src/app/dist/hero.service.js
@@ -0,0 +1,102 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.HeroService = void 0;
+var core_1 = require("@angular/core");
+var http_1 = require("@angular/common/http");
+var rxjs_1 = require("rxjs");
+var operators_1 = require("rxjs/operators");
+var httpOptions = {
+    headers: new http_1.HttpHeaders({ 'Content-Type': 'application/json' })
+};
+var HeroService = /** @class */ (function () {
+    function HeroService(http, messageService) {
+        this.http = http;
+        this.messageService = messageService;
+        this.heroesUrl = 'api/heroes'; // URL to web api
+    }
+    /** GET heroes from the server */
+    HeroService.prototype.getHeroes = function () {
+        var _this = this;
+        return this.http.get(this.heroesUrl)
+            .pipe(operators_1.tap(function (heroes) { return _this.log("fetched heroes"); }), operators_1.catchError(this.handleError('getHeroes', [])));
+    };
+    /** GET hero by id. Return `undefined` when id not found */
+    HeroService.prototype.getHeroNo404 = function (id) {
+        var _this = this;
+        var url = this.heroesUrl + "/?id=" + id;
+        return this.http.get(url)
+            .pipe(operators_1.map(function (heroes) { return heroes[0]; }), // returns a {0|1} element array
+        operators_1.tap(function (h) {
+            var outcome = h ? "fetched" : "did not find";
+            _this.log(outcome + " hero id=" + id);
+        }), operators_1.catchError(this.handleError("getHero id=" + id)));
+    };
+    /** GET hero by id. Will 404 if id not found */
+    HeroService.prototype.getHero = function (id) {
+        var _this = this;
+        var url = this.heroesUrl + "/" + id;
+        return this.http.get(url).pipe(operators_1.tap(function (_) { return _this.log("fetched hero id=" + id); }), operators_1.catchError(this.handleError("getHero id=" + id)));
+    };
+    /* GET heroes whose name contains search term */
+    HeroService.prototype.searchHeroes = function (term) {
+        var _this = this;
+        if (!term.trim()) {
+            // if not search term, return empty hero array.
+            return rxjs_1.of([]);
+        }
+        return this.http.get("api/heroes/?name=" + term).pipe(operators_1.tap(function (_) { return _this.log("found heroes matching \"" + term + "\""); }), operators_1.catchError(this.handleError('searchHeroes', [])));
+    };
+    //////// Save methods //////////
+    /** POST: add a new hero to the server */
+    HeroService.prototype.addHero = function (hero) {
+        var _this = this;
+        return this.http.post(this.heroesUrl, hero, httpOptions).pipe(operators_1.tap(function (hero) { return _this.log("added hero w/ id=" + hero.id); }), operators_1.catchError(this.handleError('addHero')));
+    };
+    /** DELETE: delete the hero from the server */
+    HeroService.prototype.deleteHero = function (hero) {
+        var _this = this;
+        var id = typeof hero === 'number' ? hero : hero.id;
+        var url = this.heroesUrl + "/" + id;
+        return this.http["delete"](url, httpOptions).pipe(operators_1.tap(function (_) { return _this.log("deleted hero id=" + id); }), operators_1.catchError(this.handleError('deleteHero')));
+    };
+    /** PUT: update the hero on the server */
+    HeroService.prototype.updateHero = function (hero) {
+        var _this = this;
+        return this.http.put(this.heroesUrl, hero, httpOptions).pipe(operators_1.tap(function (_) { return _this.log("updated hero id=" + hero.id); }), operators_1.catchError(this.handleError('updateHero')));
+    };
+    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    HeroService.prototype.handleError = function (operation, result) {
+        var _this = this;
+        if (operation === void 0) { operation = 'operation'; }
+        return function (error) {
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+            // TODO: better job of transforming error for user consumption
+            _this.log(operation + " failed: " + error.message);
+            // Let the app keep running by returning an empty result.
+            return rxjs_1.of(result);
+        };
+    };
+    /** Log a HeroService message with the MessageService */
+    HeroService.prototype.log = function (message) {
+        this.messageService.add('HeroService: ' + message);
+    };
+    HeroService = __decorate([
+        core_1.Injectable({
+            providedIn: 'root'
+        })
+    ], HeroService);
+    return HeroService;
+}());
+exports.HeroService = HeroService;
