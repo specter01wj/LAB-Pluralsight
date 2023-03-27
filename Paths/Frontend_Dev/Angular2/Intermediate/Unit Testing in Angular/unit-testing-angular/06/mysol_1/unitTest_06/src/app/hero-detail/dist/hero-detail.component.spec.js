35c2d71a8b8c038d873a5c57349e834fa1db8f81
Jin Wang <specter01wj@gmail.com>
2023-03-27 02:12:34 -0400

    v3

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Unit Testing in Angular/unit-testing-angular/06/mysol_1/unitTest_06/src/app/hero-detail/dist/hero-detail.component.spec.js
@@ -0,0 +1,35 @@
+"use strict";
+exports.__esModule = true;
+var testing_1 = require("@angular/core/testing");
+var hero_detail_component_1 = require("./hero-detail.component");
+var router_1 = require("@angular/router");
+var hero_service_1 = require("../hero.service");
+var common_1 = require("@angular/common");
+var rxjs_1 = require("rxjs");
+var forms_1 = require("@angular/forms");
+fdescribe('HeroDetailComponent', function () {
+    var fixture;
+    var mockActivatedRoute, mockHeroService, mockLocation;
+    beforeEach(function () {
+        mockActivatedRoute = {
+            snapshot: { paramMap: { get: function () { return '3'; } } }
+        };
+        mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
+        mockLocation = jasmine.createSpyObj(['back']);
+        testing_1.TestBed.configureTestingModule({
+            imports: [forms_1.FormsModule],
+            declarations: [hero_detail_component_1.HeroDetailComponent],
+            providers: [
+                { provide: router_1.ActivatedRoute, useValue: mockActivatedRoute },
+                { provide: hero_service_1.HeroService, useValue: mockHeroService },
+                { provide: common_1.Location, useValue: mockLocation },
+            ]
+        });
+        fixture = testing_1.TestBed.createComponent(hero_detail_component_1.HeroDetailComponent);
+        mockHeroService.getHero.and.returnValue(rxjs_1.of({ id: 3, name: 'SuperDude', strength: 100 }));
+    });
+    it('should render hero name in a h2 tag', function () {
+        fixture.detectChanges();
+        expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERDUDE');
+    });
+});
