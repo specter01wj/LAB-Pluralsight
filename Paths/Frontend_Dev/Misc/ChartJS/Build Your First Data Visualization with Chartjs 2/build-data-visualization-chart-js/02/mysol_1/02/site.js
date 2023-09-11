fd739091b1ac583af38570030db263fd9b51f7d0
Jin Wang <specter01wj@gmail.com>
2023-09-11 00:33:51 -0400

    v3

--- Paths/Frontend_Dev/Misc/ChartJS/Build Your First Data Visualization with Chartjs 2/build-data-visualization-chart-js/02/mysol_1/02/site.js
+++ Paths/Frontend_Dev/Misc/ChartJS/Build Your First Data Visualization with Chartjs 2/build-data-visualization-chart-js/02/mysol_1/02/site.js
@@ -8,6 +8,11 @@
                     "San Francisco", "San Diego"],
                 datasets: [
                     {
+                        backgroundColor: "green",
+                        label: "Revenues this year",
+                        data: [84532, 57543, 43422, 64532, 74323]
+                    },
+                    {
                         backgroundColor: "red",
                         label: "Revenues last year",
                         data: [64532, 77543, 33422, 24532, 64323]
