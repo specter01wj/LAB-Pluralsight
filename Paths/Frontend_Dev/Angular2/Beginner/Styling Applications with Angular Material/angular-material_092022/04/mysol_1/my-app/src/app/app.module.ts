794f6bd0fa266625b0dcf0211094c47d03469fb0
Jin Wang <specter01wj@gmail.com>
2023-12-21 16:53:12 -0500

    v4

--- Paths/Frontend_Dev/Angular2/Beginner/Styling Applications with Angular Material/angular-material_092022/04/mysol_1/my-app/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Styling Applications with Angular Material/angular-material_092022/04/mysol_1/my-app/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SharedAngularMaterialModule } from './shared/angular-material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedAngularMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
