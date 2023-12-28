e1a09b17ee7f8d3b00b7d75996b0f9217acdec59
Jin Wang <specter01wj@gmail.com>
2023-12-28 18:17:45 -0500

    v3

--- Paths/Frontend_Dev/Angular2/Beginner/Styling Applications with Angular Material/angular-material_092022/06/mysol_1/my-app/src/app/contactmanager/services/user.service.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Styling Applications with Angular Material/angular-material_092022/06/mysol_1/my-app/src/app/contactmanager/services/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private _users: BehaviorSubject<User[]>;
 
-  constructor() { }
+  private dataStore: {
+    users: User[];
+  }
+
+  constructor(private http: HttpClient) {
+    this.dataStore = { users: [ ] };
+    this._users = new BehaviorSubject<User[]>([]);
+  }
+
+  loadAll() {
+    const usersUrl = 'https://angular-material-api.azurewebsites.net/users'
+
+    return this.http.get<User[]>(usersUrl)
+      .subscribe(data => {
+        this.dataStore.users = data;
+      }, error => {
+        console.log("Failed to fetch users");
+      });
+  }
+
 }
