673b5a0eb456345a57724c86fcdd3e8f306c920f
Jin Wang <specter01wj@gmail.com>
2023-12-29 01:14:51 -0500

    v3

--- Paths/Frontend_Dev/Angular2/Beginner/Styling Applications with Angular Material/angular-material_092022/07/mysol_1/my-app/src/app/contactmanager/components/notes/notes.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Styling Applications with Angular Material/angular-material_092022/07/mysol_1/my-app/src/app/contactmanager/components/notes/notes.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Note } from '../../models/note';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
 })
-export class NotesComponent {
+export class NotesComponent implements OnInit, AfterViewInit {
+  @Input() notes: Note[];
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+  }
+
 
 }
