8c2f4eed0909a2a189fb4db78e326579f94c79c0
Jin Wang <specter01wj@gmail.com>
2024-01-08 01:56:21 -0500

    v3

--- Paths/Backend_Dev/Spring Framework 6 and Spring Boot 3/Beginner/Spring Framework 6 Fundamentals/spring-framework-6-fundamentals/02/mysol_1/conference/src/main/java/com/jinwang/service/SpeakServiceImpl.java
+++ Paths/Backend_Dev/Spring Framework 6 and Spring Boot 3/Beginner/Spring Framework 6 Fundamentals/spring-framework-6-fundamentals/02/mysol_1/conference/src/main/java/com/jinwang/service/SpeakServiceImpl.java
@@ -1,4 +1,17 @@
 package com.jinwang.service;
 
-public class SpeakServiceImpl {
+import com.jinwang.model.Speaker;
+import com.jinwang.repository.SpeakerRepository;
+import com.jinwang.repository.StubSpeakerRepositoryImpl;
+
+import java.util.List;
+
+public class SpeakServiceImpl implements SpeakService {
+
+    private SpeakerRepository repository = new StubSpeakerRepositoryImpl();
+
+    @Override
+    public List<Speaker> findAll() {
+        return repository.findAll();
+    }
 }
