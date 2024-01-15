c57dcef83f1867b8785e530fb123d86b69b32725
Jin Wang <specter01wj@gmail.com>
2024-01-15 00:22:23 -0500

    v3

--- Paths/Backend_Dev/Spring Framework 6 and Spring Boot 3/Beginner/Spring Framework 6 Fundamentals/spring-framework-6-fundamentals/04/mysol_1/conference-java/src/main/java/AppConfig.java
+++ Paths/Backend_Dev/Spring Framework 6 and Spring Boot 3/Beginner/Spring Framework 6 Fundamentals/spring-framework-6-fundamentals/04/mysol_1/conference-java/src/main/java/AppConfig.java
@@ -1,7 +1,16 @@
 import com.jinwang.service.SpeakService;
+import com.jinwang.service.SpeakServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+
+    @Bean(name="speakerService")
+    public SpeakService getSpeakerService() {
+        return new SpeakServiceImpl();
+    }
+
+
 }
