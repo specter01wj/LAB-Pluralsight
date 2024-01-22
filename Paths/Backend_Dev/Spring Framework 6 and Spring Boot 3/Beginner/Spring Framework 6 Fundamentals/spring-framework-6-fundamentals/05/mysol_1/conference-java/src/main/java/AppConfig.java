3d568c67b7aed0ba53359483c426cc7f75ff739e
Jin Wang <specter01wj@gmail.com>
2024-01-22 01:28:49 -0500

    v2

--- Paths/Backend_Dev/Spring Framework 6 and Spring Boot 3/Beginner/Spring Framework 6 Fundamentals/spring-framework-6-fundamentals/05/mysol_1/conference-java/src/main/java/AppConfig.java
+++ Paths/Backend_Dev/Spring Framework 6 and Spring Boot 3/Beginner/Spring Framework 6 Fundamentals/spring-framework-6-fundamentals/05/mysol_1/conference-java/src/main/java/AppConfig.java
@@ -2,14 +2,17 @@ import com.jinwang.repository.SpeakerRepository;
 import com.jinwang.repository.StubSpeakerRepositoryImpl;
 import com.jinwang.service.SpeakService;
 import com.jinwang.service.SpeakServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
 
     @Bean(name="speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakService getSpeakerService() {
         SpeakServiceImpl service = new SpeakServiceImpl(getSpeakerRepository());
 //        service.setRepository(getSpeakerRepository());
