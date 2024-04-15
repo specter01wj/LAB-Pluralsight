48f3f53140b028b6e093a9c7fb33f5ca5c887f0c
Jin Wang <specter01wj@gmail.com>
2024-04-15 00:24:19 -0400

    v1

---
+++ Paths/Frontend_Dev/Angular2/old/Beginner/Angular Forms/angular-forms/04/demos/m4/src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings : UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
+
+  userSettings : UserSettings = { ...this.originalUserSettings };
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
