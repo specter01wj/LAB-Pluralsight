2cef15e7351e2ee78e87f6399a0c04dcfadd6e5a
Jin Wang <specter01wj@gmail.com>
2024-05-03 01:59:14 -0400

    v8

---
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/06/joes-robot-shop-m6-end/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CatalogComponent,
+    SiteHeaderComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
