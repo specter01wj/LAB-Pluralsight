1840e05972be0bf95c0c55af0b4f17ae6c648612
Jin Wang <specter01wj@gmail.com>
2024-05-05 22:46:24 -0400

    v6

---
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/12/joes-robot-shop-m12-end/src/app/app.component.spec.ts
@@ -0,0 +1,68 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('fakes', () => {
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('1', () => {
+    expect(1).toBe(1);
+  });
+});
