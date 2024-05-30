02ecc0ef548e0474b2bd54889c5ae4ff963f39c1
Jin Wang <specter01wj@gmail.com>
2024-05-30 01:31:15 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/09/mysol_1/joes-robot-shop/src/app/service/cart.service.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/09/mysol_1/joes-robot-shop/src/app/service/cart.service.ts
@@ -10,20 +10,32 @@ export class CartService {
   // private cart: Product[] = [];
   private cart: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.http.get<Product[]>('/api/cart').subscribe({
+      next: (cart) => this.cart.next(cart),
+    });
+  }
 
   getCart(): Observable<Product[]> {
     return this.cart.asObservable();
   }
 
   add(product: Product) {
-    this.cart.push(product);
+    // this.cart.push(product);
+    const newCart = [...this.cart.getValue(), product];
+    this.cart.next(newCart);
     this.http.post('/api/cart', this.cart).subscribe(() => {
       console.log('added ' + product.name + ' to cart!');
     });
   }
 
-
+  remove(product: Product) {
+    let newCart = this.cart.getValue().filter((i) => i !== product);
+    this.cart.next(newCart);
+    this.http.post('/api/cart', newCart).subscribe(() => {
+      console.log('removed ' + product.name + ' from cart!');
+    });
+  }
 
 
 }
