a0e74eefa28dff24e24841ce49e1af766e8486b1
Jin Wang <specter01wj@gmail.com>
2024-06-18 01:56:21 -0400

    v2

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/10/mysol_1/joes-robot-shop/src/app/service/user.service.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/10/mysol_1/joes-robot-shop/src/app/service/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+
+import { User, UserCredentials } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private user: BehaviorSubject<User | null>;
+
+  constructor(private http: HttpClient) {
+    this.user = new BehaviorSubject<User | null>(null);
+  }
+
+  getUser(): Observable<User | null> {
+    return this.user;
+  }
+
+  signIn(credentials: UserCredentials): Observable<User> {
+    return this.http
+      .post<User>('/api/sign-in', credentials)
+      .pipe(map((user: User) => {
+        this.user.next(user);
+        return user;
+      }));
+  }
+
+  signOut() {
+    this.user.next(null);
+  }
 }
--- Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/10/mysol_1/joes-robot-shop/src/app/user/sign-in.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/10/mysol_1/joes-robot-shop/src/app/user/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserCredentials } from '../model/user';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
+  credentials: UserCredentials = { email: '', password: '' };
+  signInError: boolean = false;
 
+  constructor(private userService: UserService, private router: Router) { }
+
+  signIn() {
+    this.signInError = false;
+    this.userService.signIn(this.credentials).subscribe({
+      next: () => this.router.navigate(['/catalog']),
+      error: () => (this.signInError = true)
+    });
+  }
 }
