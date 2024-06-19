8d7242463e4a8a6fe7863f6ea77fbff3ef41bea1
Jin Wang <specter01wj@gmail.com>
2024-06-19 01:39:16 -0400

    v6

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/10/mysol_1/joes-robot-shop/src/app/user/template-form-controls/template-form-controls.component.ts
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/10/mysol_1/joes-robot-shop/src/app/user/template-form-controls/template-form-controls.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template-form-controls',
   templateUrl: './template-form-controls.component.html',
   styleUrl: './template-form-controls.component.scss'
 })
-export class TemplateFormControlsComponent {
+export class TemplateFormControlsComponent implements OnInit {
+  textInput: string = '';
+  numericInput: number = 0;
+  stringInput: string = '';
+  numericSelect: number = 0;
+  checkboxInput: boolean = false;
+  radioInput: number | null = null;
 
+  selectOptions: any[] = [
+    { text: 'Option One', value: 1 },
+    { text: 'Option Two', value: 2 },
+  ];
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  getType(value: any) {
+    if (value === null || value === undefined) return '';
+
+    console.log('ns', this.numericSelect);
+    return typeof value;
+  }
 }
