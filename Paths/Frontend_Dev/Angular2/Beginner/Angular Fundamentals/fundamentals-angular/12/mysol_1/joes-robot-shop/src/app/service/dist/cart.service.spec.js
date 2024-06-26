4c70244d59e0cbf3914838bd89833a94477ca4a0
Jin Wang <specter01wj@gmail.com>
2024-06-26 10:08:13 -0400

    v1

--- Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/12/mysol_1/joes-robot-shop/src/app/service/dist/cart.service.spec.js
+++ Paths/Frontend_Dev/Angular2/Beginner/Angular Fundamentals/fundamentals-angular/12/mysol_1/joes-robot-shop/src/app/service/dist/cart.service.spec.js
@@ -1,2 +1,29 @@
-zx;
-zx;
+"use strict";
+exports.__esModule = true;
+var testing_1 = require("@angular/core/testing");
+var testing_2 = require("@angular/common/http/testing");
+var cart_service_1 = require("./cart.service");
+var http_1 = require("@angular/common/http");
+fdescribe('CartService', function () {
+    var httpClient;
+    var TESTDATA = [{ id: 1 }, { id: 2 }];
+    var service;
+    var httpTestingController;
+    beforeEach(function () {
+        testing_1.TestBed.configureTestingModule({
+            providers: [cart_service_1.CartService],
+            imports: [testing_2.HttpClientTestingModule]
+        });
+        httpClient = testing_1.TestBed.inject(http_1.HttpClient);
+        httpTestingController = testing_1.TestBed.inject(testing_2.HttpTestingController);
+        service = testing_1.TestBed.inject(cart_service_1.CartService);
+    });
+    it('should have the returned data in the cart', function (done) {
+        var req = httpTestingController.expectOne('/api/cart');
+        req.flush(TESTDATA);
+        service.getCart().subscribe(function (data) {
+            expect(data.length).toEqual(2);
+            done();
+        });
+    });
+});
