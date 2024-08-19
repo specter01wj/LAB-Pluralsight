bec7f23ab5ccc2bdcdb98a467daf9d2ed06a8caa
Jin Wang <specter01wj@gmail.com>
2024-08-19 02:57:01 -0400

    v2

--- Paths/Frontend_Dev/Angular2/Intermediate/Styling Angular Applications/angular-styling-applications/04/mysol_1/my-app/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Styling Angular Applications/angular-styling-applications/04/mysol_1/my-app/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  pills = [
+    {
+        label: 'HTML'
+    },
+    {
+        label: 'CSS'
+    },
+    {
+        label: 'SASS'
+    }
+];
 }
--- Paths/Frontend_Dev/Angular2/Intermediate/Styling Angular Applications/angular-styling-applications/04/mysol_1/my-app/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Styling Angular Applications/angular-styling-applications/04/mysol_1/my-app/src/app/app.module.ts
@@ -5,10 +5,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 
+// Content Components
+import { AccordionComponent }       from './shared/components/content/accordion/accordion.component';
+import { AccordionPaneComponent }   from './shared/components/content/accordion/accordion-pane/accordion-pane.component';
+import { TabsComponent }            from './shared/components/content/tabs/tabs.component';
+import { TabsPaneComponent }        from './shared/components/content/tabs/tabs-pane/tabs-pane.component';
+
+// Navigation Components
+import { NavBarPrimaryComponent }   from './shared/components/navigation/navbar-primary/navbar-primary.component';
+import { NavBarSecondaryComponent } from './shared/components/navigation/navbar-secondary/navbar-secondary.component';
+import { PillsComponent }           from './shared/components/navigation/pills/pills.component';
+
+// PopUp Components
+import {
+  ModalComponent,
+  ModalContentDirective }           from './shared/components/popups/modal/modal.component';
+import {
+  ToolTipComponent,
+  TooltipMessageDirective }         from './shared/components/popups/tooltip/tooltip.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    AccordionComponent,
+    AccordionPaneComponent,
+    TabsComponent,
+    TabsPaneComponent,
+    NavBarPrimaryComponent,
+    NavBarSecondaryComponent,
+    PillsComponent,
+    ModalComponent,
+    ModalContentDirective,
+    ToolTipComponent,
+    TooltipMessageDirective
   ],
   imports: [
     BrowserModule,
