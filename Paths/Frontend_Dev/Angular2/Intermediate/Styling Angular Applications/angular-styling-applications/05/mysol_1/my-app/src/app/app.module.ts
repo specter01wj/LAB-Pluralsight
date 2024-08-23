8f07b8c0cec82c0a70ca94043e4c6f246bba7fda
Jin Wang <specter01wj@gmail.com>
2024-08-23 01:25:57 -0400

    v6

--- Paths/Frontend_Dev/Angular2/Intermediate/Styling Angular Applications/angular-styling-applications/05/mysol_1/my-app/src/app/app.module.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Styling Angular Applications/angular-styling-applications/05/mysol_1/my-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 
 // Content Components
 import { AccordionComponent }       from './shared/components/content/accordion/accordion.component';
