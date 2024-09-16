83cc424ddafe48e27c6e1e18efc68c0f0ea9c335
Jin Wang <specter01wj@gmail.com>
2024-09-15 22:39:24 -0400

    v1

---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/04/demos/exercise-files/before/CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -0,0 +1,13 @@
+package com.pluralsight.calcengine;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/04/demos/exercise-files/before/LetsGetLogical/src/com/pluralsight/letsgetlogical/Main.java
@@ -0,0 +1,15 @@
+package com.pluralsight.letsgetlogical;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int students = 150;
+        int rooms = 4;
+
+
+
+
+        System.out.println("");
+        System.out.println("*** end of program ***");
+    }
+}
