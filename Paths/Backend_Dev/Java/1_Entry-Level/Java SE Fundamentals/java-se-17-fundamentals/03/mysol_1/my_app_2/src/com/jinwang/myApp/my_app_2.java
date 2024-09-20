0c703e1801ac55d245d2ef1252cbe8fe0cd05bcf
Jin Wang <specter01wj@gmail.com>
2024-09-20 01:41:10 -0400

    v2

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/03/mysol_1/my_app_2/src/com/jinwang/myApp/my_app_2.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/03/mysol_1/my_app_2/src/com/jinwang/myApp/my_app_2.java
@@ -9,7 +9,27 @@ public class my_app_2 {
     }
 
     public void testMyApp_1() {
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
 
+        System.out.println("floatVal: " + floatVal);
+        System.out.println("doubleVal: " + doubleVal);
+        System.out.println("byteVal: " + byteVal);
+        System.out.println("shortVal: " + shortVal);
+        System.out.println("longVal: " + longVal);
+
+        short result1 = (short) longVal;
+        short result2 = (short) (byteVal - longVal);
+        float result3 = longVal - floatVal;
+
+        System.out.println("result1: " + result1);
+        System.out.println("result2: " + result2);
+        System.out.println("result3: " + result3);
+
+        System.out.println("Success");
 
     }
 
