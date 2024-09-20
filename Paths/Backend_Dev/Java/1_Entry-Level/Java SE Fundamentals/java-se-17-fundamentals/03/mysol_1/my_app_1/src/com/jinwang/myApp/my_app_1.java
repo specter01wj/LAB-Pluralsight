9e9e1bfdd390e74a5d116157a6fd05fb8783891d
Jin Wang <specter01wj@gmail.com>
2024-09-19 23:48:02 -0400

    v4

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/03/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/03/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
@@ -16,6 +16,15 @@ public class my_app_1 {
         int someVal_2 = 5;
         System.out.println(someVal_2++);
         System.out.println(someVal_2);
+
+        int myVal_1 = 50;
+        myVal_1 -= 5;
+        System.out.println(myVal_1);
+
+        int myVal_2 = 100;
+        int val1 = 5, val2 = 10;
+        System.out.println(myVal_2 /= val1 * val2);
+
     }
 
 }
