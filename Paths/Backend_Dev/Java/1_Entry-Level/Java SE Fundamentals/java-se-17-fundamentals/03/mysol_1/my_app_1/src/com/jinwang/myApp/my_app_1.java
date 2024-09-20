c2d016982fd5863f73908e8b0f133a412ad88749
Jin Wang <specter01wj@gmail.com>
2024-09-20 00:31:12 -0400

    v5

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/03/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/03/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
@@ -5,10 +5,11 @@ public class my_app_1 {
 
     public static void main(String[] args) {
         my_app_1 sol = new my_app_1();
-        sol.testMyApp();
+        sol.testMyApp_1();
+        sol.testMyApp_2();
     }
 
-    public void testMyApp() {
+    public void testMyApp_1() {
         int someVal = 5;
         System.out.println(++someVal);
         System.out.println(someVal);
@@ -24,6 +25,26 @@ public class my_app_1 {
         int myVal_2 = 100;
         int val1 = 5, val2 = 10;
         System.out.println(myVal_2 /= val1 * val2);
+
+    }
+
+    public void testMyApp_2() {
+        int valA = 21;
+        int valB = 6;
+        int valC = 3;
+        int valD = 1;
+
+        int result1 = valA - valB / valC;
+        int result2 = (valA - valB) / valC;
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+        int result3 = valA / valC * valD + valB;
+        int result4 = valA / (valC * (valD + valB));
+
+        System.out.println(result3);
+        System.out.println(result4);
 
     }
 
