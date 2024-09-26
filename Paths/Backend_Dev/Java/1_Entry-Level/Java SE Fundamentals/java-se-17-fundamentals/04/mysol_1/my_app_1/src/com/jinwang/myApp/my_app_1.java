2c3c867ccdf76c74e42fed8cc4c1fe86bd7c606b
Jin Wang <specter01wj@gmail.com>
2024-09-26 02:03:27 -0400

    v2

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/04/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/04/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
@@ -9,7 +9,30 @@ public class my_app_1 {
     }
 
     public void testMyApp_1() {
+        double value1 = 100.0d;
+        double value2 = 0.0d;
+        double result = 0.0d;
+        char opCode = 'd';
 
+        switch(opCode) {
+            case 'a':
+                result = value1 + value2;
+                break;
+            case 's':
+                result = value1 - value2;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            case 'd':
+                result = value2 != 0 ? value1 / value2 : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+        }
+        System.out.println(result);
     }
 
 }
