f454a5e0a1c3bbc46743d2ee9191d06238fe9c15
Jin Wang <specter01wj@gmail.com>
2024-09-26 02:05:12 -0400

    v3

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/04/mysol_1/my_app_2/src/com/jinwang/myApp/my_app_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/04/mysol_1/my_app_2/src/com/jinwang/myApp/my_app_1.java
@@ -9,6 +9,14 @@ public class my_app_1 {
     }
 
     public void testMyApp_1() {
-        
+        int students = 150;
+        int rooms = 0;
+
+        if(rooms != 0 && students/rooms > 30) {
+            System.out.println("Crowded");
+        }
+
+        System.out.println("");
+        System.out.println("*** end of program ***");
     }
 }
