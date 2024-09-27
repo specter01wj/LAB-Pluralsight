77bf27e5991be43c80e16f669b6ae2f92f9c07a7
Jin Wang <specter01wj@gmail.com>
2024-09-27 12:17:41 -0400

    v1

---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/05/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
@@ -0,0 +1,15 @@
+package com.jinwang.myApp;
+import java.util.*;
+
+public class my_app_1 {
+
+    public static void main(String[] args) {
+        my_app_1 sol = new my_app_1();
+        sol.testMyApp_1();
+    }
+
+    public void testMyApp_1() {
+        
+    }
+
+}
