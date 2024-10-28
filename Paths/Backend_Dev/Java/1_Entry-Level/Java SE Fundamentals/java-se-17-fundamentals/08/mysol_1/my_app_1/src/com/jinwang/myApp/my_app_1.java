f0abf0d37239435db5bc85b9f4b4a177c04f5aa7
Jin Wang <specter01wj@gmail.com>
2024-10-28 00:01:40 -0400

    v2

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/08/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/08/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
@@ -1,4 +1,21 @@
 package com.jinwang.myApp;
+import java.util.*;
 
 public class my_app_1 {
+
+    public static void main(String[] args) {
+        my_app_1 sol = new my_app_1();
+        if(args.length == 0) {
+            performCalculations();
+        } else if(args.length == 1 && args[0].equals("interactive")) {
+            executeInteractively();
+        } else if(args.length == 3) {
+            handleCommandLine(args);
+        } else {
+            System.out.println("Please provide an operation code and 2 numeric values");\
+        }
+    }
+
+
+
 }
