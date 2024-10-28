845ed379517b9c43e8a4cbcc19c89bc76c73420e
Jin Wang <specter01wj@gmail.com>
2024-10-28 02:36:52 -0400

    v1

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/08/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/08/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
@@ -16,6 +16,18 @@ public class my_app_1 {
         }
     }
 
+    static void performCalculations() {
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(11.0d, 3.0d, 'm');
+
+        for(MathEquation equation : equations) {
+            equation.execute();
+            System.out.println("result = " + equation.getResult());
+        }
+    }
 
 
 }
