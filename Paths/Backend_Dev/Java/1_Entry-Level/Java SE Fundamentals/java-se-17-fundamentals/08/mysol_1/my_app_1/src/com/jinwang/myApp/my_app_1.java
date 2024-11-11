9a3b76b58ac407110747dc54964d59df5ab7eb66
Jin Wang <specter01wj@gmail.com>
2024-11-10 22:08:55 -0500

    v1

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/08/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/08/mysol_1/my_app_1/src/com/jinwang/myApp/my_app_1.java
@@ -12,7 +12,7 @@ public class my_app_1 {
         } else if(args.length == 3) {
             handleCommandLine(args);
         } else {
-            System.out.println("Please provide an operation code and 2 numeric values");\
+            System.out.println("Please provide an operation code and 2 numeric values");
         }
     }
 
@@ -27,6 +27,109 @@ public class my_app_1 {
             equation.execute();
             System.out.println("result = " + equation.getResult());
         }
+    }
+
+    private static MathEquation create(double leftVal, double rightVal, char opCode) {
+        MathEquation equation = new MathEquation();
+        equation.setLeftVal(leftVal);
+        equation.setRightVal(rightVal);
+        equation.setOpCode(opCode);
+        return equation;
+    }
+
+    static void executeInteractively() {
+        System.out.println("Enter an operation and two numbers");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        displayResult(opCode, leftVal, rightVal, result);
+    }
+
+    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
+        char symbol = symbolFromOpCode(opCode);
+        StringBuilder builder = new StringBuilder(20);
+        builder.append(leftVal);
+        builder.append(" ");
+        builder.append(symbol);
+        builder.append(" ");
+        builder.append(rightVal);
+        builder.append(" = ");
+        builder.append(result);
+        String output = builder.toString();
+        System.out.println(output);
+    }
+
+    private static char symbolFromOpCode(char opCode) {
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        char[] symbols = {'+', '-', '*', '/'};
+        char symbol = ' ';
+        for(int index = 0; index < opCodes.length; index++) {
+            if(opCode == opCodes[index]) {
+                symbol = symbols[index];
+                break;
+            }
+        }
+
+        return symbol;
+    }
+
+    private static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+    }
+
+    static double execute(char opCode, double leftVal, double rightVal) {
+        double result;
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+        }
+        return result;
+    }
+
+    static char opCodeFromString(String operationName) {
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word) {
+        String[] numberWords = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+        };
+        double value = 0d;
+        for(int index = 0; index < numberWords.length; index++) {
+            if(word.equals(numberWords[index])) {
+                value = index;
+                break;
+            }
+        }
+        return value;
     }
 
 
