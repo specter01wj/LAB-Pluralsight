ca08b38fb8b5886d9ff7c0b56768d9f152b81aea
Jin Wang <specter01wj@gmail.com>
2024-11-10 22:17:02 -0500

    v2

--- Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/08/mysol_1/my_app_1/src/com/jinwang/myApp/MathEquation.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Java SE Fundamentals/java-se-17-fundamentals/08/mysol_1/my_app_1/src/com/jinwang/myApp/MathEquation.java
@@ -1,95 +1,12 @@
 package com.jinwang.myApp;
-import java.util.*;
 
 public class MathEquation {
-    public static void main(String[] args) {
-        my_app_1 sol = new my_app_1();
-        if(args.length == 0) {
-            performCalculations();
-        } else if(args.length == 1 && args[0].equals("interactive")) {
-            executeInteractively();
-        } else if(args.length == 3) {
-            handleCommandLine(args);
-        } else {
-            System.out.println("Please provide an operation code and 2 numeric values");
-        }
-    }
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
 
-    static void performCalculations() {
-        MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
-
-        for(MathEquation equation : equations) {
-            equation.execute();
-            System.out.println("result = " + equation.getResult());
-        }
-    }
-
-    private static MathEquation create(double leftVal, double rightVal, char opCode) {
-        MathEquation equation = new MathEquation();
-        equation.setLeftVal(leftVal);
-        equation.setRightVal(rightVal);
-        equation.setOpCode(opCode);
-        return equation;
-    }
-
-    static void executeInteractively() {
-        System.out.println("Enter an operation and two numbers");
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        String[] parts = userInput.split(" ");
-        performOperation(parts);
-    }
-
-    private static void performOperation(String[] parts) {
-        char opCode = opCodeFromString(parts[0]);
-        double leftVal = valueFromWord(parts[1]);
-        double rightVal = valueFromWord(parts[2]);
-        double result = execute(opCode, leftVal, rightVal);
-        displayResult(opCode, leftVal, rightVal, result);
-    }
-
-    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
-        char symbol = symbolFromOpCode(opCode);
-        StringBuilder builder = new StringBuilder(20);
-        builder.append(leftVal);
-        builder.append(" ");
-        builder.append(symbol);
-        builder.append(" ");
-        builder.append(rightVal);
-        builder.append(" = ");
-        builder.append(result);
-        String output = builder.toString();
-        System.out.println(output);
-    }
-
-    private static char symbolFromOpCode(char opCode) {
-        char[] opCodes = {'a', 's', 'm', 'd'};
-        char[] symbols = {'+', '-', '*', '/'};
-        char symbol = ' ';
-        for(int index = 0; index < opCodes.length; index++) {
-            if(opCode == opCodes[index]) {
-                symbol = symbols[index];
-                break;
-            }
-        }
-
-        return symbol;
-    }
-
-    private static void handleCommandLine(String[] args) {
-        char opCode = args[0].charAt(0);
-        double leftVal = Double.parseDouble(args[1]);
-        double rightVal = Double.parseDouble(args[2]);
-        double result = execute(opCode, leftVal, rightVal);
-        System.out.println(result);
-    }
-
-    static double execute(char opCode, double leftVal, double rightVal) {
-        double result;
+    void execute() {
         switch (opCode) {
             case 'a':
                 result = leftVal + rightVal;
@@ -108,27 +25,39 @@ public class MathEquation {
                 result = 0.0d;
                 break;
         }
-        return result;
+
     }
 
-    static char opCodeFromString(String operationName) {
-        char opCode = operationName.charAt(0);
+    public double getLeftVal() {
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode() {
         return opCode;
     }
 
-    static double valueFromWord(String word) {
-        String[] numberWords = {
-                "zero", "one", "two", "three", "four",
-                "five", "six", "seven", "eight", "nine"
-        };
-        double value = 0d;
-        for(int index = 0; index < numberWords.length; index++) {
-            if(word.equals(numberWords[index])) {
-                value = index;
-                break;
-            }
-        }
-        return value;
+    public void setOpCode(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
     }
 
 }
