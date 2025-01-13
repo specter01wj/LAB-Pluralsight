95642e1e3c73827720921323cac06def2e9e3447
Jin Wang <specter01wj@gmail.com>
2025-01-13 01:52:59 -0500

    v1

---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/07/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
@@ -0,0 +1,14 @@
+package com.jinwang.myapp_1;
+import java.util.*;
+
+public class MyApp_1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Alarm alarm = new Alarm("Temperature too high");
+        alarm.turnOn();
+        alarm.snooze();
+        Thread.sleep(600 * 6);
+        alarm.sendReport();
+    }
+    
+}
