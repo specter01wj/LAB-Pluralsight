d2a991b4a951d92e00400720f5d061becb2ba404
Jin Wang <specter01wj@gmail.com>
2025-01-13 02:15:15 -0500

    v3

---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/07/mysol_1/myapp_1/src/com/jinwang/myapp_1/Alarm.java
@@ -0,0 +1,72 @@
+package com.jinwang.myapp_1;
+
+import java.time.LocalDateTime;
+
+public class Alarm {
+
+    protected boolean active;
+    private final String message;
+    private LocalDateTime snoozeUntil;
+
+    public Alarm(String message) {
+        this.message = message;
+        stopSnoozing();
+    }
+
+    public void snooze() {
+        if (active)
+            snoozeUntil = LocalDateTime.now().plusMinutes(5);
+    }
+
+    public LocalDateTime getSnoozeUntil() {
+        return snoozeUntil;
+    }
+
+    public boolean isSnoozing() {
+        return snoozeUntil.isAfter(LocalDateTime.now());
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void turnOn() {
+        active = true;
+        stopSnoozing();
+    }
+
+    public void turnOff() {
+        active = false;
+        stopSnoozing();
+    }
+
+    public String getReport() {
+        return getReport(false);
+    }
+
+    public String getReport(boolean uppercase) {
+        if (active && !isSnoozing()) {
+            if (uppercase)
+                return message.toUpperCase();
+            else
+                return message;
+        } else
+            return "";
+    }
+
+    public void sendReport() {
+        System.out.println(getReport(true));
+    }
+
+    private void stopSnoozing() {
+        snoozeUntil = LocalDateTime.now().minusSeconds(1);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Alarm alarm = new Alarm("Temperature too high");
+        alarm.turnOn();
+        alarm.snooze();
+        alarm.active = false;
+    }
+
+}
--- Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/07/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/07/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
@@ -1,14 +1,28 @@
 package com.jinwang.myapp_1;
 import java.util.*;
 
-public class MyApp_1 {
+public class MyApp_1 extends Alarm {
+
+    // PrioritizedAlarm
+    private final int priority;
+
+    MyApp_1(String message, int priority) {
+        super(message);
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "MyApp_1 [Message=" + getMessage() + ", Active=" + active + ", Priority=" + getPriority() + "]";
+    }
 
     public static void main(String[] args) throws InterruptedException {
-        Alarm alarm = new Alarm("Temperature too high");
-        alarm.turnOn();
-        alarm.snooze();
-        Thread.sleep(600 * 6);
-        alarm.sendReport();
+        // PrioritizedAlarm
+        System.out.println(new MyApp_1("Temperature too high", 1));
     }
 
 }
