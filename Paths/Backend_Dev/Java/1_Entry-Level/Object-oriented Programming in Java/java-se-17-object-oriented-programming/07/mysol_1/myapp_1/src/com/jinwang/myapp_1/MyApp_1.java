e871f976abac05bfb3e74e9aea42cff0f41fe5f3
Jin Wang <specter01wj@gmail.com>
2025-01-13 12:17:45 -0500

    v2

--- Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/07/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/07/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class MyApp_1 extends Alarm {
 
     // PrioritizedAlarm
-    private final int priority;
+    /*private final int priority;
 
     MyApp_1(String message, int priority) {
         super(message);
@@ -18,11 +18,31 @@ public class MyApp_1 extends Alarm {
     @Override
     public String toString() {
         return "MyApp_1 [Message=" + getMessage() + ", Active=" + active + ", Priority=" + getPriority() + "]";
+    }*/
+
+    // HighVisibilityAlarm
+    public MyApp_1(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getReport(boolean uppercase) {
+        String report = super.getReport(uppercase);
+        if (report.isEmpty()) {
+            return report;
+        } else {
+            return report + "!";
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
         // PrioritizedAlarm
-        System.out.println(new MyApp_1("Temperature too high", 1));
+        // System.out.println(new MyApp_1("Temperature too high", 1));
+
+        // HighVisibilityAlarm
+        MyApp_1 alarm = new MyApp_1("Test message");
+        alarm.turnOn();
+        alarm.sendReport();
     }
 
 }
