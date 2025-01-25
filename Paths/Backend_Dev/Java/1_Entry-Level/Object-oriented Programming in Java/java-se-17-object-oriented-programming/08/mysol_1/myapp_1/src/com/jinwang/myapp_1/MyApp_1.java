fff38f7b80465e949663e046f2c652b8ff9d8c2e
Jin Wang <specter01wj@gmail.com>
2025-01-25 12:54:55 -0500

    v6

--- Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/08/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/08/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
@@ -1,4 +1,39 @@
 package com.jinwang.myapp_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyApp_1 {
+
+
+    private final List<Alarm> allAlarms = new ArrayList<>();
+
+    public void add(Alarm alarm) {
+        alarm.turnOn();
+        allAlarms.add(alarm);
+    }
+
+    public void printBigReport() {
+        for (Alarm alarm: allAlarms)
+            System.out.println(alarm.getReport(true));
+    }
+
+    public static void main(String[] args) {
+
+        // HighVisibilityAlarm
+        HighVisibilityAlarm alarm = new HighVisibilityAlarm("Pressure low");
+        alarm.turnOn();
+        String report = alarm.getReport();
+        System.out.println("HighVisibilityAlarm report: " + report);
+
+        /*MyApp_1 dashboard = new MyApp_1();
+
+        dashboard.add(new PrioritizedAlarm("Temperature too high", 42));
+        dashboard.add(new HighVisibilityAlarm("Pressure too low"));
+        dashboard.add(new TimeSensitiveAlarm("Never mind the other alarms, you're late for dinner"));
+
+        dashboard.printBigReport();*/
+
+    }
+
 }
