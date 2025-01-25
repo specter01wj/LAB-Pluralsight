79107e40625c90683cdee5edc58b36b069e842dc
Jin Wang <specter01wj@gmail.com>
2025-01-25 12:20:05 -0500

    v3

---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/08/mysol_1/myapp_1/src/com/jinwang/myapp_1/HighVisibilityAlarm.java
@@ -0,0 +1,19 @@
+package com.jinwang.myapp_1;
+
+public class HighVisibilityAlarm extends Alarm {
+
+    public HighVisibilityAlarm(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getReport(boolean uppercase) {
+        String report = super.getReport(uppercase);
+        if (report.isEmpty()) {
+            return report;
+        } else {
+            return report + "!";
+        }
+    }
+
+}
