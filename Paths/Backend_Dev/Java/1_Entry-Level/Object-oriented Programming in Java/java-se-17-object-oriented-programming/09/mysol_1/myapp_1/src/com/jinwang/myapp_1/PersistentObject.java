d0ffe07ecce459d818d9d9ab6b42224863af51c0
Jin Wang <specter01wj@gmail.com>
2025-01-26 00:45:17 -0500

    v2

---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/09/mysol_1/myapp_1/src/com/jinwang/myapp_1/PersistentObject.java
@@ -0,0 +1,7 @@
+package com.jinwang.myapp_1;
+
+public interface PersistentObject {
+
+    void save();
+
+}
---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/09/mysol_1/myapp_1/src/com/jinwang/myapp_1/Widget.java
@@ -0,0 +1,7 @@
+package com.jinwang.myapp_1;
+
+public interface Widget {
+
+    String getHelpText();
+
+}
