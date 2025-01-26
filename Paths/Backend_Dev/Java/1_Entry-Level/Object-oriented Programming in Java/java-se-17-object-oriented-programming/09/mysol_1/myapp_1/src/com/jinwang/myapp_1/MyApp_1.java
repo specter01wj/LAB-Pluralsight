e0a8e8080ffb19695eaf94d0d510e3cc9bfab2a4
Jin Wang <specter01wj@gmail.com>
2025-01-26 00:50:22 -0500

    v3

--- Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/09/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/09/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
@@ -1,4 +1,25 @@
 package com.jinwang.myapp_1;
 
 public class MyApp_1 {
+
+    private static void activate(Alarm alarm) {
+        alarm.turnOn();
+    }
+
+    private static void printHelpText(Widget widget) {
+        System.out.println(widget.getHelpText());
+    }
+
+    private static void saveItTwice(PersistentObject persistentObject) {
+        persistentObject.save();
+        persistentObject.save();
+    }
+
+    public static void main(String[] args) {
+        HighVisibilityAlarm alarm = new HighVisibilityAlarm("Hello there!");
+        activate(alarm);
+        printHelpText(alarm);
+        saveItTwice(alarm);
+    }
+
 }
---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/09/mysol_1/myapp_1/src/com/jinwang/myapp_1/PrioritizedAlarm.java
@@ -0,0 +1,23 @@
+package com.jinwang.myapp_1;
+
+import java.awt.*;
+
+public class PrioritizedAlarm extends Alarm {
+
+    private final int priority;
+
+    public PrioritizedAlarm(String message, int priority) {
+        super(message);
+        this.priority = priority;
+    }
+
+    @Override
+    public Color getColor() {
+        return Color.GREEN;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+}
