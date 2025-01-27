1eb22bb35d535206a5dccf164c16a97818f9b2cb
Jin Wang <specter01wj@gmail.com>
2025-01-27 01:34:37 -0500

    v4

---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/11/mysol_1/myapp_1/src/com/jinwang/myapp_1/Example.java
@@ -0,0 +1,9 @@
+package com.jinwang.myapp_1;
+
+public class Example {
+
+    public Example() {
+        System.out.println("Example constructor");
+    }
+
+}
--- Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/11/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Object-oriented Programming in Java/java-se-17-object-oriented-programming/11/mysol_1/myapp_1/src/com/jinwang/myapp_1/MyApp_1.java
@@ -1,4 +1,9 @@
 package com.jinwang.myapp_1;
 
 public class MyApp_1 {
+
+    public static void main(String[] args) {
+        new Example();
+    }
+
 }
