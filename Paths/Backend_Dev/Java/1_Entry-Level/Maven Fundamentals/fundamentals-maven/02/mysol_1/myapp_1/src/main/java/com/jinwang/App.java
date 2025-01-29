f63e78b4d4c3c889c21552d55a88895a6d4a3c0d
Jin Wang <specter01wj@gmail.com>
2025-01-29 00:02:56 -0500

    v1

--- Paths/Backend_Dev/Java/1_Entry-Level/Maven Fundamentals/fundamentals-maven/02/mysol_1/myapp_1/src/main/java/com/jinwang/App.java
+++ Paths/Backend_Dev/Java/1_Entry-Level/Maven Fundamentals/fundamentals-maven/02/mysol_1/myapp_1/src/main/java/com/jinwang/App.java
@@ -1,5 +1,7 @@
 package com.jinwang;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        String str = "   Hello Apache Commons   ";
+        System.out.println(StringUtils.trim(str));
     }
 }
