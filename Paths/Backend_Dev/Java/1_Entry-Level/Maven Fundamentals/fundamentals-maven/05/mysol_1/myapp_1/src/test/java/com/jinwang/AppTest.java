4a6a2e07295a67d4010e9baa5fdb83bb07228fca
Jin Wang <specter01wj@gmail.com>
2025-02-02 23:23:34 -0500

    v1

---
+++ Paths/Backend_Dev/Java/1_Entry-Level/Maven Fundamentals/fundamentals-maven/05/mysol_1/myapp_1/src/test/java/com/jinwang/AppTest.java
@@ -0,0 +1,18 @@
+package com.jinwang;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+  /**
+   * Basic test
+   */
+  @Test
+  void testApp() {
+      assertTrue(true);
+  }
+}
