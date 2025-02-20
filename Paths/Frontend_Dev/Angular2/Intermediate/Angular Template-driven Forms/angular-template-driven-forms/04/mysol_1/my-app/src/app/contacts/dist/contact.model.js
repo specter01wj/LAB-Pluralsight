5eb2b0c28d2bd4ac90a85d73d08c6a405a6b5505
Jin Wang <specter01wj@gmail.com>
2025-02-20 02:20:37 -0500

    v3

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Template-driven Forms/angular-template-driven-forms/04/mysol_1/my-app/src/app/contacts/dist/contact.model.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.phoneTypeValues = void 0;
+exports.phoneTypeValues = [
+    { title: 'Mobile', value: 'mobile' },
+    { title: 'Work', value: 'work' },
+    { title: 'Other', value: 'other' },
+];
