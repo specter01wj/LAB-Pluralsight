38c933b5a1313bfdc508cb516c4e4ff5e5445378
Jin Wang <specter01wj@gmail.com>
2025-02-20 02:25:22 -0500

    v5

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Template-driven Forms/angular-template-driven-forms/04/mysol_1/my-app/src/app/edit-contact/edit-contact.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Template-driven Forms/angular-template-driven-forms/04/mysol_1/my-app/src/app/edit-contact/edit-contact.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Contact } from '../contacts/contact.model';
+import { Contact, phoneTypeValues } from '../contacts/contact.model';
 import { ContactsService } from '../contacts/contacts.service';
 import { NgForm } from '@angular/forms';
 
@@ -11,6 +11,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
+  phoneTypes = phoneTypeValues;
+
   contact: Contact = {
     id: '',
     firstName: '',
