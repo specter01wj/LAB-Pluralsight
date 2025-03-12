48f6adeec984d9ec2f31563116234bb946f74849
Jin Wang <specter01wj@gmail.com>
2025-03-12 19:47:18 -0400

    v1

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Template-driven Forms/angular-template-driven-forms/07/mysol_1/my-app/src/app/profile-icon-selector/dist/profile-icon-selector.component.js
@@ -0,0 +1,51 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.ProfileIconSelectorComponent = void 0;
+var core_1 = require("@angular/core");
+var profile_icon_names_1 = require("./profile-icon-names");
+var forms_1 = require("@angular/forms");
+var PROFILE_ICON_VALUE_ACCESSOR = {
+    provide: forms_1.NG_VALUE_ACCESSOR,
+    useExisting: core_1.forwardRef(function () { return ProfileIconSelectorComponent; }),
+    multi: true
+};
+var ProfileIconSelectorComponent = /** @class */ (function () {
+    function ProfileIconSelectorComponent() {
+        this.profileIcons = profile_icon_names_1.profileIconNames;
+        this.showAllIcons = true;
+    }
+    ProfileIconSelectorComponent.prototype.iconSelected = function (icon) {
+        this.showAllIcons = false;
+        this.selectedIcon = icon;
+        this.onChange(icon);
+    };
+    ProfileIconSelectorComponent.prototype.writeValue = function (icon) {
+        this.selectedIcon = icon;
+        if (icon && icon !== '')
+            this.showAllIcons = false;
+        else
+            this.showAllIcons = true;
+    };
+    ProfileIconSelectorComponent.prototype.registerOnChange = function (fn) {
+        this.onChange = function (icon) { fn(icon); };
+    };
+    ProfileIconSelectorComponent.prototype.registerOnTouched = function (fn) {
+        this.onTouched = fn;
+    };
+    ProfileIconSelectorComponent = __decorate([
+        core_1.Component({
+            selector: 'app-profile-icon-selector',
+            templateUrl: './profile-icon-selector.component.html',
+            styleUrl: './profile-icon-selector.component.scss',
+            providers: [PROFILE_ICON_VALUE_ACCESSOR]
+        })
+    ], ProfileIconSelectorComponent);
+    return ProfileIconSelectorComponent;
+}());
+exports.ProfileIconSelectorComponent = ProfileIconSelectorComponent;
