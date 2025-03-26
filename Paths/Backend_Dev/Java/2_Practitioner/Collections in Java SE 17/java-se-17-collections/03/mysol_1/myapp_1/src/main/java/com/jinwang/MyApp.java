bbe57974ab1e92f8103e3f5d12274491b2160b18
Jin Wang <specter01wj@gmail.com>
2025-03-26 03:03:53 -0400

    v1

---
+++ Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 17/java-se-17-collections/03/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -0,0 +1,21 @@
+package com.jinwang;
+
+import com.common.Product;
+
+import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
