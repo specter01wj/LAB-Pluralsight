a6993977175f2f0efded597f709c213f5475c719
Jin Wang <specter01wj@gmail.com>
2025-04-17 19:07:52 -0400

    v6

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-reactive-forms/04/mysol_1/my-contacts-app/src/app/contacts/dist/contact.model.js
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-reactive-forms/04/mysol_1/my-contacts-app/src/app/contacts/dist/contact.model.js
@@ -1,8 +1,13 @@
 "use strict";
 exports.__esModule = true;
-exports.phoneTypeValues = void 0;
+exports.addressTypeValues = exports.phoneTypeValues = void 0;
 exports.phoneTypeValues = [
     { title: 'Mobile', value: 'mobile' },
+    { title: 'Work', value: 'work' },
+    { title: 'Other', value: 'other' },
+];
+exports.addressTypeValues = [
+    { title: 'Home', value: 'home' },
     { title: 'Work', value: 'work' },
     { title: 'Other', value: 'other' },
 ];
