2d9cbc440a901a16b6334ac9d4c19714423d58b3
Jin Wang <specter01wj@gmail.com>
2025-04-23 01:53:48 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-reactive-forms/06/mysol_1/my-contacts-app/src/app/date-value-accessor/date-value-accessor.directive.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-reactive-forms/06/mysol_1/my-contacts-app/src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -1,7 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, forwardRef, Provider } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const DATE_VALUE_PROVIDER: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => DateValueAccessorDirective),
+  multi: true,
+};
 
 @Directive({
-  selector: '[appDateValueAccessor]',
+  selector: 'input([type=date])[formControlName],input([type=date])[formControl],input([type=date])[ngModel]',
+  providers: [DATE_VALUE_PROVIDER],
   standalone: false
 })
 export class DateValueAccessorDirective {
