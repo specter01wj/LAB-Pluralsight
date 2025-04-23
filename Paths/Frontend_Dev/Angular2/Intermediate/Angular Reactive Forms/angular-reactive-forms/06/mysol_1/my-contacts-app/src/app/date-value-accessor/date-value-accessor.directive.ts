591ef60ea1c22dfc6263088656d421101c6f6bd5
Jin Wang <specter01wj@gmail.com>
2025-04-23 01:57:05 -0400

    v5

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-reactive-forms/06/mysol_1/my-contacts-app/src/app/date-value-accessor/date-value-accessor.directive.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Reactive Forms/angular-reactive-forms/06/mysol_1/my-contacts-app/src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -1,5 +1,5 @@
-import { Directive, forwardRef, Provider } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Directive, ElementRef, forwardRef, HostListener, Provider } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 const DATE_VALUE_PROVIDER: Provider = {
   provide: NG_VALUE_ACCESSOR,
@@ -12,8 +12,28 @@ const DATE_VALUE_PROVIDER: Provider = {
   providers: [DATE_VALUE_PROVIDER],
   standalone: false
 })
-export class DateValueAccessorDirective {
+export class DateValueAccessorDirective implements ControlValueAccessor {
 
-  constructor() { }
+  constructor(private element: ElementRef) { }
+
+  @HostListener('input', ['$event.target.valueAsDate'])
+  private onChange!: Function;
+
+  @HostListener('blur')
+  private onTouched!: Function;
+
+  registerOnChange(fn: Function) {
+    this.onChange = (valueAsDate: Date) => { fn(valueAsDate); };
+  }
+
+  registerOnTouched(fn: Function) {
+    this.onTouched = fn;
+  }
+
+  writeValue(newValue: any) {
+    if (newValue instanceof Date) {
+      this.element.nativeElement.value = newValue.toISOString().split('T')[0];
+    }
+  }
 
 }
