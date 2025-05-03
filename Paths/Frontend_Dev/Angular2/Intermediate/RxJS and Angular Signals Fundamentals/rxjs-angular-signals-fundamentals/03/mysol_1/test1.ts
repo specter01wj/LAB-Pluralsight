aa12a19369b5309eafd3925168ce47fee49472e6
Jin Wang <specter01wj@gmail.com>
2025-05-03 01:26:09 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/03/mysol_1/test1.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/03/mysol_1/test1.ts
@@ -1,9 +1,14 @@
 import { of, from } from 'rxjs';
 
 let a1 = of(2, 4, 6, 8);
+let b1 = from([3, 5, 7, 9]);
 
 a1.subscribe(val => {
   console.log('Value from of:', val);
+});
+
+b1.subscribe(val => {
+  console.log('Value from from:', val);
 });
 
 
