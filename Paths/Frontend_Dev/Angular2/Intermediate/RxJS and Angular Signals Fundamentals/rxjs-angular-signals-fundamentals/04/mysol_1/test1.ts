b1f4071715b068caaf9415ba2237d52cb7205381
Jin Wang <specter01wj@gmail.com>
2025-05-05 02:54:10 -0400

    v2

--- Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/04/mysol_1/test1.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/04/mysol_1/test1.ts
@@ -1,12 +1,15 @@
 import { of, from, Subscription, map } from 'rxjs';
 
 let subApples!: Subscription;
+let subNumbers!: Subscription;
 
 const apples$ = from([
   {id: 1, type: 'macintosh'},
   {id: 2, type: 'gala'},
   {id: 3, type: 'fuji'},
 ]);
+
+const numbers$ = of(2, 4, 6);
 
 subApples = apples$.pipe(
   map(a => ({
@@ -16,4 +19,8 @@ subApples = apples$.pipe(
   console.log('Apple:', val);
 });
 
-
+subNumbers = numbers$.pipe(
+  map(item => item * 2)
+).subscribe(val => {
+  return console.log('Map x2:', val)
+});
