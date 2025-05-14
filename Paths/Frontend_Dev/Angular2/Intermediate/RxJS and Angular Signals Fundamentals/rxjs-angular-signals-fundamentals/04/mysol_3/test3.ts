b27ca463c6403698e7f87d9c0bcbf46c7149baa6
Jin Wang <specter01wj@gmail.com>
2025-05-14 02:34:44 -0400

    v3

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/04/mysol_3/test3.ts
@@ -0,0 +1,24 @@
+import { of, from, Subscription, map, tap } from 'rxjs';
+
+let subApples!: Subscription;
+let subNumbers!: Subscription;
+
+const apples$ = from([
+  {id: 1, type: 'macintosh'},
+  {id: 2, type: 'gala'},
+  {id: 3, type: 'fuji'},
+]);
+
+const numbers$ = of(2, 4, 6);
+
+subApples = apples$.pipe(
+  map(a => ({
+    ...a, color: 'red'
+  })),
+  tap(a => console.log('Apple:', a))
+).subscribe();
+
+subNumbers = numbers$.pipe(
+  map(item => item * 2),
+  tap(item => console.log('Map x2:', item))
+).subscribe(val => {});
