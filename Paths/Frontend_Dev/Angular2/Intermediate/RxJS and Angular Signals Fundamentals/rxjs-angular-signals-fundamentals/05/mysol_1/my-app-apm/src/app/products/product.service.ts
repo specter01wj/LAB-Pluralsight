69e44e9a8ff82a00c5a13d76184a72ffa45b1b74
Jin Wang <specter01wj@gmail.com>
2025-05-30 19:27:13 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/05/mysol_1/my-app-apm/src/app/products/product.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/05/mysol_1/my-app-apm/src/app/products/product.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,12 @@ export class ProductService {
   private productsUrl = 'api/products';
 
   private http = inject(HttpClient);
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl)
+      .pipe(
+        tap(() => console.log('In http.get pipeline'))
+      );
+  }
 
 }
