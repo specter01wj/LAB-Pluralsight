0e1a3cb8c5cf33395d356aa35147239385316430
Jin Wang <specter01wj@gmail.com>
2025-06-03 18:39:57 -0400

    v4

--- Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/06/mysol_1/dist/test1.js
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/06/mysol_1/dist/test1.js
@@ -1,13 +1,13 @@
 import { of, map } from 'rxjs';
-var subData;
-var data$ = of(2, 4, 6);
-subData = data$.pipe(map(function (i) {
+let subData;
+const data$ = of(2, 4, 6);
+subData = data$.pipe(map(i => {
     if (i === 4) {
         throw 'Error!';
     }
     return i;
 })).subscribe({
-    next: function (x) { return console.log(x); },
-    error: function (err) { return console.error(err); },
-    complete: function () { return console.log('complete'); }
+    next: x => console.log(x),
+    error: err => console.error(err),
+    complete: () => console.log('complete')
 });
