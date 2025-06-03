38e6e716a6bc14f5c2d1fad7e73846da9b705514
Jin Wang <specter01wj@gmail.com>
2025-06-03 18:15:01 -0400

    v1

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/06/mysol_1/my-app-apm/src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppData } from './app-data';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    importProvidersFrom(
+      FormsModule,
+      InMemoryWebApiModule.forRoot(AppData, { delay: 1000 })
+    ),
+    provideRouter(routes)
+  ]
+};
---
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/06/mysol_1/my-app-apm/src/app/products/dist/product.service.js
@@ -0,0 +1,34 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.ProductService = void 0;
+var http_1 = require("@angular/common/http");
+var core_1 = require("@angular/core");
+var rxjs_1 = require("rxjs");
+var ProductService = /** @class */ (function () {
+    function ProductService() {
+        this.productsUrl = 'api/products';
+        this.http = core_1.inject(http_1.HttpClient);
+    }
+    ProductService.prototype.getProducts = function () {
+        return this.http.get(this.productsUrl)
+            .pipe(rxjs_1.tap(function () { return console.log('In http.get pipeline'); }));
+    };
+    ProductService.prototype.getProduct = function (id) {
+        var productUrl = this.productsUrl + '/' + id;
+        return this.http.get(productUrl)
+            .pipe(rxjs_1.tap(function () { return console.log('In http.get by id pipeline'); }));
+    };
+    ProductService = __decorate([
+        core_1.Injectable({
+            providedIn: 'root'
+        })
+    ], ProductService);
+    return ProductService;
+}());
+exports.ProductService = ProductService;
