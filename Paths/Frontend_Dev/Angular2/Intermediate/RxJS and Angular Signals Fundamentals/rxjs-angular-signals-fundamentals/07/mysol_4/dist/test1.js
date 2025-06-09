0060d62e9f21ec413bc6e15ab28a4e3a3b216e45
Jin Wang <specter01wj@gmail.com>
2025-06-09 02:41:28 -0400

    v8

--- Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/07/mysol_4/dist/test1.js
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/07/mysol_4/dist/test1.js
@@ -1,8 +1,8 @@
 import { of, range, delay, switchMap } from 'rxjs';
 range(21, 5)
-    .pipe(switchMap(function (i) { return of(i)
-    .pipe(delay(randomDelay())); })).subscribe({
-    next: function (x) { return console.log('switchMap: ', x); }
+    .pipe(switchMap(i => of(i)
+    .pipe(delay(randomDelay())))).subscribe({
+    next: x => console.log('switchMap: ', x),
 });
 function randomDelay() {
     return Math.floor(Math.random() * 1000) + 500;
