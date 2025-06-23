6b3db1de0a96fc03cf0d6dad29e49f437fa14aff
Jin Wang <specter01wj@gmail.com>
2025-06-23 03:08:56 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/12/mysol_1/toSignal_app/src/app/app.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/12/mysol_1/toSignal_app/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'toSignal_app';
+  o$ = of(1, 2, 3, 4);
+  s: any;
+
+  constructor() {
+    this.s = toSignal(this.o$, { initialValue: 0 });
+
+    effect(() => {
+      console.log('Signal value:', this.s());
+    });
+  }
+
 }
---
+++ Paths/Frontend_Dev/Angular2/Intermediate/RxJS and Angular Signals Fundamentals/rxjs-angular-signals-fundamentals/12/mysol_1/toSignal_app/src/app/dist/app.component.js
@@ -0,0 +1,35 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppComponent = void 0;
+var core_1 = require("@angular/core");
+var rxjs_interop_1 = require("@angular/core/rxjs-interop");
+var router_1 = require("@angular/router");
+var rxjs_1 = require("rxjs");
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        var _this = this;
+        this.title = 'toSignal_app';
+        this.o$ = rxjs_1.of(1, 2, 3, 4);
+        this.s = rxjs_interop_1.toSignal(this.o$, { initialValue: 0 });
+        core_1.effect(function () {
+            console.log('Signal value:', _this.s());
+        });
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'app-root',
+            standalone: true,
+            imports: [router_1.RouterOutlet],
+            templateUrl: './app.component.html',
+            styleUrl: './app.component.scss'
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
