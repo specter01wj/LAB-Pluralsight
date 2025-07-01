8e31b77be5e6bc9fdb8dc7f72bbf5dccd058b6aa
Jin Wang <specter01wj@gmail.com>
2025-07-01 02:36:22 -0400

    v1

---
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/services-angular/03/mysol_1/my-robot-shop/src/app/catalog/search/dist/search.component.js
@@ -0,0 +1,42 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.SearchComponent = void 0;
+var core_1 = require("@angular/core");
+var SearchComponent = /** @class */ (function () {
+    function SearchComponent(productsService, cartService) {
+        this.productsService = productsService;
+        this.cartService = cartService;
+        this.products = [];
+        this.searchTerm = '';
+    }
+    SearchComponent.prototype.ngOnInit = function () {
+        this.products = this.productsService.getProducts();
+    };
+    SearchComponent.prototype.addToCart = function (product) {
+        this.cartService.add(product);
+    };
+    SearchComponent.prototype.filter = function (event) {
+        this.searchTerm = event.target.value.toLowerCase();
+    };
+    SearchComponent.prototype.getFilteredProducts = function () {
+        var _this = this;
+        return this.searchTerm === ''
+            ? this.products
+            : this.products.filter(function (product) { return product.name.toLowerCase().includes(_this.searchTerm); });
+    };
+    SearchComponent = __decorate([
+        core_1.Component({
+            selector: 'bot-search',
+            templateUrl: './search.component.html',
+            styleUrls: ['./search.component.css']
+        })
+    ], SearchComponent);
+    return SearchComponent;
+}());
+exports.SearchComponent = SearchComponent;
