41327a7f6c372510d84bb5ff09486481f860b718
Jin Wang <specter01wj@gmail.com>
2025-07-31 00:41:10 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/services-angular/05/mysol_1/my-robot-shop/src/app/catalog/products.service.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular Services/services-angular/05/mysol_1/my-robot-shop/src/app/catalog/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from "@shared/product.model";
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
