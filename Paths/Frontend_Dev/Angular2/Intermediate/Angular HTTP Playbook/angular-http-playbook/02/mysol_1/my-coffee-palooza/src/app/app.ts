a1fe941b34c484b053263135538cf1e5729a6a5e
Jin Wang <specter01wj@gmail.com>
2025-08-19 02:32:52 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Playbook/angular-http-playbook/02/mysol_1/my-coffee-palooza/src/app/app.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Playbook/angular-http-playbook/02/mysol_1/my-coffee-palooza/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  standalone: false,
   styleUrl: './app.scss'
 })
 export class App {
