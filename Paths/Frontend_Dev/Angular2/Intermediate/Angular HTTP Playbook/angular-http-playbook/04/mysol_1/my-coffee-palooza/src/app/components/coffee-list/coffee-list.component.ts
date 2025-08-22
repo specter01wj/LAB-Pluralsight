5ec88c088416a16cb921107d3cf74cdbbf02cc65
Jin Wang <specter01wj@gmail.com>
2025-08-22 02:46:47 -0400

    v3

--- Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Playbook/angular-http-playbook/04/mysol_1/my-coffee-palooza/src/app/components/coffee-list/coffee-list.component.ts
+++ Paths/Frontend_Dev/Angular2/Intermediate/Angular HTTP Playbook/angular-http-playbook/04/mysol_1/my-coffee-palooza/src/app/components/coffee-list/coffee-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CoffeeApiService } from '../../services/coffee-api.service';
 import { Coffee } from '../../types/coffee';
-import { Observable, map, of, tap } from 'rxjs';
+import { Observable, Subject, debounceTime, delay, distinctUntilChanged, map, of, switchMap, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -17,11 +17,19 @@ import { FormsModule } from '@angular/forms';
 export class CoffeeListComponent implements OnInit {
   coffees$: Observable<Coffee[]> = of([]);
   coffeeToAdd: Partial<Coffee> = {};
+  searchCoffee$: Observable<Coffee | undefined> = of(undefined);
+  private coffeeSearchId$ = new Subject<number>();
+
 
   constructor(public restApi: CoffeeApiService) {}
 
   ngOnInit() {
     this.coffees$ = this.restApi.getCoffees();
+    this.searchCoffee$ = this.coffeeSearchId$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(coffeeId => this.restApi.getCoffee(coffeeId))
+    );
   }
 
   deleteCoffee(id: number) {
@@ -40,6 +48,45 @@ export class CoffeeListComponent implements OnInit {
         tap(() => this.coffeeToAdd = {})
       ) 
     });
+  }
+
+  requestCoffeeBlob(imageUrl: string) {
+    this.restApi.getCoffeeImageBlob(imageUrl).subscribe(imageBlob => {
+      console.log('Image blob: ', imageBlob);
+    });
+  }
+
+  requestCoffeeBuffer(imageUrl: string) {
+    this.restApi.getCoffeeImageBuffer(imageUrl).subscribe(buffer => {
+      console.log('Image buffer: ', buffer);
+    });
+  }
+
+  requestCoffeeAsText(id: number) {
+    this.restApi.getCoffeeAsText(id).subscribe(coffeeText => {
+      console.log('Coffee text: ', coffeeText);
+    });
+  }
+
+  requestCoffeeHostIP() {
+    this.restApi.getCoffeeHostIPAddress();
+  }
+
+  cancelRequestForCoffees() {
+    this.restApi.cancel();
+  }
+
+  searchForCoffeeById(event: Event) {
+    const input = (event as InputEvent).target as HTMLInputElement;
+    this.coffeeSearchId$.next(input.value ? parseInt(input.value) : -1);
+  }
+
+  getCoffeeChunks() {
+    this.coffees$ = this.restApi.getCoffeeChunks();
+  }
+
+  getCoffeeChunksSync() {
+    this.coffees$ = this.restApi.getCoffeeChunksSync();
   }
 
   editCoffee(toUpdate: Partial<Coffee>) {
