92f5378907858a5c1521541d9f273a4d468bc54f
Jin Wang <specter01wj@gmail.com>
2025-11-08 12:15:09 -0500

    v1

---
+++ Paths/Frontend_Dev/HTML:CSS/Tailwind CSS/Foundations & Components/Component-Based UI Development with Tailwind CSS/tailwind-css-component-based-ui-development/01/mysol_1/pieshop/dist/tailwind.config.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,css}'],
+  theme: {
+    extend: {
+      colors: {
+        'header': '#C87D53'
+      }
+    }
+  },
+  plugins: []
+};
--- Paths/Frontend_Dev/HTML:CSS/Tailwind CSS/Foundations & Components/Component-Based UI Development with Tailwind CSS/tailwind-css-component-based-ui-development/01/mysol_1/pieshop/tailwind.config.js
+++ Paths/Frontend_Dev/HTML:CSS/Tailwind CSS/Foundations & Components/Component-Based UI Development with Tailwind CSS/tailwind-css-component-based-ui-development/01/mysol_1/pieshop/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,css}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header': '#C87D53',
+        'page-background': '#FDF5F4',
+        'delete-button': '#9A9794',
+        'action-button': '#FECFC9'
+      },
+    },
   },
   plugins: [],
 }
