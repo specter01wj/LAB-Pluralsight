71e4a0681218dc7f2a8b92444d71d497d68257b8
Jin Wang <specter01wj@gmail.com>
2025-11-08 12:44:51 -0500

    v8

--- Paths/Frontend_Dev/HTML:CSS/Tailwind CSS/Foundations & Components/Component-Based UI Development with Tailwind CSS/tailwind-css-component-based-ui-development/01/mysol_1/pluralsight/tailwind.config.js
+++ Paths/Frontend_Dev/HTML:CSS/Tailwind CSS/Foundations & Components/Component-Based UI Development with Tailwind CSS/tailwind-css-component-based-ui-development/01/mysol_1/pluralsight/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: [['./src/**/*.{html,css}']],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ps-purple-light': '#A5AAD0',
+        'ps-purple-dark': '#140F25',
+        'ps-bubble': '#2F3059',
+        'ps-yellow-highlight': '#FFCF3E',
+        'ps-nav-selected': '#404377',
+        'ps-nav-selected-border': '#FE1676',
+        'ps-nav-purple': '#232145'
+      }
+    },
+    fontFamily: {
+      'sans': ['PS Commons', 'Arial', 'sans-serif'],
+      'serif': ['Garamond', 'serif']
+    }
   },
   plugins: [],
 }
