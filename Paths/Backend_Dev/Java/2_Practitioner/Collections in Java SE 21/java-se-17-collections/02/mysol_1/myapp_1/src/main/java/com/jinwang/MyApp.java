5541c3917a2ea738a0c4fb517e6340ad511388e9
Jin Wang <specter01wj@gmail.com>
2025-11-16 23:06:22 -0500

    v2

--- Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/02/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/02/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -9,7 +9,7 @@ public class MyApp {
     public static void main(String[] args) {
 
         // TheArrayProblem
-        var door = new Product("Wooden Door", 35);
+        /*var door = new Product("Wooden Door", 35);
         var floorPanel = new Product("Floor Panel", 25);
         var window = new Product("Glass Window", 10);
 
@@ -26,9 +26,47 @@ public class MyApp {
 
         // Duplicate
         products = add(roof, products);
-        System.out.println(Arrays.toString(products));
+        System.out.println(Arrays.toString(products));*/
+
+        // CollectionConcepts
+        var door = new Product("Wooden Door", 35);
+        var floorPanel = new Product("Floor Panel", 25);
+        var window = new Product("Glass Window", 10);
+
+        Collection<Product> products = new ArrayList<>();
+        products.add(door);
+        products.add(floorPanel);
+        products.add(window);
+
+        /*System.out.println(products);
+
+        for (var product : products) {
+            System.out.println(product);
+        }
+
+        Iterator<Product> it = products.iterator();
+        while (it.hasNext()) {
+            var product = it.next();
+            if (product.weight() > 20) {
+                it.remove();
+            }
+        }
+
+        System.out.println(products);*/
+
+        /*System.out.println(products.size());
+        System.out.println(products.isEmpty());
+        System.out.println(products.contains(floorPanel));
+        products.remove(floorPanel);
+        System.out.println(products.contains(floorPanel));*/
 
 
+        var toRemove = new ArrayList<Product>();
+        toRemove.add(door);
+        toRemove.add(floorPanel);
+
+        products.removeAll(toRemove);
+        System.out.println(products);
     }
 
     private static Product[] add(Product product, Product[] array)
