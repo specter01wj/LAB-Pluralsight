bf1b328593028d596b6bec7afa5b426621643c07
Jin Wang <specter01wj@gmail.com>
2025-12-05 19:32:46 -0500

    v4

--- Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/04/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/04/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,16 +1,56 @@
 package com.jinwang;
 
-import com.common.Product;
+import com.common.*;
 
 import java.util.*;
 
 public class MyApp {
 
+    private static final int ITERATIONS = 5;
+    private static final int NUMBER_OF_PRODUCTS = 20_000;
+    private static final List<Product> products = generateProducts();
+
     public static void main(String[] args) {
-
-
-
+        runLookups(new MapProductLookupTable());
+        runLookups(new NaiveProductLookupTable());
     }
 
+    private static List<Product> generateProducts() {
+        final List<Product> products = new ArrayList<>();
+        final Random weightGenerator = new Random();
+
+        for (int i = 0; i < NUMBER_OF_PRODUCTS; i++) {
+            products.add(new Product(i, "Product" + i, 10 + weightGenerator.nextInt(10)));
+        }
+
+        Collections.shuffle(products);
+        Collections.shuffle(products);
+        Collections.shuffle(products);
+
+        return products;
+    }
+
+    private static void runLookups(final ProductLookupTable lookupTable) {
+        System.out.println("Running lookups with " + lookupTable.getClass().getSimpleName());
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            long startTime = System.currentTimeMillis();
+
+            for (Product product : products) {
+                lookupTable.addProduct(product);
+            }
+
+            for (Product product : products) {
+                Product result = lookupTable.lookupById(product.getId());
+                if (result != product) {
+                    throw new IllegalStateException("Lookup Table returned wrong result for " + product);
+                }
+            }
+
+            lookupTable.clear();
+
+            System.out.println((System.currentTimeMillis() - startTime) + "ms");
+        }
+    }
 
 }
