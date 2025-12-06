fe74ca36262ead0ded7d3090aaa92d43205fc8a8
Jin Wang <specter01wj@gmail.com>
2025-12-05 19:24:11 -0500

    v2

---
+++ Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/04/mysol_1/myapp_1/src/main/java/com/common/NaiveProductLookupTable.java
@@ -0,0 +1,43 @@
+package com.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaiveProductLookupTable implements ProductLookupTable {
+    private final List<Product> products = new ArrayList<>();
+
+    @Override
+    public void addProduct(final Product productToAdd)
+    {
+        var id = productToAdd.getId();
+        for (var product : products)
+        {
+            if (product.getId() == id)
+            {
+                throw new IllegalArgumentException(
+                        "Unable to add product, duplicate id for: " + productToAdd);
+            }
+        }
+
+        products.add(productToAdd);
+    }
+
+    @Override
+    public Product lookupById(final int id)
+    {
+        for (var product : products)
+        {
+            if (product.getId() == id)
+            {
+                return product;
+            }
+        }
+
+        return null;
+    }
+
+    public void clear()
+    {
+        products.clear();
+    }
+}
