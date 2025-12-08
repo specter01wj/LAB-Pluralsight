75c3b5c5293a5fc231729e195924e131769e0379
Jin Wang <specter01wj@gmail.com>
2025-12-07 23:03:03 -0500

    v6

--- Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/04/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/04/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -38,11 +38,6 @@ public class MyApp {
 
             for (Product product : products) {
                 lookupTable.addProduct(product);
-
-
-
-
-                
             }
 
             for (Product product : products) {
