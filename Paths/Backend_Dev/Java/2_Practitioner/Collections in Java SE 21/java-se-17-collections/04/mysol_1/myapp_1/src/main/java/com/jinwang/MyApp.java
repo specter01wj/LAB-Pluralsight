990c6de5e9e4efbe41ce476970fc2ee664bd09bb
Jin Wang <specter01wj@gmail.com>
2025-12-10 23:25:06 -0500

    v6

--- Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/04/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/04/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -24,11 +24,6 @@ public class MyApp {
         var ids = idToProduct.keySet();
         System.out.println(ids);
         System.out.println(idToProduct);
-
-
-
-
-        
         System.out.println();
     }
 
