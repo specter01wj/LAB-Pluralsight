b99ee72cbc8ca438c92f89c11b5c1160409d10b3
Jin Wang <specter01wj@gmail.com>
2025-12-20 22:00:15 -0500

    v3

--- Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/06/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Collections in Java SE 21/java-se-17-collections/06/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,43 @@
 package com.jinwang;
 
+import com.common.Product;
+import com.common.Shipment;
+
+import java.util.*;
+
 public class MyApp {
+
+    private static final Product door = new Product("Wooden Door", 35);
+    private static final Product floorPanel = new Product("Floor Panel", 25);
+    private static final Product window = new Product("Glass Window", 10);
+
+    public static void main(String[] args) {
+        // ShipmentsBreaker
+        Shipment shipment = new Shipment();
+
+        shipment.add(door);
+        shipment.add(window);
+        shipment.add(door);
+        shipment.add(window);
+        shipment.add(floorPanel);
+
+        shipment.prepare();
+
+        List<Product> lightVanProducts = shipment.getLightVanProducts();
+        System.out.println("Light Van Products (initial): " + lightVanProducts);
+
+        // This will throw UnsupportedOperationException
+        try {
+            lightVanProducts.remove(window);
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Attempted to remove from an unmodifiable list!");
+        }
+
+        System.out.println("Light Van Products (after attempted removal): " + shipment.getLightVanProducts());
+
+        System.out.println("Full Shipment Contents:");
+        shipment.forEach(System.out::println);
+
+    }
+
 }
