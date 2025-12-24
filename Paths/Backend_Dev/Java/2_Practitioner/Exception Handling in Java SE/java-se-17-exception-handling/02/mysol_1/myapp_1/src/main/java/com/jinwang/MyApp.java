2945955ad4e51f057ed322419691a68fa8e0c755
Jin Wang <specter01wj@gmail.com>
2025-12-24 01:53:56 -0500

    v3

--- Paths/Backend_Dev/Java/2_Practitioner/Exception Handling in Java SE/java-se-17-exception-handling/02/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Exception Handling in Java SE/java-se-17-exception-handling/02/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -7,7 +7,18 @@ import java.io.IOException;
 public class MyApp {
 
     public static void main(String[] args) {
-        if (args.length == 0) {
+        // Working with try/catch
+        int i = 12, j = 2;
+        try {
+            int result = i / (j - 2);
+            System.out.println(result);
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+
+
+        /*if (args.length == 0) {
             System.err.println("Please provide the input file as the first argument.");
             return;
         }
@@ -26,7 +37,7 @@ public class MyApp {
         } catch (IOException e) {
             System.err.println("Failed to read file: " + fileName);
             System.err.println("Error: " + e.getMessage());
-        }
+        }*/
     }
 
     private static void performOperation(String inputLine) {
