97ab7587bee48a8e4dd73531942416552531537f
Jin Wang <specter01wj@gmail.com>
2025-12-27 21:29:12 -0500

    v4

--- Paths/Backend_Dev/Java/2_Practitioner/Exception Handling in Java SE/java-se-17-exception-handling/03/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Exception Handling in Java SE/java-se-17-exception-handling/03/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -8,8 +8,18 @@ import java.io.IOException;
 public class MyApp {
 
     public static void main(String[] args) {
+        // Exception Can be handled by Type
+        int i = 12, j = 2;
+        try {
+            int result = i / (j - 2);
+            System.out.println(result);
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        System.out.println("Continue work!");
 
-        if (args.length == 0) {
+        /*if (args.length == 0) {
             System.out.println("Please provide the input file name.");
             return;
         }
@@ -28,7 +38,7 @@ public class MyApp {
         } catch (Exception ex) {
             // Catch-all for unexpected runtime issues
             System.out.println("Error processing file: " + ex.getMessage());
-        }
+        }*/
     }
 
     private static void processFile(BufferedReader reader) throws IOException {
