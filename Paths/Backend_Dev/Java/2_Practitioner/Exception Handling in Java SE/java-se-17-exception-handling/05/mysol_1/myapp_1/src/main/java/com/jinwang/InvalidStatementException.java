a87d3f7f40caaef648c09543dfa86d0e86c1f8ff
Jin Wang <specter01wj@gmail.com>
2025-12-28 22:39:11 -0500

    v4

---
+++ Paths/Backend_Dev/Java/2_Practitioner/Exception Handling in Java SE/java-se-17-exception-handling/05/mysol_1/myapp_1/src/main/java/com/jinwang/InvalidStatementException.java
@@ -0,0 +1,10 @@
+package com.jinwang;
+
+public class InvalidStatementException extends Exception {
+    public InvalidStatementException(String message) {
+        super(message);
+    }
+    public InvalidStatementException(String message, Exception ex) {
+        super(message, ex);
+    }
+}
