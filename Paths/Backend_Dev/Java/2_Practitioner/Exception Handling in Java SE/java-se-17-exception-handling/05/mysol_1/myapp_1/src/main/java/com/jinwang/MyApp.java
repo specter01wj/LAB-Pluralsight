92350dacf4be261b7b402954472f8b5afcbc7a14
Jin Wang <specter01wj@gmail.com>
2025-12-30 22:46:35 -0500

    v5

--- Paths/Backend_Dev/Java/2_Practitioner/Exception Handling in Java SE/java-se-17-exception-handling/05/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Exception Handling in Java SE/java-se-17-exception-handling/05/mysol_1/myapp_1/src/main/java/com/jinwang/MyApp.java
@@ -54,10 +54,6 @@ public class MyApp {
             int rightVal = valueFromWord(parts[2]);
 
             int result = execute(operation, leftVal, rightVal);
-
-
-
-            
             System.out.println(inputLine + " = " + result);
 
         } catch (InvalidStatementException ex) {
