a8b2f7df78011eeaf4abf41a17b7569ee97b4282
Jin Wang <specter01wj@gmail.com>
2026-01-05 00:44:03 -0500

    v3

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/02/mysol_1/myApp_1/src/main/java/com/jinwang/ProductCls.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/02/mysol_1/myApp_1/src/main/java/com/jinwang/ProductCls.java
@@ -1,4 +1,48 @@
 package com.jinwang;
 
-public class ProductCls {
+import java.util.Objects;
+
+public final class ProductCls {
+
+    private final long id;
+    private final String name;
+    private final String description;
+
+    public ProductCls(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProductCls)) return false;
+        ProductCls that = (ProductCls) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ProductCls{id=%d, name='%s', description='%s'}", id, name, description);
+    }
 }
