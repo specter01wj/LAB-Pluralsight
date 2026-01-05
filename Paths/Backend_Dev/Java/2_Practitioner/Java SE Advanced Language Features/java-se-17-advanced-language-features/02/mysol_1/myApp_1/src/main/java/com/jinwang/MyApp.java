1ff64c6cce4a28348fe0a87841c74cea9450b495
Jin Wang <specter01wj@gmail.com>
2026-01-05 00:43:21 -0500

    v3

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/02/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/02/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,27 @@
 package com.jinwang;
 
 public class MyApp {
+
+    public static void main(String[] args) {
+        // Test ProductCls (immutable class)
+        ProductCls clsProduct1 = new ProductCls(101, "Laptop", "High-end gaming laptop");
+        ProductCls clsProduct2 = new ProductCls(101, "Laptop", "High-end gaming laptop");
+
+        System.out.println("== Testing ProductCls ==");
+        System.out.println(clsProduct1);
+        System.out.println("Equal? " + clsProduct1.equals(clsProduct2));
+        System.out.println("HashCode: " + clsProduct1.hashCode());
+        System.out.println("Name: " + clsProduct1.getName());
+
+        // Test ProductRec (record)
+        ProductRec recProduct1 = new ProductRec(202, "Monitor", "27-inch 4K monitor");
+        ProductRec recProduct2 = new ProductRec(202, "Monitor", "27-inch 4K monitor");
+
+        System.out.println("\n== Testing ProductRec ==");
+        System.out.println(recProduct1);
+        System.out.println("Equal? " + recProduct1.equals(recProduct2));
+        System.out.println("HashCode: " + recProduct1.hashCode());
+        System.out.println("Name: " + recProduct1.name());
+    }
+
 }
