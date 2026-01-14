21c934786a3a592ec0bf65722ccbc53d193ec143
Jin Wang <specter01wj@gmail.com>
2026-01-14 02:57:01 -0500

    v6

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/03/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/03/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -2,22 +2,21 @@ package com.jinwang;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MyApp {
 
     public static void main(String[] args) {
-        var product1 = new Product(100123L, "Apples", "Tasty red apples");
-        var product2 = new Product(100346L, "Bread", "Whole-wheat loaf");
-
         var customer = new Customer(500567L, "Joe Smith", "joe.smith@example.com");
+        var product = new Product(100123L, "Apples", "Tasty red apples");
 
-        var order = new Order(200201L, customer, LocalDateTime.now())
-                .withLine(new SaleOrderLine(product1, 6, new BigDecimal("5.94"), new BigDecimal("0.99")))
-                .withLine(new DiscountOrderLine(product2, 2, new BigDecimal("2.50"), new BigDecimal("0.25")));
+        var order = new Order(200201L, customer, LocalDateTime.now(), List.of(
+                new SaleOrderLine(product, 6, new BigDecimal("5.94")),
+                new DiscountOrderLine("WELCOME", new BigDecimal("2.50"))));
 
-        var totalPrice = OrderService.calculateTotalPrice(order);
-
-        System.out.println("Total price of order: " + totalPrice);
+        var orderService = new OrderService();
+        var total = orderService.calculateTotalAmount(order);
+        System.out.printf("Total amount: %s%n", total);
     }
 
 }
