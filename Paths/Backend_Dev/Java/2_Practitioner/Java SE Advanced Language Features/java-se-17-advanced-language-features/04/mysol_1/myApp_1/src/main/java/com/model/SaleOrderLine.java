9027ce54272f7f739a2a325ae8331d852a06f7cb
Jin Wang <specter01wj@gmail.com>
2026-01-18 23:33:13 -0500

    v10

---
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/04/mysol_1/myApp_1/src/main/java/com/model/SaleOrderLine.java
@@ -0,0 +1,15 @@
+package com.model;
+
+import java.math.BigDecimal;
+
+import static com.jinwang.Validation.checkThat;
+
+// An order line that represents the sale of a product in a given quantity and amount.
+public record SaleOrderLine(Product product, int quantity, BigDecimal amount) implements OrderLine {
+
+    public SaleOrderLine {
+        checkThat(product != null, "Product must not be null");
+        checkThat(quantity > 0, "Quantity must be greater than zero");
+        checkThat(amount != null && amount.compareTo(BigDecimal.ZERO) > 0, "Amount must be greater than zero");
+    }
+}
