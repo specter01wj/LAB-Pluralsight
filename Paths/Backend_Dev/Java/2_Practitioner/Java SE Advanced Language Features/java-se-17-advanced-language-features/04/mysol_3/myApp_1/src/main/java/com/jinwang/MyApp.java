f4f92e19221fb6bc15aefb4642712f7074e64ddc
Jin Wang <specter01wj@gmail.com>
2026-01-19 01:55:40 -0500

    v8

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/04/mysol_3/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/04/mysol_3/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,32 @@
 package com.jinwang;
 
+import com.model.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class MyApp {
+
+    public static void main(String[] args) {
+        var product1 = new Product(100123L, "Apples", "Tasty red apples");
+        var product2 = new Product(100346L, "Bread", "Whole-wheat loaf");
+
+        var address = new Address("Main Street", "123A", "Springfield", "USA");
+        var customer = new Customer(500567L, "Joe Smith", "joe.smith@example.com", address);
+
+        var order = new Order(200201L, customer, LocalDateTime.now(), List.of(
+                new SaleOrderLine(product1, 6, new BigDecimal("5.94")),
+                new DiscountOrderLine("WELCOME", new BigDecimal("2.50"))
+        ));
+
+        var service = new OrderService();
+
+        var total1 = service.calculateTotalAmount(order);
+        var total2 = service.calculateTotalAmount2(order);
+
+        System.out.println("Total amount (switch statement): " + total1);
+        System.out.println("Total amount (switch expression): " + total2);
+    }
+
 }
