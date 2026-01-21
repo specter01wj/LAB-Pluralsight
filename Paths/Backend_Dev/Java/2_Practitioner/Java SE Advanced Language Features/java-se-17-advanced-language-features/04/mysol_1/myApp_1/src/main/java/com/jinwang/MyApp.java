59a435bea3496b6e63201798cdbbbbd051bf1c9d
Jin Wang <specter01wj@gmail.com>
2026-01-20 23:59:05 -0500

    v1

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/04/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/04/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -21,6 +21,7 @@ public class MyApp {
         ));
 
         var service = new OrderService();
+        
         var total = service.calculateTotalAmount(order);
 
         System.out.println("Total amount: " + total);
