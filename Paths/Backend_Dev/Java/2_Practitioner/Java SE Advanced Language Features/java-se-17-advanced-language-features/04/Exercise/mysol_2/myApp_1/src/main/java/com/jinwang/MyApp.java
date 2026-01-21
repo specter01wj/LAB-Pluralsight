2effba3402eeb5ea59a483d824e6e0218b6259ba
Jin Wang <specter01wj@gmail.com>
2026-01-21 01:50:50 -0500

    v12

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/04/Exercise/mysol_2/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/04/Exercise/mysol_2/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,28 @@
 package com.jinwang;
 
+import com.model.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class MyApp {
+
+    public static void main(String[] args) {
+        var apples = new Product(101L, "Apples", "Red and juicy");
+        var bread = new Product(102L, "Bread", "Whole-wheat");
+
+        var customer = new Customer(5001L, "Joe Smith", "joe@example.com",
+                new Address("Main Street", "123A", "Springfield", "USA"));
+
+        var order = new Order(3001L, customer, LocalDateTime.now(), List.of(
+                new SaleOrderLine(apples, 1, new BigDecimal("2.99")),
+                new SaleOrderLine(bread, 2, new BigDecimal("6.00")),
+                new DiscountOrderLine("SAVE10", new BigDecimal("1.50"))
+        ));
+
+        var service = new OrderService();
+        service.printOrder(order);
+    }
+
 }
