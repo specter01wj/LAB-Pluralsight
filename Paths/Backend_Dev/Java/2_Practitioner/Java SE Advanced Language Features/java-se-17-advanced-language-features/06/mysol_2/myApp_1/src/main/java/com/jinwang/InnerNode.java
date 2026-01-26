7eef99a60247af9b2df388bdcbfc8d2607d1e323
Jin Wang <specter01wj@gmail.com>
2026-01-26 00:23:23 -0500

    v7

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/06/mysol_2/myApp_1/src/main/java/com/jinwang/InnerNode.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/06/mysol_2/myApp_1/src/main/java/com/jinwang/InnerNode.java
@@ -1,18 +1,38 @@
 package com.jinwang;
 
-// Inner (non-leaf) node in a binary tree.
+import java.util.function.BinaryOperator;
+
+/**
+ * Inner (non-leaf) node in a binary tree.
+ *
+ * This version demonstrates:
+ * - extending generics
+ * - lazy evaluation of node value
+ * - passing behavior (how to combine values) via BinaryOperator
+ */
 public class InnerNode<T> implements TreeNode<T> {
 
-    private final TreeNode<T> left, right;
+    private final TreeNode<T> left;
+    private final TreeNode<T> right;
+    private final BinaryOperator<T> operator;
 
-    public InnerNode(TreeNode<T> left, TreeNode<T> right) {
+    /**
+     * Creates an inner node with two child nodes and an operator
+     * that defines how their values are combined.
+     */
+    public InnerNode(TreeNode<T> left, TreeNode<T> right, BinaryOperator<T> operator) {
         this.left = left;
         this.right = right;
+        this.operator = operator;
     }
 
+    /**
+     * Computes the value of this node dynamically by
+     * applying the operator to the values of the child nodes.
+     */
     @Override
     public T getValue() {
-        return null;
+        return operator.apply(left.getValue(), right.getValue());
     }
 
     @Override
@@ -27,6 +47,6 @@ public class InnerNode<T> implements TreeNode<T> {
 
     @Override
     public String toString() {
-        return String.format("{%s, %s}", left, right);
+        return String.format("[%s, %s]", left, right);
     }
 }
