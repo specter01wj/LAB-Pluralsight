461fd17d50b202b04fab0669b3431d4718dc8f9f
Jin Wang <specter01wj@gmail.com>
2026-02-02 02:29:57 -0500

    v3

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/08/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/08/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,56 @@
 package com.jinwang;
 
+import java.io.IOException;
+import java.util.*;
+
 public class MyApp {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Set<String> classNames = ClassUtils.findClassNames("com.pluralsight.advancedjava.examples.example01.commands");
+
+        List<Class<?>> commandClasses = new ArrayList<>();
+        for (String className : classNames) {
+            Class<?> cls = Class.forName(className);
+            Command annotation = cls.getAnnotation(Command.class);
+            if (annotation != null) {
+                System.out.printf("Found command: %s, order: %d%n", annotation.value(), annotation.order());
+                commandClasses.add(cls);
+            }
+        }
+
+        commandClasses.sort(Comparator.comparingInt(cls -> cls.getAnnotation(Command.class).order()));
+
+        var sessionState = new SessionState();
+        var scanner = new Scanner(System.in);
+
+        while (!sessionState.isFinished()) {
+            System.out.println();
+            for (Class<?> cls : commandClasses) {
+                Command annotation = cls.getAnnotation(Command.class);
+                System.out.printf("%-12s - %s%n", annotation.value(), annotation.description());
+            }
+
+            var userInput = UserInput.readUserInput(scanner);
+
+            boolean found = false;
+            for (Class<?> cls : commandClasses) {
+                Command annotation = cls.getAnnotation(Command.class);
+                if (annotation.value().equals(userInput.command())) {
+                    executeCommand(cls, sessionState, userInput);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.out.println("Command not recognized: " + userInput.command());
+            }
+        }
+    }
+
+    private static void executeCommand(Class<?> commandClass, SessionState sessionState, UserInput userInput) {
+        var instance = (CommandExecutor) ClassUtils.newInstance(commandClass);
+        instance.execute(sessionState, userInput);
+    }
+
 }
