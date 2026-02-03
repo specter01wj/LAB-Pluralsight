2e70e5406104e8dffd78bc7bfb8fbe600e3bb443
Jin Wang <specter01wj@gmail.com>
2026-02-03 01:56:23 -0500

    v3

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/08/Exercise/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/08/Exercise/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,38 @@
 package com.jinwang;
 
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+
 public class MyApp {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(">>> Chapter 8 – Exercise 1: Custom Annotation <<<");
+
+        var customerName = "Joe Smith";
+        var birthDate = LocalDate.of(1988, 3, 15);
+
+        // Anonymous class with @Loggable fields
+        var obj = new Object() {
+            @Loggable("Customer Name")
+            String name = customerName;
+
+            @Loggable(value = "Birthdate", formatter = LocalDateLoggableFormatter.class)
+            LocalDate dob = birthDate;
+        };
+
+        for (Field field : obj.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(Loggable.class)) {
+                field.setAccessible(true);
+
+                Loggable loggable = field.getAnnotation(Loggable.class);
+                LoggableFormatter formatter = loggable.formatter()
+                        .getDeclaredConstructor().newInstance();
+
+                Object value = field.get(obj);
+                System.out.printf("%s: %s\n", loggable.value(), formatter.format(value));
+            }
+        }
+
+        System.out.println("\n✅ Exercise 1 complete.\n");
+    }
 }
