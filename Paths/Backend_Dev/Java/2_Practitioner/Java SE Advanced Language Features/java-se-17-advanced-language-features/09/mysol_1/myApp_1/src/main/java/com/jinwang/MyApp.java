038fa049b9f2fa3831d0cfeaea9f97fc5e4e0cc0
Jin Wang <specter01wj@gmail.com>
2026-02-05 01:34:36 -0500

    v3

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/09/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/09/mysol_1/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,55 @@
 package com.jinwang;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Random;
+
 public class MyApp {
+
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+        System.out.println(">>> Chapter 9 – Example 1: Using Optional <<<");
+
+        Optional<Product> optional = findProductById(269912L);
+
+        Optional<Product> optional2 = Optional.ofNullable(getProductById(238461L));
+
+        if (optional.isPresent()) {
+            Product product = optional.get();
+            System.out.println("Found product: " + product);
+        } else {
+            System.out.println("Product not found");
+        }
+
+        Product product1 = optional.orElse(new Product(527502L, "Milk", new BigDecimal("1.39")));
+        System.out.println("Product: " + product1);
+
+        Product product2 = optional.orElseGet(() ->
+                Product.PRODUCTS.get(RANDOM.nextInt(Product.PRODUCTS.size())));
+        System.out.println(product2);
+
+        Product product3 = optional.orElseThrow(() -> new IllegalStateException("Product not found"));
+        System.out.println(product3);
+
+        System.out.println("✅ Example 1 complete.\n");
+    }
+
+    private static Product getProductById(long productId) {
+        for (Product product : Product.PRODUCTS) {
+            if (product.id() == productId) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    private static Optional<Product> findProductById(long productId) {
+        for (Product product : Product.PRODUCTS) {
+            if (product.id() == productId) {
+                return Optional.of(product);
+            }
+        }
+        return Optional.empty();
+    }
 }
