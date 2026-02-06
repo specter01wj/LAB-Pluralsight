c50a2a95aa6ed311d2690ebb6b35d5b3922d342b
Jin Wang <specter01wj@gmail.com>
2026-02-06 02:37:14 -0500

    v3

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/10/mysol_3/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/10/mysol_3/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,14 @@
 package com.jinwang;
 
 public class MyApp {
+
+    public static void main(String[] args) {
+        System.out.println(">>> Chapter 10 – Example 3: Resource Initialization and Closure Order <<<");
+
+        try (var r1 = new ExampleResource("1"); var r2 = new ExampleResource("2")) {
+            System.out.println("Inside the try-block");
+        }
+
+        System.out.println("✅ Example 3 complete.\n");
+    }
 }
