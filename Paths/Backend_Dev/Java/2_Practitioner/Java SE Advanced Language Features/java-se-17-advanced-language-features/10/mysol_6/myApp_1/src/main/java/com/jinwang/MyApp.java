95308a9d57996fe67825117e031d564e4eb1bcac
Jin Wang <specter01wj@gmail.com>
2026-02-06 03:40:24 -0500

    v14

--- Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/10/mysol_6/myApp_1/src/main/java/com/jinwang/MyApp.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Java SE Advanced Language Features/java-se-17-advanced-language-features/10/mysol_6/myApp_1/src/main/java/com/jinwang/MyApp.java
@@ -1,4 +1,28 @@
 package com.jinwang;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+/**
+ * Example 6: Implementing your own resource in practice.
+ */
 public class MyApp {
+
+    public static void main(String[] args) {
+        System.out.println(">>> Chapter 10 – Example 6: Custom AutoCloseable Resource <<<");
+
+        try (var tempDir = new TempDirectory("test");
+             var out = Files.newBufferedWriter(tempDir.getDirectory().resolve("test.txt"), StandardCharsets.UTF_8)) {
+
+            System.out.println("Temporary directory: " + tempDir.getDirectory());
+
+            out.write("Pluralsight");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("✅ Example 6 complete.\n");
+    }
 }
