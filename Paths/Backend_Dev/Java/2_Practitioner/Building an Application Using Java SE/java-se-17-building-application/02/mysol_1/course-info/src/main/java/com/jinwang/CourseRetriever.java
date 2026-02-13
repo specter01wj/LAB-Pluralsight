c19b6057f45af9880c45e19bdd477fbd216eb9c3
Jin Wang <specter01wj@gmail.com>
2026-02-13 02:13:00 -0500

    v1

--- Paths/Backend_Dev/Java/2_Practitioner/Building an Application Using Java SE/java-se-17-building-application/02/mysol_1/course-info/src/main/java/com/jinwang/CourseRetriever.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Building an Application Using Java SE/java-se-17-building-application/02/mysol_1/course-info/src/main/java/com/jinwang/CourseRetriever.java
@@ -1,9 +1,20 @@
 package com.jinwang;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String... args) {
-        System.out.println("CourseRetriever started!");
+
+        logger.info("Application started");
+        logger.warn("This is a warning");
+        logger.error("Something went wrong");
+
+        /*System.out.println("CourseRetriever started!");
 
         if (args.length == 0) {
             System.out.println("Please provide an author name as first argument.");
@@ -15,11 +26,11 @@ public class CourseRetriever {
         } catch (Exception e) {
             System.out.println("Unexpected error");
             e.printStackTrace();
-        }
+        }*/
     }
 
-    private static void retrieveCourses(String authorId) {
+    /*private static void retrieveCourses(String authorId) {
         System.out.println("Retrieving courses for author " + authorId);
-    }
+    }*/
 
 }
