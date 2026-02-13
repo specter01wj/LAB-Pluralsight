40ca9503b0e5e9ba3a668bf3460aafdf102908ba
Jin Wang <specter01wj@gmail.com>
2026-02-13 14:35:51 -0500

    v5

--- Paths/Backend_Dev/Java/2_Practitioner/Building an Application Using Java SE/java-se-17-building-application/02/mysol_2/course-info/src/main/java/com/jinwang/CourseRetriever.java
+++ Paths/Backend_Dev/Java/2_Practitioner/Building an Application Using Java SE/java-se-17-building-application/02/mysol_2/course-info/src/main/java/com/jinwang/CourseRetriever.java
@@ -14,6 +14,23 @@ public class CourseRetriever {
         logger.warn("This is a warning");
         logger.error("Something went wrong");
 
+        logger.info("Starting CourseRetriever application");
+
+        if (args.length == 0) {
+            logger.error("No author name provided. Please pass an author name as first argument.");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            logger.error("Unexpected error while retrieving courses", e);
+        }
+
+    }
+
+    private static void retrieveCourses(String authorId) {
+        logger.info("Retrieving courses for author '{}'", authorId);
     }
 
 }
