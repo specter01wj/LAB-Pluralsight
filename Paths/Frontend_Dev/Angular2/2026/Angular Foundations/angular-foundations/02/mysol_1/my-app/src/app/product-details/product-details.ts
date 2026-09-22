dea766acf480479e64eb78734071b3fc55b08e03
Jin Wang <specter01wj@gmail.com>
2026-09-22 01:40:07 -0400

    v3

---
+++ Paths/Frontend_Dev/Angular2/2026/Angular Foundations/angular-foundations/02/mysol_1/my-app/src/app/product-details/product-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  imports: [],
+  templateUrl: './product-details.html',
+  styleUrl: './product-details.scss',
+})
+export class ProductDetails {}
