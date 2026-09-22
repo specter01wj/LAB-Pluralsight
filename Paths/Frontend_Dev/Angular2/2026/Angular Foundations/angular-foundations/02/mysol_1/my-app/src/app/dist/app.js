ef9e284d46ead50c1edcb1b83292b121a6be95b0
Jin Wang <specter01wj@gmail.com>
2026-09-22 01:53:39 -0400

    v6

---
+++ Paths/Frontend_Dev/Angular2/2026/Angular Foundations/angular-foundations/02/mysol_1/my-app/src/app/dist/app.js
@@ -0,0 +1,27 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.App = void 0;
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var catalog_1 = require("./catalog/catalog");
+var App = /** @class */ (function () {
+    function App() {
+        this.title = core_1.signal('my-app');
+    }
+    App = __decorate([
+        core_1.Component({
+            selector: 'app-root',
+            imports: [router_1.RouterOutlet, catalog_1.Catalog],
+            templateUrl: './app.html',
+            styleUrl: './app.scss'
+        })
+    ], App);
+    return App;
+}());
+exports.App = App;
